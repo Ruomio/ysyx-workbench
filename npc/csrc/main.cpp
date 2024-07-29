b6894fa71bfccc51acd7cf31745305198a3558f3
100006513-PeiZhi Peng <1065940593@qq.com>
2024-07-29 09:54:06 +0800

    dual switch source code

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,34 @@
-#include <stdio.h>
+#include <cstdio>
+#include <verilated.h>
+#include "verilated_vcd_c.h"
+#include "Vexample.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+int main(int argc, char **argv) {
+    VerilatedContext *contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    VerilatedVcdC *tfp = new VerilatedVcdC;
+    
+    Vexample *top = new Vexample(contextp);
+    contextp->traceEverOn(true);
+    top->trace(tfp, 0);
+    tfp->open("wave.vcd");
+
+    while(!contextp->gotFinish()) {
+        static int i = 0;
+        if(i++>50) break;
+        top->a = rand()&1;
+        top->b = rand()&1;
+        top->eval();
+        printf("top->a = %d\n",top->a);
+        printf("top->b = %d\n",top->b);
+        printf("top->f = %d\n",top->f);
+
+        tfp->dump(contextp->time());
+        contextp->timeInc(1);
+    }
+    top->final();
+    delete top;
+    tfp->close();
+    delete contextp;
+    return 0;
 }
