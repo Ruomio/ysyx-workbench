dcb7ffe09755c585be37e35a7beb416f6090dc3a
tracer-ysyx <tracer@ysyx.org>
2024-08-14 16:51:37 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.6.45-1-lts #1 SMP PREEMPT_DYNAMIC Sun, 11 Aug 2024 14:02:12 +0000 x86_64 GNU/Linux  16:51:37 up  5:53,  2 users,  load average: 0.11, 0.17, 0.24

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -75,29 +75,6 @@ static struct {
 };
 
 
-static int cmd_help(char *args) {
-  /* extract the first argument */
-  char *arg = strtok(NULL, " ");
-  int i;
-
-  if (arg == NULL) {
-    /* no argument given */
-    for (i = 0; i < NR_CMD; i ++) {
-      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-    }
-  }
-  else {
-    for (i = 0; i < NR_CMD; i ++) {
-      if (strcmp(arg, cmd_table[i].name) == 0) {
-        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-        return 0;
-      }
-    }
-    printf("Unknown command '%s'\n", arg);
-  }
-  return 0;
-}
-
 void sdb_set_batch_mode() {
   is_batch_mode = true;
 }
@@ -149,6 +126,29 @@ void init_sdb() {
 }
 
 
+
+static int cmd_help(char *args) {
+  /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+  int i;
+
+  if (arg == NULL) {
+    /* no argument given */
+    for (i = 0; i < NR_CMD; i ++) {
+      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+    }
+  }
+  else {
+    for (i = 0; i < NR_CMD; i ++) {
+      if (strcmp(arg, cmd_table[i].name) == 0) {
+        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+        return 0;
+      }
+    }
+    printf("Unknown command '%s'\n", arg);
+  }
+  return 0;
+}
 
 static int cmd_c(char *args) {
   cpu_exec(-1);
