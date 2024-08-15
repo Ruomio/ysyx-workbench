848869a8b797817a19adb42eb956f31f0b5a34bc
tracer-ysyx <tracer@ysyx.org>
2024-08-15 09:53:19 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.6.45-1-lts #1 SMP PREEMPT_DYNAMIC Sun, 11 Aug 2024 14:02:12 +0000 x86_64 GNU/Linux  09:53:19 up 22:55,  2 users,  load average: 0.12, 0.31, 0.40

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -25,7 +25,7 @@ const char *regs[] = {
 
 void isa_reg_display() {
     for(int i=0; i<sizeof(regs)/sizeof(regs[0]); i++) {
-        if(gpr(i) != 0) {
+        if(gpr(i) != 0 || 1) {
             printf("%s : 0x%x\n", regs[i], gpr(i) );
         }
     }
