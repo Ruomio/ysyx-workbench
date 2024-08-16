8f778f584e93c3f4da4c6331d603c53744299a6c
tracer-ysyx <tracer@ysyx.org>
2024-08-16 15:45:27 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.5-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 15 Aug 2024 00:25:30 +0000 x86_64 GNU/Linux  15:45:27 up  2:32,  2 users,  load average: 0.53, 0.55, 0.47

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -162,7 +162,7 @@ word_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
   // TODO();
   *success = true;
-  return eval(tokens, 0, nr_token);
+  return eval(tokens, 0, nr_token-1);
 
   // return 0;
 }
