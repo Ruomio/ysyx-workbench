ad15695408a0b3fa1c9b29e2b3179de2e0516c78
tracer-ysyx <tracer@ysyx.org>
2024-08-16 09:10:05 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.6.45-1-lts #1 SMP PREEMPT_DYNAMIC Sun, 11 Aug 2024 14:02:12 +0000 x86_64 GNU/Linux  09:10:05 up 58 min,  2 users,  load average: 0.44, 0.39, 0.42

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -53,7 +53,9 @@ static struct rule {
   {"/", TK_DIV},
   {"\\(", TK_LBRACK},
   {"\\)", TK_RBRACK},
-  {"\\$()", TK_HEXNUM},
+  {"\\$(\\$0|ra|sp|gp|tp|t[0-6]|s[0-9]{1,2}|a[0-7]|)", TK_REG},
+  {"0x[0-9a-e]+", TK_HEXNUM},
+  {"0b[0-1]+", TK_BINNUM},
 };
 
 #define NR_REGEX ARRLEN(rules)
