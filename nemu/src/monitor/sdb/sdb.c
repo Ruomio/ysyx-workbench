eb590da004912ff19ae9e5604892d0bbcb30203a
tracer-ysyx <tracer@ysyx.org>
2024-08-16 16:29:39 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.5-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 15 Aug 2024 00:25:30 +0000 x86_64 GNU/Linux  16:29:39 up  3:16,  2 users,  load average: 0.48, 0.40, 0.34

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -171,6 +171,7 @@ void sdb_mainloop() {
 
   for (char *str; (str = rl_gets()) != NULL; ) {
     char *str_end = str + strlen(str);
+    printf("str = %s\n", str);
 
     /* extract the first token as the command */
     char *cmd = strtok(str, " ");
