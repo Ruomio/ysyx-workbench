561cab347786a06cf26fd29ca2e010190febd99f
tracer-ysyx <tracer@ysyx.org>
2024-08-19 17:07:30 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.5-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 15 Aug 2024 00:25:30 +0000 x86_64 GNU/Linux  17:07:30 up  5:53,  2 users,  load average: 0.39, 0.34, 0.46

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -21,6 +21,8 @@
 #include <regex.h>
 #include "common.h"
 
+#define TOKENS_SIZE 1024
+
 enum {
   TK_NOTYPE = 256,
 
@@ -158,7 +160,7 @@ typedef struct token {
   char str[32];
 } Token;
 
-static Token tokens[32] __attribute__((used)) = {};
+static Token tokens[TOKENS_SIZE] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
 static bool make_token(char *e) {
