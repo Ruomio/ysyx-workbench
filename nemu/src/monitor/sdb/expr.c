b42a7a44171991af1b2bbe5d96d3343e6969ea13
tracer-ysyx <tracer@ysyx.org>
2024-08-19 11:04:47 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.5-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 15 Aug 2024 00:25:30 +0000 x86_64 GNU/Linux  11:04:47 up  2:39,  2 users,  load average: 0.34, 0.42, 0.51

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -70,7 +70,8 @@ enum {
   TK_OR_ASSIGN,       // |=
   TK_XOR_ASSIGN,       // ^=
   TK_COMMA,       // ,
-  TK_DEREFRENCE,
+  TK_DEREFRENCE,    // *p_param
+  TK_ADDRESSOF,    // &param
 };
 
 static struct rule {
@@ -89,7 +90,10 @@ static struct rule {
   {"!=", TK_NEQ},        // equal
   {"-", TK_SUB},
   {"--", TK_DSUB},
-  {"(\\*)[a-zA-Z0-9]+", TK_DEREFRENCE},
+  {"\\*[a-zA-Z]+[0-9]*", TK_DEREFRENCE},
+  {"&[a-zA-Z]+[0-9]*", TK_ADDRESSOF},
+  {"[a-zA-Z0-9]+\\.[a-zA-Z0-9]+", TK_DOT},
+  {"[a-zA-Z0-9]+->[a-zA-Z0-9]+", TK_ARROW},
   {"\\*",TK_MULTIP},
   {"/", TK_DIV},
   {"%", TK_COMPLE},
@@ -101,8 +105,6 @@ static struct rule {
   {"0x[0-9a-fA-F]+", TK_HEXNUM},
   {"0b[0-1]+", TK_BINNUM},
   {"[0-9]+", TK_DECNUM},
-  {"\\.", TK_DOT},
-  {"->", TK_ARROW},
   {"~", TK_COUNT},
   {"&", TK_AND},
   {"|", TK_OR},
