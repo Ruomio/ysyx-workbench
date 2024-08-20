ae7525605b7d2a8b59c9fdd6d57d61f5bf4ad11b
tracer-ysyx <tracer@ysyx.org>
2024-08-20 13:51:47 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.5-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 15 Aug 2024 00:25:30 +0000 x86_64 GNU/Linux  13:51:47 up  1:01,  2 users,  load average: 0.24, 0.21, 0.19

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -277,7 +277,7 @@ static uint32_t eval(Token *tokens, int s, int e) {
       case TK_MULTIP: ret = val1 * val2; break;
       case TK_DIV: {
         if(val2 == 0) {
-          Assert(val2 != 0, "error: divisor could not be zero.\n");
+          Assert(val2 != 0, "error: divisor could not be zero. divisor position is %d\n", op);
         }
         ret = val1 / val2;
         break;
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -125,7 +125,7 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc -Woverflow -Werror /tmp/.code.c -o /tmp/.expr");
+    int ret = system("gcc -Wall -Werror /tmp/.code.c -o /tmp/.expr");
     if (ret != 0) continue;
 
     fp = popen("/tmp/.expr", "r");
