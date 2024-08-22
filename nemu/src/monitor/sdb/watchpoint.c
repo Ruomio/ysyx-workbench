6d099869d22c3310e138abe9e574daae0e57fc64
tracer-ysyx <tracer@ysyx.org>
2024-08-22 13:36:25 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 19 Aug 2024 17:02:39 +0000 x86_64 GNU/Linux  13:36:25 up  2:41,  2 users,  load average: 0.97, 0.71, 0.54

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -134,12 +134,12 @@ void print_watchpoint(bool *is_change) {
       return;
     }
     if(ret != last[p->NO]) {
-      *is_change = true;
-      last[p->NO] = ret;
-
       printf("watch point %d: %s\n", p->NO, p->str);
       printf("Old value = 0x%x\n", last[p->NO]);
       printf("New value = 0x%x\n", ret);
+
+      *is_change = true;
+      last[p->NO] = ret;
     }
   }
 }
