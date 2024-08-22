385bc9087505ac3841ad00a490dd26fe5ba4248b
tracer-ysyx <tracer@ysyx.org>
2024-08-22 10:19:03 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 19 Aug 2024 17:02:39 +0000 x86_64 GNU/Linux  10:19:03 up  2:06,  2 users,  load average: 0.81, 1.01, 0.76

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -33,6 +33,7 @@ void isa_reg_display() {
 word_t isa_reg_str2val(const char *s, bool *success) {
   word_t ret = 0;
   if(strcmp(s, "pc") == 0) {
+    *success = true;
     return cpu.pc;
   }
   for(int i=0; i<32; i++) {
