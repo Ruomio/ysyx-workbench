bbc49081819c29c0de7f94025eb599569973924c
100006513-PeiZhi Peng <1065940593@qq.com>
2024-08-22 13:56:16 +0800

    update reg to achive isa_reg_str2val

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -31,5 +31,19 @@ void isa_reg_display() {
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
+  word_t ret = 0;
+  if(strcmp(s, "pc") == 0) {
+    *success = true;
+    return cpu.pc;
+  }
+  for(int i=0; i<32; i++) {
+    if(strcmp(s, regs[i]) == 0) {
+      ret = gpr(i);
+      *success = true;
+      return ret;
+    }
+  }
+  printf("Unknow reg name\n");
+  *success = false;
   return 0;
 }
