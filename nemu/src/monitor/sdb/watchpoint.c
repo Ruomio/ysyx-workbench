7da5463edf93b4cbff9a6c0b85f43b803df8c05f
tracer-ysyx <tracer@ysyx.org>
2024-08-22 15:19:36 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 19 Aug 2024 17:02:39 +0000 x86_64 GNU/Linux  15:19:36 up  4:25,  2 users,  load average: 0.85, 0.45, 0.41

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -136,7 +136,7 @@ bool free_wp_by_no_interface(int no) {
 
 void scan_watchpoint(bool *is_change, bool *is_break) {
   static word_t last[NR_WP] = {0};
-  static bool is_first = true;
+  static bool is_first[NR_WP] = {true};
   for(WP *p = head; p != NULL; p = p->next) {
     bool success = false;
     word_t ret = expr(p->str, &success);
@@ -149,14 +149,14 @@ void scan_watchpoint(bool *is_change, bool *is_break) {
       printf("break point at 0x%x\n", ret);
       return;
     }
-    if(ret != last[p->NO] && p->type == WP_TYPE && !is_first) {
+    if(ret != last[p->NO] && p->type == WP_TYPE && !is_first[p->NO]) {
       *is_change = true;
       printf("watch point %d: %s\n", p->NO, p->str);
       printf("Old value = 0x%x\n", last[p->NO]);
       printf("New value = 0x%x\n", ret);
 
     }
-    is_first = false;
+    is_first[p->NO] = false;
     last[p->NO] = ret;
   }
 }
