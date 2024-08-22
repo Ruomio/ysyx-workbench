9a4bb8f055747ec33fcee609a9dd8235c548e05f
tracer-ysyx <tracer@ysyx.org>
2024-08-22 14:49:49 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 19 Aug 2024 17:02:39 +0000 x86_64 GNU/Linux  14:49:49 up  3:55,  2 users,  load average: 0.20, 0.35, 0.45

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -46,6 +46,7 @@ void init_wp_pool() {
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i;
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
+    memset(wp_pool[i].str, 0, STR_SIZE);
   }
 
   head = NULL;
