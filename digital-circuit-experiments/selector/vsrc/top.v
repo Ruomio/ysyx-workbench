5e9e61851947e3f0dc44af0543d2e8d0772bc40e
tracer-ysyx <tracer@ysyx.org>
2024-08-23 16:13:38 +0800

    > sim RTL ysyx_100006513 彭培智 Linux archlinux 6.10.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 19 Aug 2024 17:02:39 +0000 x86_64 GNU/Linux  16:13:38 up 1 day,  5:19,  2 users,  load average: 0.72, 0.60, 0.46

--- digital-circuit-experiments/selector/vsrc/top.v
+++ digital-circuit-experiments/selector/vsrc/top.v
@@ -9,8 +9,10 @@ module top
 );
 
     reg [31:0] cnt = 32'b0;
+    reg [1:0]f_reg;
 
-    mux41d #(.NR_KEY(NR_KEY), .KEY_LEN(KEY_LEN), .DATA_LEN(DATA_LEN)) u_mux41d (x, y, f);
+
+    mux41d #(.NR_KEY(NR_KEY), .KEY_LEN(KEY_LEN), .DATA_LEN(DATA_LEN)) u_mux41d (x, y, f_reg);
 
     always @(posedge clk) begin
         if(!rst) begin
@@ -18,11 +20,15 @@ module top
         end
         else if(cnt == 32'b11111111) begin
             cnt <=  32'b0;
+            f <= ~f;
         end
         else begin
             cnt <= cnt + 1'b1;
         end
     end
+
+
+    assign f = f_reg;
 
 endmodule
 
@@ -51,28 +57,28 @@ module mux41d
         }));
 endmodule
 
-module mux41b
-    #(parameter NR_KEY=4, parameter KEY_LEN=2, parameter DATA_LEN=1)
-(
-    input [3:0] x,
-    input [1:0] y,
-    output  f
-);
-    
-    // MuxKey #(4, 2, 2) u_MuxKey (f, y, {
-    //     2'b00. x{1,0},
-    //     2'b01, x{3,2},
-    //     2'b10, x{5,4},
-    //     2'b11, x{7,6}
-    // });
-
-    MuxKey 
-        #(.NR_KEY(NR_KEY), .KEY_LEN(KEY_LEN), .DATA_LEN(DATA_LEN)) u_MuxKey (.out(f), .key(y), .lut({
-            2'b00, x[0],
-            2'b01, x[1],
-            2'b10, x[2],
-            2'b11, x[3]
-          }));
-endmodule
+// module mux41b
+//     #(parameter NR_KEY=4, parameter KEY_LEN=2, parameter DATA_LEN=1)
+// (
+//     input [3:0] x,
+//     input [1:0] y,
+//     output  f
+// );
+//     
+//     // MuxKey #(4, 2, 2) u_MuxKey (f, y, {
+//     //     2'b00. x{1,0},
+//     //     2'b01, x{3,2},
+//     //     2'b10, x{5,4},
+//     //     2'b11, x{7,6}
+//     // });
+// 
+//     MuxKey 
+//         #(.NR_KEY(NR_KEY), .KEY_LEN(KEY_LEN), .DATA_LEN(DATA_LEN)) u_MuxKey (.out(f), .key(y), .lut({
+//             2'b00, x[0],
+//             2'b01, x[1],
+//             2'b10, x[2],
+//             2'b11, x[3]
+//           }));
+// endmodule
 
 
