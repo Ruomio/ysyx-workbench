da8a92396929725e58df91da7e3316cde1693583
tracer-ysyx <tracer@ysyx.org>
2024-08-25 22:49:59 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 19 Aug 2024 17:02:39 +0000 x86_64 GNU/Linux  22:49:59 up  6:40,  2 users,  load average: 0.30, 0.52, 0.57

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -438,6 +438,7 @@ void test_expr() {
   bool success = false;
   while((fscanf(fp, "%[^\n]\n", str)) != EOF) {
     if(strcmp(str,"") == 0) {
+      assert(0);
       line++;
       continue;
     }
