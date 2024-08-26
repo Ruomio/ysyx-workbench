98ecde043c8fcccd0036f371ee79f0a46b1fd39d
tracer-ysyx <tracer@ysyx.org>
2024-08-26 11:42:35 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 19 Aug 2024 17:02:39 +0000 x86_64 GNU/Linux  11:42:35 up  2:44,  2 users,  load average: 0.84, 0.84, 0.81

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -41,7 +41,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
 #ifdef CONFIG_WATCHPOINT_COND
-  printf("define CONFIG_WATCH");
+  printf("define CONFIG_WATCH\n");
   // #ifdef WATCHPOINT_COND 
     // printf("\n");
   // #endif
