5c84dade605692cf51659452e8d46bd82f6f5fc5
tracer-ysyx <tracer@ysyx.org>
2024-08-26 09:51:25 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 19 Aug 2024 17:02:39 +0000 x86_64 GNU/Linux  09:51:25 up 53 min,  2 users,  load average: 0.72, 0.73, 0.63

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -341,7 +341,6 @@ static bool check_parentheses(Token *tokens, int s, int e) {
       top--;
       if(top < 0) return false;
       if(top == 0 && i != e) return false;
-      else if(top == 0 && i == e) return true;
     }
   }
   if(tokens[s].str[0] == '(' && \
