1bd9703ae6655791f9061b3372d1174b2f632c42
tracer-ysyx <tracer@ysyx.org>
2024-08-30 16:36:50 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 19 Aug 2024 17:02:39 +0000 x86_64 GNU/Linux  16:36:50 up  3:46,  2 users,  load average: 0.68, 0.46, 0.37

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -52,8 +52,8 @@ int strcmp(const char *s1, const char *s2) {
 
 int strncmp(const char *s1, const char *s2, size_t n) {
   for(int i=0; (i < n) && (s1[i] != '\0' || s2[i] != '\0') ; i++) {
-    if(s1[i] == '\0') return s2[i];
-    else if(s2[i] == '\0') return s1[1];
+    if(s1[i] != '\0') return s1[i];
+    else if(s2[i] != '\0') return s2[1];
     else if(*((uint8_t *)s1 + i) != *((uint8_t *)s2 + i)) {
       return *((uint8_t *)s1 + i) - *((uint8_t *)s2 + i);
     }
@@ -104,8 +104,10 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  for(int i=0; i<n; i++) {
-    if(*((uint8_t *)s1 + i) != *((uint8_t *)s2 + i)) {
+  for(int i=0; (i<n) && (*((uint8_t*)s1 + i) != '\0' || *((uint8_t*)s2 + i) != '\0') ; i++) {
+    if(*((uint8_t*)s1 + i) != '\0') return *((uint8_t*)s1 + i);
+    else if(*((uint8_t*)s2 + i) != '\0') return *((uint8_t*)s2 + i);
+    else if(*((uint8_t *)s1 + i) != *((uint8_t *)s2 + i)) {
       return *((uint8_t *)s1 + i) - *((uint8_t *)s2 + i);
     }
   }
