5d20f6b2aef972c3f49de9904eb2e5a7ffebd64c
tracer-ysyx <tracer@ysyx.org>
2024-08-30 08:26:08 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 19 Aug 2024 17:02:39 +0000 x86_64 GNU/Linux  08:26:08 up 13:44,  2 users,  load average: 0.98, 0.72, 0.46

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -14,11 +14,9 @@
 ***************************************************************************************/
 
 #include "local-include/reg.h"
-#include "macro.h"
 #include <cpu/cpu.h>
 #include <cpu/ifetch.h>
 #include <cpu/decode.h>
-#include <stdint.h>
 
 #define R(i) gpr(i)
 #define Mr vaddr_read
