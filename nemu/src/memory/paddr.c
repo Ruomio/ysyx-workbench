181cf48d1c321ebe91c36c6743784b9e7f7442dd
tracer-ysyx <tracer@ysyx.org>
2024-09-02 16:47:42 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 29 Aug 2024 16:48:57 +0000 x86_64 GNU/Linux  16:47:42 up  7:35,  2 users,  load average: 1.54, 1.07, 0.84

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -70,7 +70,7 @@ static void pmem_write(paddr_t addr, int len, word_t data) {
 }
 
 static void out_of_bound(paddr_t addr) {
-  IFDEF(CONFIG_MTRACE_COND, MtraceBuf_save());
+  IFDEF(CONFIG_MTRACE_COND, MtraceBuf_add_arrow(); MtraceBuf_save());
   panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
       addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
 }
