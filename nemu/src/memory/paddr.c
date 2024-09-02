559818f6fd6a597f32d2f8014d2d019d6cf9455a
tracer-ysyx <tracer@ysyx.org>
2024-09-02 16:46:45 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 29 Aug 2024 16:48:57 +0000 x86_64 GNU/Linux  16:46:45 up  7:34,  2 users,  load average: 1.03, 0.92, 0.78

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -13,15 +13,48 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
+#include "common.h"
 #include <memory/host.h>
 #include <memory/paddr.h>
 #include <device/mmio.h>
 #include <isa.h>
+#include <stdio.h>
 
 #if   defined(CONFIG_PMEM_MALLOC)
 static uint8_t *pmem = NULL;
 #else // CONFIG_PMEM_GARRAY
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
+#endif
+
+#ifdef CONFIG_MTRACE_COND
+  struct MtraceBuf {
+    uint8_t m_buffer[64][64];
+    int idx;
+  }MtraceBuf = {.m_buffer={}, .idx = 0};
+
+  void MtraceBuf_write(paddr_t addr, int len, word_t data) {
+    int idx = MtraceBuf.idx;
+    memset(MtraceBuf.m_buffer[idx], 0, 64);
+    memset(MtraceBuf.m_buffer[idx], ' ', 3);
+    sprintf((char *)MtraceBuf.m_buffer[idx]+3, "%u    %d    %u", addr, len, data);
+    MtraceBuf.idx = (idx+1)%64;
+  }
+
+  void MtraceBuf_add_arrow() {
+    int idx = (MtraceBuf.idx + 63) % 64;
+    memcpy(MtraceBuf.m_buffer[idx], "-> ", 3);
+  }
+
+  void MtraceBuf_save() {
+    FILE *fp = fopen("/home/papillon/Documents/All_codes/ysyx-workbench/nemu/build/mtrace-log.txt", "w");
+    for(int i=0; i<MtraceBuf.idx; i++) {
+      if(strlen((char *)MtraceBuf.m_buffer[i]) != 0) {
+        fprintf(fp, "%s\n", MtraceBuf.m_buffer[i]);
+      }
+    }
+    fclose(fp);
+  }
+
 #endif
 
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
@@ -37,6 +70,7 @@ static void pmem_write(paddr_t addr, int len, word_t data) {
 }
 
 static void out_of_bound(paddr_t addr) {
+  IFDEF(CONFIG_MTRACE_COND, MtraceBuf_save());
   panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
       addr, PMEM_LEFT, PMEM_RIGHT, cpu.pc);
 }
@@ -57,6 +91,7 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
+  IFDEF(CONFIG_MTRACE_COND, MtraceBuf_write(addr, len, 0));
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
@@ -64,6 +99,7 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+  IFDEF(CONFIG_MTRACE_COND, MtraceBuf_write(addr, len, data));
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
