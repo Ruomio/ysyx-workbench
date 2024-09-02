d97855b593f0f2ff7ce34db3ee90920178f5cc3b
tracer-ysyx <tracer@ysyx.org>
2024-09-02 10:51:00 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 29 Aug 2024 16:48:57 +0000 x86_64 GNU/Linux  10:51:00 up  1:38,  2 users,  load average: 0.49, 0.41, 0.37

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,16 +5,140 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+#define NR_SIZE 1024
+
+static int itoa(int val, char *buf, char type);
+
+static int itoa_recur(int val, char *buf, char type) {
+  char *tmp_buf = buf;
+  tmp_buf += itoa(val, buf, type);
+  *tmp_buf++ = '\0';
+  return tmp_buf - buf;
+}
+
+static int itoa(int val, char *buf, char type) {
+  char *tmp_buf = buf;
+  // static bool is_prefix = false;
+  if(val < 0) {
+    *tmp_buf++ = '-';
+    val = -val;
+  }
+  switch(type) {
+    case 'd': {
+      if(val/10 != 0) {
+        tmp_buf += itoa(val/10, tmp_buf, 'd');
+      }
+      *tmp_buf++ = val%10 + '0';
+      break;
+    }
+    case 'x': {
+      if(val/16 != 0) {
+        tmp_buf += itoa(val/16, tmp_buf, 'x');
+      }
+      if(val%16 <= 9) *tmp_buf++ = val%16 + '0';
+      else *tmp_buf++ = val%16 - 10 + 'a';
+      break;
+    }
+    case 'X': {
+      if(val/16 != 0) {
+        tmp_buf += itoa(val/16, tmp_buf, 'X');
+      }
+      if(val%16 <= 9) *tmp_buf++ = val%16 + '0';
+      else *tmp_buf++ = val%16 - 10 + 'A';
+      break;
+    }
+
+    default: {
+      #define MSG "Unknown format.\n"
+      strcpy(tmp_buf, MSG);
+      tmp_buf += strlen(MSG);
+      break;
+    }
+  }
+
+  return tmp_buf - buf;
+}
+
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  int i = 0;
+  va_list arg;
+  char buf[NR_SIZE] = {0};
+  va_start(arg, fmt);
+  i = vsprintf(buf, fmt, arg);
+  va_end(arg);
+  // ioe_write(0, buf);
+  return i;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  char *p = NULL;
+  char tmp[NR_SIZE] = {};
+  va_list p_next = ap;
+
+  for(p = out; fmt != NULL; fmt++) {
+    if(*fmt != '%') {
+      *p++ = *fmt;
+      continue;
+    }
+
+    fmt++;
+
+    switch(*fmt) {
+      case 'd': {
+        int val = va_arg(p_next, int);
+        itoa_recur(val, tmp, 'd'); 
+        strcpy(p, tmp);
+        p += strlen(tmp);
+        break;
+      } 
+      case 'x': {
+        int val = va_arg(p_next, int);
+        itoa_recur(val, tmp, 'x');
+        strcpy(p, tmp);
+        p += strlen(tmp);
+        break;
+      }
+      case 'X': {
+        int val = va_arg(p_next, int);
+        itoa_recur(val, tmp, 'X');
+        strcpy(p, tmp);
+        p += strlen(tmp);
+        break;
+      }
+      case 'c': {
+        char val = (char)va_arg(p_next, int);
+        *p++ = val;
+        break;
+      }
+      case 's': {
+        char *s = va_arg(p_next, char *);
+        strcpy(p, s);
+        p += strlen(s);
+        break;
+      }
+
+      default: {
+        // unknown format
+        *p++ = '%';
+        *p++ = *fmt;
+        break;
+      }
+    }
+
+    // clear tmp
+    memset(tmp, 0, NR_SIZE);
+  }
+  
+  return p - out;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  int i = 0;
+  va_list arg;
+  va_start(arg, fmt);
+  i = vsprintf(out, fmt, arg);
+  va_end(arg);
+  return i;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,4 +1,3 @@
-#include "am.h"
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdint.h>
