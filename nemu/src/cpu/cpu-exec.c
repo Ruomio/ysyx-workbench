11c5a0cbcb4a1b78c93efb9ec9e40380b1718c65
tracer-ysyx <tracer@ysyx.org>
2024-09-02 15:23:55 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 29 Aug 2024 16:48:57 +0000 x86_64 GNU/Linux  15:23:55 up  6:11,  2 users,  load average: 0.93, 0.66, 0.58

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -19,7 +19,6 @@
 #include <locale.h>
 
 #include "../../src/monitor/sdb/sdb.h"
-#include "ringbuffer.h"
 /* The assembly code of instructions executed is only output to the screen
  * when the number of instructions executed is less than this value.
  * This is useful when you use the `si' command.
