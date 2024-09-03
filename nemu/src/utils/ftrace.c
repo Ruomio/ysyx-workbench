da8bbb8c4869c24bba280409259532f49c45576d
tracer-ysyx <tracer@ysyx.org>
2024-09-03 14:04:40 +0800

    > compile NEMU ysyx_24080020 彭培智 Linux archlinux 6.10.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 29 Aug 2024 16:48:57 +0000 x86_64 GNU/Linux  14:04:40 up 1 day,  4:52,  2 users,  load average: 0.38, 0.45, 0.49

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -28,94 +28,51 @@ void init_ftrace(const char *img_file, const char *ftrace_file) {
   if(!ftrace_file) {
     ftrace_file = "/home/papillon/Documents/All_codes/ysyx-workbench/nemu/build/ftrace-log.txt";
   }
-
   FILE *file = fopen(elf_file, "rb");
   assert(file);
-Elf32_Ehdr header;
-    fread(&header, sizeof(Elf32_Ehdr), 1, file);
 
-    // 确认是32位ELF文件
-    if (header.e_ident[0] != 0x7F || header.e_ident[1] != 'E' || header.e_ident[2] != 'L' || header.e_ident[3] != 'F' ||
-        header.e_ident[4] != 1 || header.e_ident[5] != 0x1) {
-        printf("Not a 32-bit ELF file\n");
-        fclose(file);
-        return ;
-    }
-
-    // 读取节头表
-    fseek(file, header.e_shoff, SEEK_SET);
-    Elf32_Shdr *shdrs = malloc(header.e_shnum * sizeof(Elf32_Shdr));
-    fread(shdrs, sizeof(Elf32_Shdr), header.e_shnum, file);
-
-    // 查找符号表节
-    for (int i = 0; i < header.e_shnum; i++) {
-        if (shdrs[i].sh_type == 2) { // SHT_SYMTAB
-            // 读取符号表
-            fseek(file, shdrs[i].sh_offset, SEEK_SET);
-            Elf32_Sym *syms = malloc(shdrs[i].sh_size);
-            fread(syms, shdrs[i].sh_entsize, shdrs[i].sh_size / sizeof(Elf32_Sym), file);
-
-            // 读取字符串表
-            fseek(file, shdrs[header.e_shstrndx].sh_offset, SEEK_SET);
-            char *strtab = malloc(shdrs[header.e_shstrndx].sh_size);
-            fread(strtab, 1, shdrs[header.e_shstrndx].sh_size, file);
-
-            // 遍历符号表
-            for (int j = 0; j < shdrs[i].sh_size / sizeof(Elf32_Sym); j++) {
-                if (ELF32_ST_TYPE(syms[j].st_info) == 2) { // STT_FUNC
-                    char *name = strtab + syms[j].st_name;
-                    printf("Function: %s, Address: 0x%X\n", name, syms[j].st_value);
-                }
-            }
-
-            free(strtab);
-            free(syms);
-        }
-    }
-
-    free(shdrs);
-    fclose(file);
-  // Elf32_Ehdr ehdr;
-  // fread(&ehdr, sizeof(ehdr), 1, file);
+  Elf32_Ehdr ehdr;
+  fread(&ehdr, sizeof(ehdr), 1, file);
   
-  // // 读取节头
-  // fseek(file, ehdr.e_shoff, SEEK_SET);
-  // Elf32_Shdr *shdrs = malloc(ehdr.e_shnum * sizeof(Elf32_Shdr));
-  // fread(shdrs, sizeof(Elf32_Shdr), ehdr.e_shnum, file);
+  // 读取节头
+  fseek(file, ehdr.e_shoff, SEEK_SET);
+  Elf32_Shdr *shdrs = malloc(ehdr.e_shnum * sizeof(Elf32_Shdr));
+  fread(shdrs, sizeof(Elf32_Shdr), ehdr.e_shnum, file);
 
-  // // 找到符号表和字符串表
-  // int symtab_index = -1;
-  // int strtab_index = -1;
+  // 找到符号表和字符串表
+  int symtab_index = -1;
+  int strtab_index = -1;
 
-  // for (int i = 0; i < ehdr.e_shnum; i++) {
-  //   if (shdrs[i].sh_type == 2) { // SHT_SYMTAB
-  //     symtab_index = i;
-  //   } else if (shdrs[i].sh_type == 3) { // SHT_STRTAB
-  //     strtab_index = i;
-  //   }
-  // }
+  for (int i = 0; i < ehdr.e_shnum; i++) {
+    if (shdrs[i].sh_type == 2) { // SHT_SYMTAB
+      symtab_index = i;
+    } else if (shdrs[i].sh_type == 3) { // SHT_STRTAB
+      strtab_index = i;
+    }
+  }
+  printf("%d %d \n", symtab_index, strtab_index);
 
-  // // 读取符号表
-  // Elf32_Sym *symbols = malloc(shdrs[symtab_index].sh_size);
-  // fseek(file, shdrs[symtab_index].sh_offset, SEEK_SET);
-  // fread(symbols, shdrs[symtab_index].sh_size, 1, file);
+  // 读取符号表
+  Elf32_Sym *symbols = malloc(shdrs[symtab_index].sh_size);
+  fseek(file, shdrs[symtab_index].sh_offset, SEEK_SET);
+  fread(symbols, shdrs[symtab_index].sh_size, 1, file);
 
-  // // 读取字符串表
-  // char *strtab = malloc(shdrs[strtab_index].sh_size);
-  // fseek(file, shdrs[strtab_index].sh_offset, SEEK_SET);
-  // fread(strtab, shdrs[strtab_index].sh_size, 1, file);
+  // 读取字符串表
+  char *strtab = malloc(shdrs[strtab_index].sh_size);
+  fseek(file, shdrs[strtab_index].sh_offset, SEEK_SET);
+  fread(strtab, shdrs[strtab_index].sh_size, 1, file);
 
-  // // 打印函数名和地址
-  // for (int i = 0; i < shdrs[symtab_index].sh_size / sizeof(Elf32_Sym); i++) {
-  //   if (ELF32_ST_TYPE(symbols[i].st_info) == STT_FUNC) {
-  //     printf("Offset: %x, Index: %u ,Function: %10s, Address: 0x%x    \n",symbols[i].st_size, symbols[i].st_name, (strtab + symbols[i].st_name), symbols[i].st_value);
-  //   }
-  // }
+  // 打印函数名和地址
+  for (int i = 0; i < shdrs[symtab_index].sh_size / sizeof(Elf32_Sym); i++) {
+    if (ELF32_ST_TYPE(symbols[i].st_info) == STT_FUNC) {
+      printf("Offset: %x, Index: %u ,Function: %10s, Address: 0x%x    \n",symbols[i].st_size, symbols[i].st_name, (strtab + symbols[i].st_name), symbols[i].st_value);
+    }
+  }
 
-  // free(shdrs);
-  // free(symbols);
-  // free(strtab);
-  // fclose(file);
+  free(shdrs);
+  free(symbols);
+  free(strtab);
+  fclose(file);
   free(elf_file);
 
   Log("leave init_ftrace");
