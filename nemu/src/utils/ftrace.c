f4472b4416348ec14c63fa0d14b59f664ae52341
tracer-ysyx <tracer@ysyx.org>
2024-09-03 10:39:01 +0800

    > compile NEMU ysyx_100006513 彭培智 Linux archlinux 6.10.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 29 Aug 2024 16:48:57 +0000 x86_64 GNU/Linux  10:39:01 up 1 day,  1:26,  2 users,  load average: 0.42, 0.42, 0.44

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -18,6 +18,7 @@
 #include <elf.h>
 
 void init_ftrace(const char *img_file, const char *ftrace_file) {
+  Log("entry init_ftrace");
   if(!img_file) return;
   if(!ftrace_file) {
     ftrace_file = "/home/papillon/Documents/All_codes/ysyx-workbench/nemu/build/ftrace-log.txt";
@@ -67,4 +68,5 @@ void init_ftrace(const char *img_file, const char *ftrace_file) {
     free(strtab);
     fclose(file);
 
+  Log("leave init_ftrace");
 }
