45a14f91fdc9ce50a483dedc472dcf8237116c70
tracer-ysyx <tracer@ysyx.org>
2024-09-03 14:18:36 +0800

    > compile NEMU ysyx_24080020 彭培智 Linux archlinux 6.10.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 29 Aug 2024 16:48:57 +0000 x86_64 GNU/Linux  14:18:36 up 1 day,  5:06,  2 users,  load average: 1.28, 0.77, 0.63

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -50,6 +50,7 @@ void init_ftrace(const char *img_file, const char *ftrace_file) {
       strtab_index = i;
     }
   }
+  printf("%d %d \n", symtab_index, strtab_index);
   // 读取符号表
   Elf32_Sym *symbols = malloc(shdrs[symtab_index].sh_size);
   fseek(file, shdrs[symtab_index].sh_offset, SEEK_SET);
