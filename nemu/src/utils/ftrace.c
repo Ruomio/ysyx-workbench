2d5b68f4e0d6a68636d61a027b135653d2cac5cb
tracer-ysyx <tracer@ysyx.org>
2024-09-03 20:04:17 +0800

    > compile NEMU ysyx_24080020 彭培智 Linux archlinux 6.10.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 29 Aug 2024 16:48:57 +0000 x86_64 GNU/Linux  20:04:17 up  1:23,  2 users,  load average: 0.73, 0.73, 0.51

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -121,7 +121,7 @@ int ftrace_update(uint32_t pc, uint32_t addr, uint32_t rs1, uint32_t rd) {
       }
 
       // type: call or ret
-      if(rs1 == 0 && rd == 0) {
+      if(rs1 == 1 && rd == 0) {
         top--;
         sprintf(str+idx, "ret  ");
         idx += strlen(str+idx);
