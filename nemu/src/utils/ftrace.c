1a9b8ce9daa6d4a09f3e21a4a963f691c294da88
tracer-ysyx <tracer@ysyx.org>
2024-09-03 14:14:45 +0800

    > compile NEMU ysyx_24080020 彭培智 Linux archlinux 6.10.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 29 Aug 2024 16:48:57 +0000 x86_64 GNU/Linux  14:14:45 up 1 day,  5:02,  2 users,  load average: 0.31, 0.56, 0.56

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -67,7 +67,7 @@ void init_ftrace(const char *img_file, const char *ftrace_file) {
   // 打印函数名和地址
   for (int i = 0; i < shdrs[symtab_index].sh_size / sizeof(Elf32_Sym); i++) {
     if (ELF32_ST_TYPE(symbols[i].st_info) == STT_FUNC) {
-      printf("Offset: %x, Index: %u ,Function: %10s, Address: 0x%x    \n",symbols[i].st_size, symbols[i].st_name, (strtab + symbols[i].st_name), symbols[i].st_value);
+      printf("Function: %10s, Address: 0x%x    \n",(strtab + symbols[i].st_name), symbols[i].st_value);
     }
   }
 
