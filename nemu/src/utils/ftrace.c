b2f9e6d435f282b84544ce39af87813b8f96756e
tracer-ysyx <tracer@ysyx.org>
2024-09-04 09:23:36 +0800

    > compile NEMU ysyx_24080020 彭培智 Linux archlinux 6.10.7-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 29 Aug 2024 16:48:57 +0000 x86_64 GNU/Linux  09:23:36 up 12:50,  2 users,  load average: 1.06, 0.85, 0.72

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -117,6 +117,9 @@ int update_ftrace(uint32_t pc, uint32_t addr, uint32_t rs1, uint32_t rd) {
         top--;
         sprintf(str+idx, "ret  ");
         idx += strlen(str+idx);
+        // function name
+        sprintf(str+idx, "[%s]", Ftrace_tab[i].name);
+        idx += strlen(str+idx);
       }
       else {
         top ++;
@@ -127,11 +130,11 @@ int update_ftrace(uint32_t pc, uint32_t addr, uint32_t rs1, uint32_t rd) {
         }
         sprintf(str+idx, "call ");
         idx += strlen(str+idx);
+        // function name
+        sprintf(str+idx, "[%s@0x%x]", Ftrace_tab[i].name, Ftrace_tab[i].addr);
+        idx += strlen(str+idx);
       }
 
-      // function name
-      sprintf(str+idx, "[%s@0x%x]", Ftrace_tab[i].name, Ftrace_tab[i].addr);
-      idx += strlen(str+idx);
 
 
     }
