6c5348661b508494e6ac25ce738efe77183d5eee
tracer-ysyx <tracer@ysyx.org>
2024-09-09 15:55:22 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.10.8-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 04 Sep 2024 15:16:37 +0000 x86_64 GNU/Linux  15:55:22 up  5:38,  2 users,  load average: 1.16, 0.49, 0.41

--- npc/vsrc/mem.v
+++ npc/vsrc/mem.v
@@ -11,7 +11,7 @@ module ysyx_24080020_MEM(
 
     reg [`ysyx_24080020_WIDTH-1:0] mrdata_reg;
 
-    reg [`ysyx_24080020_WIDTH-1:0] mem [32:0];     // 4KB
+    reg [`ysyx_24080020_WIDTH-1:0] mem[32:0];     // 4KB
 
     always @(posedge clk) begin
         if(!clk) begin
