1378d9f8c93aed38a01560ea1ae0372cbff3703e
tracer-ysyx <tracer@ysyx.org>
2024-09-09 12:58:01 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.10.8-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 04 Sep 2024 15:16:37 +0000 x86_64 GNU/Linux  12:58:01 up  2:40,  2 users,  load average: 0.36, 0.26, 0.19

--- npc/vsrc/reg.v
+++ npc/vsrc/reg.v
@@ -9,7 +9,7 @@ module ysyx_24080020_REG #(
     input [4:0] raddr2,
 
     input wen,
-    input [31:0] waddr,
+    input [4:0] waddr,
     input [31:0] wdata,
 
     output reg [`ysyx_24080020_WIDTH-1:0] val_raddr1,
