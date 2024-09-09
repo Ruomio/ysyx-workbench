eab3b0935febfbd75d96ab7e720e2fc55d9f6f99
tracer-ysyx <tracer@ysyx.org>
2024-09-09 11:08:00 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.10.8-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 04 Sep 2024 15:16:37 +0000 x86_64 GNU/Linux  11:08:00 up 50 min,  2 users,  load average: 0.42, 0.30, 0.28

--- npc/vsrc/npc.v
+++ npc/vsrc/npc.v
@@ -27,7 +27,7 @@ module ysyx_24080020_NPC(
 
     ysyx_24080020_IDU idu(.inst(inst), .opcode(opcode), .rd(rd), .funct3(funct3), .rs1(rs1), .rs2(rs2), .imm(imm), .funct7(funct7));
 
-    ysyx_24080020_REG u_reg(.clk(clk), .rst(rst), .radd1(rs1), .radd2(rs2), .wen(wen), .waddr(waddr), .wdata(wdata), .val_raddr1(scr1), .val_raddr2(src2));
+    ysyx_24080020_REG u_reg(.clk(clk), .rst(rst), .radd1(rs1), .radd2(rs2), .wen(wen), .waddr(waddr), .wdata(wdata), .val_raddr1(src1), .val_raddr2(src2));
 
     ysyx_24080020_EXU exu(.opcode(opcode), .funct3(funct3), .val_raddr1(src1), .val_raddr2(src2), .imm(imm), .waddr(waddr), .wdata(wdata), .wen(wen), .pc(snpc));
 
