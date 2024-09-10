774a0105edc9cfee456d6ff7c6473d911fb75f71
tracer-ysyx <tracer@ysyx.org>
2024-09-10 16:23:43 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.10.8-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 04 Sep 2024 15:16:37 +0000 x86_64 GNU/Linux  16:23:43 up 18:27,  2 users,  load average: 1.04, 1.03, 1.00

--- npc/csrc/include/define.h
+++ npc/csrc/include/define.h
@@ -1,17 +1,10 @@
 #ifndef __DEFINE_H__
 #define __DEFINE_H__
 
+#include <getopt.h>
+
 #define MSIZE 0x40000000     // 4G 2^31
 
-struct option
-{
-  const char *name;
-  /* has_arg can't be an enum because some compilers complain about
-     type mismatches in all the code that assumes it is an int.  */
-  int has_arg;
-  int *flag;
-  int val;
-};
 
 #define no_argument		0
 #define required_argument	1
