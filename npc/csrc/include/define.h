6dd3adaa98aafcfbcebcfa2b299cf4e0f3dbf07a
tracer-ysyx <tracer@ysyx.org>
2024-09-10 16:25:25 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.10.8-arch1-1 #1 SMP PREEMPT_DYNAMIC Wed, 04 Sep 2024 15:16:37 +0000 x86_64 GNU/Linux  16:25:25 up 18:28,  2 users,  load average: 0.86, 1.00, 0.99

--- npc/csrc/include/define.h
+++ npc/csrc/include/define.h
@@ -10,7 +10,14 @@
 #define required_argument	1
 #define optional_argument	2
 
-extern int getopt_long (int ___argc, char *__getopt_argv_const *___argv, const char *__shortopts, const struct option *__longopts, int *__longind) __THROW __nonnull ((2, 3));
-extern int getopt_long_only (int ___argc, char *__getopt_argv_const *___argv, const char *__shortopts, const struct option *__longopts, int *__longind) __THROW __nonnull ((2, 3));
+extern int getopt_long (int ___argc, char *__getopt_argv_const *___argv, 
+                const char *__shortopts, 
+                const struct option *__longopts, int *__longind) 
+                __THROW __nonnull ((2, 3));
+
+extern int getopt_long_only (int ___argc, char *__getopt_argv_const *___argv, 
+                const char *__shortopts, 
+                const struct option *__longopts, int *__longind) 
+                __THROW __nonnull ((2, 3));
 
 #endif
