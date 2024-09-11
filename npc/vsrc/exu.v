5f5d4f53c3d3af6e84a674353d06b3bdadd7707e
tracer-ysyx <tracer@ysyx.org>
2024-09-11 20:43:13 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.10.9-arch1-2 #1 SMP PREEMPT_DYNAMIC Tue, 10 Sep 2024 14:37:32 +0000 x86_64 GNU/Linux  20:43:13 up  1:29,  2 users,  load average: 0.67, 0.59, 0.48

--- npc/vsrc/exu.v
+++ npc/vsrc/exu.v
@@ -31,10 +31,10 @@ module ysyx_24080020_EXU
 
     always @(*) begin
         // initial
-        flag = 1'b0;
         wdata = 32'b0;
         waddr = 5'b0;
         wen = 1'b0;
+        flag = 1'b0;
         mwen = 1'b0;
         maddr = 32'b0;
         mdata = 32'b0;
