e6aa54764bd938f38b70459e369ce6b275c3b95a
tracer-ysyx <tracer@ysyx.org>
2024-09-11 16:17:24 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.10.9-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Sep 2024 02:38:45 +0000 x86_64 GNU/Linux  16:17:24 up 20:12,  2 users,  load average: 0.99, 0.64, 0.58

--- npc/vsrc/idu.v
+++ npc/vsrc/idu.v
@@ -13,24 +13,27 @@ module ysyx_24080020_IDU (
     import "DPI-C" function void ebreak();
     import "DPI-C" function void invalid_inst();
 
+    reg [`ysyx_24080020_WIDTH-1:0] imm_reg;
+
     assign opcode = inst[`ysyx_24080020_OPCODE];
     assign rd = inst[`ysyx_24080020_RD];
     assign funct3 = inst[`ysyx_24080020_FUNCT3];
     assign rs1 = inst[`ysyx_24080020_RS1];
     assign rs2 = inst[`ysyx_24080020_RS2];
+    assign imm = imm_reg;
 
     always @(*) begin
         case(opcode)
             `ysyx_24080020_I_TYPE, `ysyx_24080020_JALR: begin
-                imm = {{20{inst[31]}}, inst[`ysyx_24080020_IMM_I]};
+                imm_reg = {{20{inst[31]}}, inst[`ysyx_24080020_IMM_I]};
             end
 
             `ysyx_24080020_AUIPC, `ysyx_24080020_LUI: begin
-                imm = {inst[`ysyx_24080020_IMM_U], {12{1'b0}}};
+                imm_reg = {inst[`ysyx_24080020_IMM_U], {12{1'b0}}};
             end
 
             `ysyx_24080020_JAL: begin
-                imm = {{12{inst[31]}}, inst[19:12], inst[20], inst[30:21], 1'b0};
+                imm_reg = {{12{inst[31]}}, inst[19:12], inst[20], inst[30:21], 1'b0};
             end
 
             `ysyx_24080020_EBREAK: begin
@@ -38,15 +41,15 @@ module ysyx_24080020_IDU (
             end
 
             `ysyx_24080020_S_TYPE: begin
-                imm = {inst[31:25], {25{1'b0}}};
+                imm_reg = {inst[31:25], {25{1'b0}}};
             end
 
             7'b0000000 : begin
                 // rst
-                imm = 32'b0;
+                imm_reg = 32'b0;
             end
             default: begin
-                imm = 32'b0;
+                imm_reg = 32'b0;
                 invalid_inst();
             end
         endcase
