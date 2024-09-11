ed85c96552d56f0beb55fede9a3992d8ad13f131
tracer-ysyx <tracer@ysyx.org>
2024-09-11 10:06:21 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.10.9-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Sep 2024 02:38:45 +0000 x86_64 GNU/Linux  10:06:21 up 14:01,  2 users,  load average: 1.18, 0.65, 0.59

--- npc/csrc/memory.c
+++ npc/csrc/memory.c
@@ -27,8 +27,8 @@ void init_memory(const svOpenArrayHandle memory) {
 
   printf("memory addr is %p\n", memory);
   // 将 svOpenArrayHandle 转换为指针类型
-  unsigned int *memory_ptr = (unsigned int *)svGetArrayPtr(memory);
-  assert(memory_ptr != NULL);
+  // unsigned int *memory_ptr = (unsigned int *)svGetArrayPtr(memory);
+  // assert(memory_ptr != NULL);
   int ret = fread(memory, size, 1, fp);
   assert(ret == 1);
 
