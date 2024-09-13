0407645a0a8e3ed07281e8547662493520c5ada3
tracer-ysyx <tracer@ysyx.org>
2024-09-13 09:42:02 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.10.9-arch1-2 #1 SMP PREEMPT_DYNAMIC Tue, 10 Sep 2024 14:37:32 +0000 x86_64 GNU/Linux  09:42:02 up 1 day, 14:27,  2 users,  load average: 1.08, 0.97, 0.87

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -79,7 +79,7 @@ void exec_all_npc() {
 void exec_npc(int n) {
   switch (u_npc_state.state) {
     case NPC_END: case NPC_ABORT:
-      printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
+      printf("Program execution has ended. To restart the program, exit NPC and run again.\n");
       return;
     default: u_npc_state.state = NPC_RUNNING;
   }
