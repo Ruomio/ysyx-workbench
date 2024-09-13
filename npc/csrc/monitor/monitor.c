a4d4e7b728bf832b8c83c624a4bfaca0fbcaf456
tracer-ysyx <tracer@ysyx.org>
2024-09-13 14:23:13 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.10.9-arch1-2 #1 SMP PREEMPT_DYNAMIC Tue, 10 Sep 2024 14:37:32 +0000 x86_64 GNU/Linux  14:23:13 up 1 day, 19:09,  2 users,  load average: 0.46, 0.52, 0.47

--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -24,7 +24,7 @@ void init_mem();
 void init_difftest(char *ref_so_file, long img_size, int port);
 void init_device();
 void init_sdb();
-// void init_disasm(const char *triple);
+extern "C" void init_disasm(const char *triple);
 IFDEF(CONFIG_FTRACE_COND, void init_ftrace(const char *img_file, const char *ftrace_file));
 
 static void welcome() {
