7d4c84eb411a919f38367bcf6c6f10fd08927609
tracer-ysyx <tracer@ysyx.org>
2024-09-13 14:29:05 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.10.9-arch1-2 #1 SMP PREEMPT_DYNAMIC Tue, 10 Sep 2024 14:37:32 +0000 x86_64 GNU/Linux  14:29:05 up 1 day, 19:14,  2 users,  load average: 0.78, 0.57, 0.49

--- npc/include/isa.h
+++ npc/include/isa.h
@@ -19,8 +19,6 @@
 // Located at src/isa/$(GUEST_ISA)/include/isa-def.h
 #include <isa-def.h>
 
-extern "C" void init_disasm(const char *triple);
-
 
 // The macro `__GUEST_ISA__` is defined in $(CFLAGS).
 // It will be expanded as "x86" or "mips32" ...
