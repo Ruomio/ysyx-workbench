27709ec1fca93f51da71eddca734d2ad4b5a20a2
tracer-ysyx <tracer@ysyx.org>
2024-09-13 13:09:39 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.10.9-arch1-2 #1 SMP PREEMPT_DYNAMIC Tue, 10 Sep 2024 14:37:32 +0000 x86_64 GNU/Linux  13:09:39 up 1 day, 17:55,  2 users,  load average: 1.15, 0.77, 0.59

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -1,8 +1,9 @@
 #include <cstdint>
 #include <readline/chardefs.h>
+#include "Vtop.h"
 #include "define.h"
 #include "verilated_vcd_c.h"
-#include "svdpi.h"
+// #include "svdpi.h"
 #include "Vtop__Dpi.h"
 
 
