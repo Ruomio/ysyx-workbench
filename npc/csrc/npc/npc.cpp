b756e104dc0a2dcc46e09cc10b5a403226532cb1
tracer-ysyx <tracer@ysyx.org>
2024-09-14 16:10:22 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.10.9-arch1-2 #1 SMP PREEMPT_DYNAMIC Tue, 10 Sep 2024 14:37:32 +0000 x86_64 GNU/Linux  16:10:22 up 2 days, 20:56,  2 users,  load average: 1.15, 0.85, 0.67

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -182,7 +182,7 @@ void free_npc() {
 }
 
 void update_ftrace_dpi() {
-  IFDEF(CONFIG_FTRACE, update_ftrace(g_get_pc(), g_get_dnpc(), g_get_rs1(), g_get_rd()));
+  IFDEF(CONFIG_FTRACE, update_ftrace(last_pc, g_get_dnpc(), g_get_rs1(), g_get_rd()));
 }
 static void close_ftace() {
   IFDEF(CONFIG_FTRACE, close_ftrace());
