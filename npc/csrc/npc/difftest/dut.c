7e5a4490c54106382db1f59b2e11077b063f6b64
tracer-ysyx <tracer@ysyx.org>
2024-09-18 13:31:04 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  13:31:04 up 16:43,  2 users,  load average: 0.58, 0.87, 0.95

--- npc/csrc/npc/difftest/dut.c
+++ npc/csrc/npc/difftest/dut.c
@@ -14,16 +14,24 @@
 ***************************************************************************************/
 
 #include <dlfcn.h>
-
+#include <difftest-def.h>
 #include <isa.h>
 #include <memory/paddr.h>
 #include <debug.h>
 
 
-void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
-void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
-void (*ref_difftest_exec)(uint64_t n) = NULL;
-void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
+typedef void (*ref_difftest_memcpy_type)(paddr_t addr, void *buf, size_t n, bool direction);
+typedef void (*ref_difftest_regcpy_type)(void *dut, bool direction);
+typedef void (*ref_difftest_exec_type)(uint64_t n);
+typedef void (*ref_difftest_raise_intr_type)(uint64_t NO);
+ref_difftest_memcpy_type ref_difftest_memcpy = NULL;
+ref_difftest_regcpy_type ref_difftest_regcpy = NULL;
+ref_difftest_exec_type ref_difftest_exec = NULL;
+ref_difftest_raise_intr_type ref_difftest_raise_intr = NULL;
+// void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
+// void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
+// void (*ref_difftest_exec)(uint64_t n) = NULL;
+// void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 
 #ifdef CONFIG_DIFFTEST
 #ifdef __cplusplus
@@ -68,19 +76,21 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   handle = dlopen(ref_so_file, RTLD_LAZY);
   assert(handle);
 
-  ref_difftest_memcpy = dlsym(handle, "difftest_memcpy");
+  ref_difftest_memcpy = (ref_difftest_memcpy_type)dlsym(handle, "difftest_memcpy");
   assert(ref_difftest_memcpy);
 
-  ref_difftest_regcpy = dlsym(handle, "difftest_regcpy");
+  ref_difftest_regcpy = (ref_difftest_regcpy_type)dlsym(handle, "difftest_regcpy");
   assert(ref_difftest_regcpy);
 
-  ref_difftest_exec = dlsym(handle, "difftest_exec");
+  ref_difftest_exec = (ref_difftest_exec_type)dlsym(handle, "difftest_exec");
   assert(ref_difftest_exec);
 
-  ref_difftest_raise_intr = dlsym(handle, "difftest_raise_intr");
+  ref_difftest_raise_intr = (ref_difftest_raise_intr_type)dlsym(handle, "difftest_raise_intr");
   assert(ref_difftest_raise_intr);
 
-  void (*ref_difftest_init)(int) = dlsym(handle, "difftest_init");
+  typedef void (*ref_difftest_init_type)(int n);
+  ref_difftest_init_type ref_difftest_init = NULL;
+  ref_difftest_init = (ref_difftest_init_type)dlsym(handle, "difftest_init");
   assert(ref_difftest_init);
 
   Log("Differential testing: %s", ANSI_FMT("ON", ANSI_FG_GREEN));
