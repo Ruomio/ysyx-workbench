4ea63a96c746e59e0724eab1332a5a4e1db90ebe
tracer-ysyx <tracer@ysyx.org>
2024-09-18 12:58:38 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  12:58:38 up 16:11,  2 users,  load average: 0.94, 0.86, 0.75

--- npc/csrc/npc/difftest/dut.c
+++ npc/csrc/npc/difftest/dut.c
@@ -17,7 +17,8 @@
 
 #include <isa.h>
 #include <memory/paddr.h>
-#include <utils.h>
+#include <debug.h>
+
 
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
