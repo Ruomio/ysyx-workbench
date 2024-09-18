d299751890e5475c691d35d93f0f53aaab97ae7f
tracer-ysyx <tracer@ysyx.org>
2024-09-18 10:02:49 +0800

    > compile NEMU ysyx_24080020 彭培智 Linux archlinux 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  10:02:49 up 13:15,  2 users,  load average: 0.94, 0.81, 0.76

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -19,15 +19,17 @@
 #include <memory/paddr.h>
 
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+  if(direction == DIFFTEST_TO_REF) likely(in_pmem(addr)) ? memcpy(buf, guest_to_host(addr), n) : assert(0);
+  else likely(in_pmem(addr)) ? memcpy(guest_to_host(addr), buf, n) : assert(0);
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+  if(direction == DIFFTEST_TO_REF) dut != NULL ? memcpy(dut, cpu.gpr, 32 * 4) : assert(0);
+  else dut != NULL ? memcpy(cpu.gpr, dut, 32 * 4) : assert(0);
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
-  assert(0);
+  cpu_exec(n);
 }
 
 __EXPORT void difftest_raise_intr(word_t NO) {
