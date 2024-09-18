91a32907777e98a14cec5be6c049ac8df5a1a3e9
tracer-ysyx <tracer@ysyx.org>
2024-09-18 16:09:08 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  16:09:08 up 19:21,  2 users,  load average: 1.27, 1.01, 0.91

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -51,6 +51,7 @@ uint32_t g_get_snpc();
 uint32_t g_get_dnpc();
 uint32_t g_get_rs1();
 uint32_t g_get_rd();
+void update_npc_cpu();
 
 static void trace_and_difftest(vaddr_t dnpc) {
 #ifdef CONFIG_ITRACE_COND
