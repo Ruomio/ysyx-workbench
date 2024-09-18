444d9dbe4fcb658c63a14ec27cb642b1e461d05d
tracer-ysyx <tracer@ysyx.org>
2024-09-18 16:52:22 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  16:52:22 up 20:05,  2 users,  load average: 0.94, 0.95, 0.94

--- npc/csrc/monitor/sdb/sdb.c
+++ npc/csrc/monitor/sdb/sdb.c
@@ -59,7 +59,6 @@ static int cmd_c(char *args) {
 
 static int cmd_q(char *args) {
   u_npc_state.state = NPC_QUIT;
-  free_npc();
   free_memory();
   return -1;
 }
@@ -244,8 +243,6 @@ void sdb_set_batch_mode() {
 }
 
 void sdb_mainloop() {
-  /* Init NPC */
-  init_npc();
 
   if (is_batch_mode) {
     cmd_c(NULL);
