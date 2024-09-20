782330605ad9a43f3213700c24eda822750e11d9
tracer-ysyx <tracer@ysyx.org>
2024-09-20 14:35:02 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  14:35:02 up 2 days, 17:47,  2 users,  load average: 0.74, 0.78, 0.86

--- npc/vsrc/idu.v
+++ npc/vsrc/idu.v
@@ -40,7 +40,7 @@ module ysyx_24080020_IDU (
 
                 `ysyx_24080020_AUIPC, `ysyx_24080020_LUI: begin
                     imm = {inst[`ysyx_24080020_IMM_U], {12{1'b0}}};
-                    imm = 32'100;
+                    imm = 32'b100;
                 end
 
                 `ysyx_24080020_JAL: begin
