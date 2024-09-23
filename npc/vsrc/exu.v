55528eee93aa6910a15d09aeaa6f429bd43e0038
tracer-ysyx <tracer@ysyx.org>
2024-09-23 14:49:58 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  14:49:58 up 13 min,  2 users,  load average: 0.64, 0.82, 0.57

--- npc/vsrc/exu.v
+++ npc/vsrc/exu.v
@@ -68,7 +68,7 @@ module ysyx_24080020_EXU
                         wdata = val_raddr1 << imm[4:0];
                     end
                     `ysyx_24080020_SRI: begin
-                        wdata = imm[10] == 0 ? val_raddr1 >> imm[4:0] : val_raddr1 >>> imm[4:0];
+                        wdata = imm[10] == 0 ? val_raddr1 >> imm[4:0] : val_raddr1 >> imm[4:0] | ({32{val_raddr1[31]}} & ~(32'hffffffff >> imm[4:0]));
                     end
                     
                     default: begin
