f11131a388b007fb13128b9443d92d85ba0dddaa
tracer-ysyx <tracer@ysyx.org>
2024-09-23 17:42:50 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  17:42:50 up  3:05,  2 users,  load average: 0.65, 0.54, 0.43

--- npc/vsrc/reg.v
+++ npc/vsrc/reg.v
@@ -1,4 +1,4 @@
-`include "vsrc/define.v"
+`include "/home/papillon/Documents/All_codes/ysyx-workbench/npc/vsrc/define.v"
 module ysyx_24080020_REG #(
     parameter WIDTH = 32
 ) (
@@ -12,8 +12,8 @@ module ysyx_24080020_REG #(
     input [4:0] waddr,
     input [31:0] wdata,
 
-    output reg [`ysyx_24080020_WIDTH-1:0] val_raddr1,
-    output reg [`ysyx_24080020_WIDTH-1:0] val_raddr2
+    output [`ysyx_24080020_WIDTH-1:0] val_raddr1,
+    output [`ysyx_24080020_WIDTH-1:0] val_raddr2
 );
 
     reg [WIDTH-1:0] regs[WIDTH-1:0];
