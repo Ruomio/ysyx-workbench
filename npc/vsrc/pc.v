431fc860fa56bc10f5a90529712abe399f446e46
tracer-ysyx <tracer@ysyx.org>
2024-09-23 14:01:15 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  14:01:15 up  6:43,  2 users,  load average: 0.32, 0.55, 0.68

--- npc/vsrc/pc.v
+++ npc/vsrc/pc.v
@@ -10,7 +10,7 @@ module ysyx_24080020_PC (
     output [3:0] pc_len
 );
     // reg [`ysyx_24080020_WIDTH-1:0] tmp_pc;
-    reg [1:0] cnt;
+    reg cnt;
 
     // assign pc = !rst ? `ysyx_24080020_WIDTH : (is_dnpc ? dnpc : snpc);
     assign pc_len = 4'b100;
@@ -18,16 +18,16 @@ module ysyx_24080020_PC (
     always @(posedge clk) begin
         if(!rst) begin
             pc <= `ysyx_24080020_MBASE;
-            cnt <= 2'b0;
+            cnt <= 1'b0;
         end
-        else if(cnt == 2'b01) begin
+        else if(cnt == 1'b1) begin
             if(is_dnpc) pc <= dnpc;
             else pc <= snpc;
 
-            cnt <= 2'b0;
+            cnt <= 1'b0;
         end
         else begin
-            cnt <= cnt + 2'b1;
+            cnt <= cnt + 1'b1;
         end
 
     end
