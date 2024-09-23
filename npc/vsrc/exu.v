19c9837bc8bf98e3af22caf42ef06b8435408d44
tracer-ysyx <tracer@ysyx.org>
2024-09-23 13:48:58 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  13:48:58 up  6:31,  2 users,  load average: 0.58, 0.72, 0.80

--- npc/vsrc/exu.v
+++ npc/vsrc/exu.v
@@ -33,13 +33,13 @@ module ysyx_24080020_EXU
     output reg [3:0] mwlen
 );
 
-    reg flag;
+    // reg flag;
 
-    assign is_dnpc = flag;
+    // assign is_dnpc = flag;
 
     always @(inst or mrdata) begin
         // initial
-        flag = 1'b0;
+        is_dnpc = 1'b0;
         mwen = 1'b0;
         wen = 1'b0;
         mraddr = 32'b0;
@@ -173,7 +173,7 @@ module ysyx_24080020_EXU
             end
 
             `ysyx_24080020_B_TYPE: begin
-                flag = 1'b1;
+                is_dnpc = 1'b1;
                 case(funct3)
                     `ysyx_24080020_BEQ: begin
                         dnpc = val_raddr1 == val_raddr2 ? pc + imm : pc + 4;
@@ -195,7 +195,7 @@ module ysyx_24080020_EXU
                     end
 
                     default: begin
-                        flag = 1'b0;
+                        is_dnpc = 1'b0;
                     end
                 endcase
             end
