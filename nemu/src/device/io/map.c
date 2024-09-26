1441145ad5a9a962b4c4c19fb26036392ec208bc
tracer-ysyx <tracer@ysyx.org>
2024-09-26 15:54:17 +0800

    > compile NEMU ysyx_24080020 彭培智 Linux archlinux 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  15:54:17 up  3:01,  2 users,  load average: 1.36, 1.84, 2.19

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -107,7 +107,9 @@ static void dtrace_update(IOMap map, int len, uint32_t data, bool iswrite) {
 
   memset(dtrace_buf[db_idx], 0, sizeof(dtrace_buf[db_idx]));
   memset(dtrace_buf[db_idx], ' ', 3);
-  sprintf(dtrace_buf[(db_idx++)%NR_DB] + 3, "Device:%s,    W/R:%s,    LEN:%d,    DATA:0x%u\n", map.name, iswrite ? "write" : "read", len, data);
+  sprintf(dtrace_buf[db_idx] + 3, "Device:%s,    W/R:%s,    LEN:%d,    DATA:0x%u\n", map.name, iswrite ? "write" : "read", len, data);
+
+  db_idx = (db_idx + 1) % NR_DB;
 }
 
 #endif
