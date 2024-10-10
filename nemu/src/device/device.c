12df90ce2489d09163a436ad32326f2041c7e8e9
tracer-ysyx <tracer@ysyx.org>
2024-10-10 20:30:23 +0800

    > compile NEMU ysyx_24080020 彭培智 Linux archlinux 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  20:30:23 up  7:04,  2 users,  load average: 0.54, 0.45, 0.43

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -49,11 +49,11 @@ void device_update() {
     switch (event.type) {
       case SDL_QUIT:
         nemu_state.state = NEMU_QUIT;
+  printf("line show.\n");
         break;
 #ifdef CONFIG_HAS_KEYBOARD
       // If a key was pressed
       case SDL_KEYDOWN:
-  printf("line show.\n");
       case SDL_KEYUP: {
         uint8_t k = event.key.keysym.scancode;
         bool is_keydown = (event.key.type == SDL_KEYDOWN);
