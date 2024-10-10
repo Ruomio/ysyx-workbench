5153e255ef54d07c6ca26559065f2551cfc9b144
tracer-ysyx <tracer@ysyx.org>
2024-10-10 20:36:20 +0800

    > compile NEMU ysyx_24080020 彭培智 Linux archlinux 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  20:36:20 up  7:10,  2 users,  load average: 0.92, 0.61, 0.50

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -46,7 +46,7 @@ void device_update() {
 #ifndef CONFIG_TARGET_AM
   SDL_Event event;
   while (SDL_PollEvent(&event)) {
-  printf("line show.\tevent.type = %d\n", event.type);
+  printf("line show.\tevent.type = 0x%x\n", event.type);
     switch (event.type) {
       case SDL_QUIT:
         nemu_state.state = NEMU_QUIT;
