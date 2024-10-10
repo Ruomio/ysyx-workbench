518f603addbe25f863eb8ab03b9abd73b7fbdbc2
tracer-ysyx <tracer@ysyx.org>
2024-10-10 20:24:57 +0800

    > compile NEMU ysyx_24080020 彭培智 Linux archlinux 6.10.10-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 12 Sep 2024 17:21:02 +0000 x86_64 GNU/Linux  20:24:57 up  6:59,  2 users,  load average: 0.28, 0.25, 0.39

--- nemu/src/device/keyboard.c
+++ nemu/src/device/keyboard.c
@@ -73,8 +73,8 @@ void send_key(uint8_t scancode, bool is_keydown) {
   if (nemu_state.state == NEMU_RUNNING && keymap[scancode] != _KEY_NONE) {
     uint32_t am_scancode = keymap[scancode] | (is_keydown ? KEYDOWN_MASK : 0);
     key_enqueue(am_scancode);
-  printf("line show.\n");
   }
+  printf("line show.\n");
 }
 #else // !CONFIG_TARGET_AM
 #define _KEY_NONE 0
