715fd6ebc4b6cbe71e8dee928bbbc1f7780271ce
tracer-ysyx <tracer@ysyx.org>
2024-10-16 11:23:23 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.56-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 10 Oct 2024 12:04:53 +0000 x86_64 GNU/Linux  11:23:23 up  1:01,  2 users,  load average: 0.73, 0.80, 0.79

--- npc/csrc/device/io/map.c
+++ npc/csrc/device/io/map.c
@@ -13,6 +13,7 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
+#include <cstdint>
 #include <isa.h>
 #include <memory/memory.h>
 #include <memory/vaddr.h>
@@ -57,7 +58,7 @@ static void invoke_callback(io_callback_t c, paddr_t offset, int len, bool is_wr
 }
 
 void init_map() {
-  io_space = malloc(IO_SPACE_MAX);
+  io_space = (uint8_t *)malloc(IO_SPACE_MAX);
   assert(io_space);
   p_space = io_space;
 }
