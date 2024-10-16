21217e9b285d8a72a1e8e8d18df995cf3f25341a
tracer-ysyx <tracer@ysyx.org>
2024-10-16 16:32:37 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.56-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 10 Oct 2024 12:04:53 +0000 x86_64 GNU/Linux  16:32:37 up 5 min,  2 users,  load average: 1.24, 1.07, 0.50

--- npc/vsrc/npc.v
+++ npc/vsrc/npc.v
@@ -42,7 +42,7 @@ module ysyx_24080020_NPC(
     ysyx_24080020_PC u_pc(
         .clk(clk), 
         .rst(rst), 
-        .snpc(snpc), 
+        // .snpc(snpc), 
         .is_dnpc(is_dnpc), 
         .dnpc(dnpc), 
         .pc(pc), 
--- npc/vsrc/pc.v
+++ npc/vsrc/pc.v
@@ -2,33 +2,23 @@
 module ysyx_24080020_PC (
     input clk,
     input rst,
-    input [`ysyx_24080020_WIDTH-1:0] snpc,
     input is_dnpc,
     input [`ysyx_24080020_WIDTH-1:0] dnpc,
 
     output reg [`ysyx_24080020_WIDTH-1:0] pc,
     output [3:0] pc_len
 );
-    reg cnt;
 
-    assign pc_len = 4'b100;
+  assign pc_len = 4'b100;
 
-    always @(posedge clk) begin
-        if(!rst) begin
-            pc <= `ysyx_24080020_MBASE;
-            cnt <= 1'b0;
-        end
-        else if(cnt == 1'b1) begin
-            if(is_dnpc) pc <= dnpc;
-            else pc <= snpc;
-
-            cnt <= 1'b0;
-        end
-        else begin
-            cnt <= cnt + 1'b1;
-        end
-
+  always @(posedge clk) begin
+    if (!rst) begin
+      pc <= `ysyx_24080020_MBASE;
+    end else begin
+      if (is_dnpc) pc <= dnpc;
+      else pc <= pc + 32'b100;
     end
+  end
 
 
 endmodule
