90c45b8ae6cd1d61da4883327802c29627fd7b7d
tracer-ysyx <tracer@ysyx.org>
2024-10-16 11:41:22 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.56-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 10 Oct 2024 12:04:53 +0000 x86_64 GNU/Linux  11:41:22 up 4 min,  2 users,  load average: 0.16, 0.31, 0.17

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -96,7 +96,7 @@ int read_memory(int addr, int len) {
   if(!in_pmem(addr)) {
     out_of_bound(addr);
     u_npc_state.state = NPC_ABORT;
-    u_npc_state.ret = true;
+    u_npc_state.ret = false;
     return 0;
   }
   printf("addr = 0x%x\n", addr);
