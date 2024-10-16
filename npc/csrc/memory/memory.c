571b4864299938be9159f76bfbe6e8a5a1e9dddf
tracer-ysyx <tracer@ysyx.org>
2024-10-16 16:48:46 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.56-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 10 Oct 2024 12:04:53 +0000 x86_64 GNU/Linux  16:48:46 up 21 min,  2 users,  load average: 0.60, 0.64, 0.57

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -101,12 +101,13 @@ static void pmem_write(paddr_t addr, int len, word_t data) {
   host_write(guest_to_host(addr), len, data);
 }
 
+static uint64_t u_time;
 int read_memory(int addr, int len) {
   IFDEF(CONFIG_MTRACE_COND, MtraceBuf_write(addr, len, 0));
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
   IFDEF(CONFIG_DEVICE,
-    // if(addr == 0xa0000048) { return (uint32_t)get_time(); }
-    // else if(addr == 0xa0000048 + 0x4) { return get_time() >> 32;}
+    if(addr == 0xa0000048 + 4) { u_time = get_time(); return u_time >> 32;}
+    else if(addr == 0xa0000048) { return (uint32_t)u_time;}
     // return mmio_read(addr, len)
     return 0;
   );
