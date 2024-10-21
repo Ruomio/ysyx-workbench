c7cc04ad1069242d540295b312f79b173df2969a
24080020-Peizhi Peng <1065940593@qq.com>
2024-10-21 13:23:14 +0800

    update nemuu to achive ecall

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -38,8 +38,22 @@ enum {
 #define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i, 30, 21) << 1) | (BITS(i, 20, 20) << 11) | (BITS(i, 19, 12) << 12); } while(0)
 #define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | (BITS(i, 30, 25) << 5) | (BITS(i, 11, 8) << 1) | (BITS(i, 7, 7) << 11); } while(0)
 
+#define imm() BITS(s->isa.inst.val, 31, 20)
+#define CSR(i) *get_csr_reg(i) 
+#define ECALL() do { \
+    IFDEF(__riscv_e, bool success;  isa_raise_intr(isa_reg_str2val("a5", &success), s->pc);  return;) \
+    bool success; isa_raise_intr(isa_reg_str2val("a7", &success), s->pc); \
+} while(0)
+
 IFDEF(CONFIG_FTRACE_COND, int update_ftrace(uint32_t pc, uint32_t addr, uint32_t rs1, uint32_t rd));
 IFDEF(CONFIG_FTRACE_COND, int close_ftrace());
+static vaddr_t *get_csr_reg(word_t csr) {
+    if(csr == 0x341) {return &cpu.csrs[mepc];} 
+    else if(csr == 0x300) {return &cpu.csrs[mstatus];} 
+    else if(csr == 0x342) {return &cpu.csrs[mcause];} 
+    else if(csr == 0x305) {return &cpu.csrs[mtvec];} 
+    else { Assert(0, "Unknown csr reg."); }
+}
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
@@ -117,14 +131,14 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000??? ????? 00000 000 00000 00011 11", fence  , N, );       // sync mem and i/o
   INSTPAT("0000000 00000 00000 001 00000 00011 11", fence.i, N, );       // sync insts
 
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, );
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, ECALL(); /*assert(0)*/ );
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10)); IFDEF(CONFIG_FTRACE_COND, close_ftrace())); // R(10) is $a0
-  INSTPAT("??????? ????? ????? 001 00000 11100 11", csrrw  , I, );
-  INSTPAT("??????? ????? ????? 010 00000 11100 11", csrrs  , I, );
-  INSTPAT("??????? ????? ????? 011 00000 11100 11", csrrc  , I, );
-  INSTPAT("??????? ????? ????? 101 00000 11100 11", csrrw  , I, );
-  INSTPAT("??????? ????? ????? 110 00000 11100 11", csrrsi , I, );
-  INSTPAT("??????? ????? ????? 111 00000 11100 11", csrrci , I, );
+  INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , I, R(rd) = CSR(imm()); CSR(imm()) = src1; /*assert(0)*/ );
+  INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , I, R(rd) = CSR(imm()); CSR(imm()) = CSR(imm()) | src1; assert(0) );
+  INSTPAT("??????? ????? ????? 011 ????? 11100 11", csrrc  , I, );
+  INSTPAT("??????? ????? ????? 101 ????? 11100 11", csrrw  , I, );
+  INSTPAT("??????? ????? ????? 110 ????? 11100 11", csrrsi , I, );
+  INSTPAT("??????? ????? ????? 111 ????? 11100 11", csrrci , I, );
 
   /* RV32M */
   INSTPAT("0000001 ????? ????? 000 ????? 01100 11", mul    , R, R(rd) = (int)src1 * (int)src2);
--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -19,8 +19,11 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
+  cpu.csrs[mcause] = NO;
+  cpu.csrs[mepc] = epc;
 
-  return 0;
+  printf("mtvec = 0x%x.\n", cpu.csrs[mtvec]);
+  return cpu.csrs[mtvec];
 }
 
 word_t isa_query_intr() {
