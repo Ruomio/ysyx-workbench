69399fcd697d94c9ac1c460a3b1655fc15621baa
tracer-ysyx <tracer@ysyx.org>
2024-10-21 11:41:46 +0800

    > run NEMU ysyx_24080020 彭培智 Linux archlinux 6.11.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 10 Oct 2024 20:11:06 +0000 x86_64 GNU/Linux  11:41:46 up  2:36,  2 users,  load average: 0.78, 0.83, 0.80

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -43,7 +43,7 @@ enum {
 #define ECALL() do { \
     IFDEF(__riscv_e, bool success;  isa_raise_intr(isa_reg_str2val("a5", &success), s->pc);  return;) \
     bool success; isa_raise_intr(isa_reg_str2val("a7", &success), s->pc); \
-    assert(0); \
+    printf("ECALL().\n"); \
 } while(0)
 
 IFDEF(CONFIG_FTRACE_COND, int update_ftrace(uint32_t pc, uint32_t addr, uint32_t rs1, uint32_t rd));
