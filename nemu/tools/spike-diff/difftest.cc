1c27a76f50121f7809f92748f65c20575f597a79
tracer-ysyx <tracer@ysyx.org>
2024-10-22 21:33:15 +0800

    > run NEMU ysyx_24080020 彭培智 Linux archlinux 6.6.56-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 10 Oct 2024 12:04:53 +0000 x86_64 GNU/Linux  21:33:15 up 12:18,  2 users,  load average: 1.19, 1.07, 1.09

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -60,6 +60,7 @@ static state_t *state = NULL;
 void sim_t::diff_init(int port) {
   p = get_core("0");
   state = p->get_state();
+  state->mcause->write(0xffffffff);
 }
 
 void sim_t::diff_step(uint64_t n) {
