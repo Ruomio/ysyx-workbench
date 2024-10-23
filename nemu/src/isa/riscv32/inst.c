a04976d15c37022d5d8dcae0e09031949ea857a7
tracer-ysyx <tracer@ysyx.org>
2024-10-23 13:51:53 +0800

    > run NEMU ysyx_24080020 彭培智 Linux archlinux 6.11.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 10 Oct 2024 20:11:06 +0000 x86_64 GNU/Linux  13:51:53 up  4:16,  2 users,  load average: 1.25, 1.24, 1.18

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -50,7 +50,7 @@ static vaddr_t *get_csr_reg(word_t csr) {
     if(csr == 0x341) {return &cpu.csrs[mepc];}
     else if(csr == 0x300) { /*printf("mstatus = 0x%x\n", cpu.csrs[mstatus]);*/ return &cpu.csrs[mstatus];}
     else if(csr == 0x342) { /*printf("mcause = 0x%x\n", cpu.csrs[mcause]);*/ return &cpu.csrs[mcause];}
-    else if(csr == 0x305) {return &cpu.csrs[mtvec];}
+    else if(csr == 0x305) { printf("mtvec = 0x%x\n", cpu.csrs[mtvec]); return &cpu.csrs[mtvec];}
     else { Assert(0, "Unknown csr reg."); }
 }
 static void ecall(Decode *s) {
@@ -144,8 +144,8 @@ static int decode_exec(Decode *s) {
   INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, s->dnpc = CSR(0x341); /*assert(0)*/ );
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, ecall(s); /*assert(0)*/ );
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10)); IFDEF(CONFIG_FTRACE_COND, close_ftrace())); // R(10) is $a0
-  INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , N, R(rd) = CSR(imm()); CSR(imm()) = src1; IFDEF(CONFIG_DIFFTEST ,difftest_skip_ref());/*printf("csrrw imm():0x%x\n", (unsigned int)imm()); assert(0)*/ );
-  INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , N, R(rd) = CSR(imm()); CSR(imm()) = CSR(imm()) | src1; IFDEF(CONFIG_DIFFTEST ,difftest_skip_ref());/*printf("inst imm() = 0x%x\n", (unsigned int)imm()); assert(0)*/ );
+  INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , N, R(rd) = CSR(imm()); CSR(imm()) = src1; IFDEF(CONFIG_DIFFTEST ,difftest_skip_ref()); /* assert(0) */ );
+  INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , N, R(rd) = CSR(imm()); CSR(imm()) = CSR(imm()) | src1; IFDEF(CONFIG_DIFFTEST ,difftest_skip_ref()); /* assert(0) */ );
   INSTPAT("??????? ????? ????? 011 ????? 11100 11", csrrc  , I, assert(0));
   INSTPAT("??????? ????? ????? 101 ????? 11100 11", csrrw  , I, assert(0));
   INSTPAT("??????? ????? ????? 110 ????? 11100 11", csrrsi , I, assert(0));
