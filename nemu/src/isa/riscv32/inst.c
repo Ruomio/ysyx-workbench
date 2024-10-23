c2896faeda99194a852321474deb9aade4ec401b
tracer-ysyx <tracer@ysyx.org>
2024-10-23 14:04:49 +0800

    > run NEMU ysyx_24080020 彭培智 Linux archlinux 6.11.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 10 Oct 2024 20:11:06 +0000 x86_64 GNU/Linux  14:04:49 up  4:28,  2 users,  load average: 1.42, 1.52, 1.45

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -51,7 +51,7 @@ static vaddr_t *get_csr_reg(word_t csr) {
   if(csr == 0x341) {return &cpu.csrs[mepc];}
   else if(csr == 0x300) { /*printf("mstatus = 0x%x\n", cpu.csrs[mstatus]);*/ return &cpu.csrs[mstatus];}
   else if(csr == 0x342) { /*printf("mcause = 0x%x\n", cpu.csrs[mcause]);*/ return &cpu.csrs[mcause];}
-  else if(csr == 0x305) { printf("mtvec = 0x%x\n", cpu.csrs[mtvec]); return &cpu.csrs[mtvec];}
+  else if(csr == 0x305) { /*printf("mtvec = 0x%x\n", cpu.csrs[mtvec]);*/ return &cpu.csrs[mtvec];}
   else { Assert(0, "Unknown csr reg."); }
 }
 static void ecall(Decode *s) {
