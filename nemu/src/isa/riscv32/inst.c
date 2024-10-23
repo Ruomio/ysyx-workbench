78eb94a7d136db2b27e4d62e64a073993076411f
tracer-ysyx <tracer@ysyx.org>
2024-10-23 13:58:47 +0800

    > run NEMU ysyx_24080020 彭培智 Linux archlinux 6.11.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 10 Oct 2024 20:11:06 +0000 x86_64 GNU/Linux  13:58:47 up  4:22,  2 users,  load average: 2.81, 1.94, 1.47

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -142,7 +142,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000??? ????? 00000 000 00000 00011 11", fence  , N, );       // sync mem and i/o
   INSTPAT("0000000 00000 00000 001 00000 00011 11", fence.i, N, );       // sync insts
 
-  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, s->dnpc = CSR(0x341); /*assert(0)*/ );
+  INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, /* s->dnpc = CSR(0x341); */ /*assert(0)*/ );
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, ecall(s); /*assert(0)*/ );
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10)); IFDEF(CONFIG_FTRACE_COND, close_ftrace())); // R(10) is $a0
   INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , N, R(rd) = CSR(imm()); CSR(imm()) = src1; IFDEF(CONFIG_DIFFTEST ,difftest_skip_ref()); /* assert(0) */ );
