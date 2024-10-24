9f71f701299bdc0c9486205f60cea02ba7b1f6d7
tracer-ysyx <tracer@ysyx.org>
2024-10-24 09:45:12 +0800

    > run NEMU ysyx_24080020 彭培智 Linux archlinux 6.11.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 10 Oct 2024 20:11:06 +0000 x86_64 GNU/Linux  09:45:12 up 1 day, 9 min,  2 users,  load average: 0.96, 1.10, 1.34

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -21,6 +21,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
+  printf("NO = 0x%x,    EPC = 0x%x\n", NO, epc);
   cpu.csrs[mcause] = NO;
   cpu.csrs[mepc] = NO == EVENT_YIELD ? epc + 4 : epc;
 
