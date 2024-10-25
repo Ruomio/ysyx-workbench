6936077badc3ffcb314a8c0c9f200152d9bea46c
tracer-ysyx <tracer@ysyx.org>
2024-10-25 09:29:59 +0800

    > run NEMU ysyx_24080020 彭培智 Linux archlinux 6.11.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 10 Oct 2024 20:11:06 +0000 x86_64 GNU/Linux  09:29:59 up  1:10,  2 users,  load average: 0.65, 0.80, 0.88

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -145,7 +145,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , R, s->dnpc = CSR(MEPC_ADDR); /* CSR(MSTATUS_ADDR) |= 1 << 7; */  /*assert(0)*/ );
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, ecall(s); /*assert(0)*/ );
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , I, NEMUTRAP(s->pc, R(10)); IFDEF(CONFIG_FTRACE_COND, close_ftrace())); // R(10) is $a0
-  INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , I, R(rd) = CSR(imm()); CSR(imm()) = src1; /* IFDEF(CONFIG_DIFFTEST ,difftest_skip_ref());  assert(0) */ );
+  INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , I, R(rd) = CSR(imm()); CSR(imm()) = src1; IFDEF(CONFIG_DIFFTEST ,difftest_skip_ref()); /* assert(0) */ );
   INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , I, R(rd) = CSR(imm()); CSR(imm()) = CSR(imm()) | src1; /* IFDEF(CONFIG_DIFFTEST ,difftest_skip_ref());  assert(0) */ );
   INSTPAT("??????? ????? ????? 011 ????? 11100 11", csrrc  , I, assert(0));
   INSTPAT("??????? ????? ????? 101 ????? 11100 11", csrrw  , I, assert(0));
