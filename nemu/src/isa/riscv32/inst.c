86555279d9fd4ccddb262992c101e3b277c66264
tracer-ysyx <tracer@ysyx.org>
2024-10-28 09:08:08 +0800

    > run NEMU ysyx_24080020 彭培智 Linux archlinux 6.11.3-arch1-1 #1 SMP PREEMPT_DYNAMIC Thu, 10 Oct 2024 20:11:06 +0000 x86_64 GNU/Linux  09:08:08 up 14:44,  2 users,  load average: 1.08, 1.12, 1.05

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -60,7 +60,7 @@ static void ecall(Decode *s) {
   s->dnpc =  isa_raise_intr(isa_reg_str2val("a5", &success), s->pc);
   assert(0);
 #else
-  s->dnpc = isa_raise_intr(isa_reg_str2val("a7", &success), s->pc + 4);
+  s->dnpc = isa_raise_intr(isa_reg_str2val("a7", &success), s->pc);
   Assert(success, "isa_reg_str2val error.");
 #endif
 }
