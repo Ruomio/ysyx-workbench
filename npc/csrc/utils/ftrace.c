2c6d1034037a8522feb4fcd521eb32a5a3676b7e
tracer-ysyx <tracer@ysyx.org>
2024-11-01 16:35:16 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.58-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 23 Oct 2024 17:28:15 +0000 x86_64 GNU/Linux  16:35:16 up  3:46,  2 users,  load average: 0.62, 0.83, 0.90

--- npc/csrc/utils/ftrace.c
+++ npc/csrc/utils/ftrace.c
@@ -17,7 +17,7 @@
 #include <elf.h>
 
 #ifdef CONFIG_FTRACE
-#define NR_FT (2048 * 10)
+#define NR_FT (2048 * 100)
 
 struct Ftrace_struct{
   uint32_t addr;
@@ -81,7 +81,7 @@ void init_ftrace(const char *img_file, const char *ftrace_file) {
   printf("init ftrace 1\n");
   // 打印函数名和地址
   for (int i = 0; i < shdrs[symtab_index].sh_size / sizeof(Elf32_Sym); i++) {
-    assert(i < 20480);
+    assert(i < 204800);
     if (ELF32_ST_TYPE(symbols[i].st_info) == STT_FUNC) {
       printf("Function: %10s, Address: 0x%x  Size: %d  \n",(strtab + symbols[i].st_name), symbols[i].st_value, symbols[i].st_size);
       strcpy(Ftrace_tab[i].name, strtab + symbols[i].st_name);
