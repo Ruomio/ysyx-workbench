43104f675903ccf5aa26c4b575b48be7c483d887
tracer-ysyx <tracer@ysyx.org>
2024-11-01 09:17:15 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.11.5-arch1-1 #1 SMP PREEMPT_DYNAMIC Tue, 22 Oct 2024 18:31:38 +0000 x86_64 GNU/Linux  09:17:15 up 25 min,  2 users,  load average: 1.03, 0.96, 0.85

--- npc/vsrc/exu.v
+++ npc/vsrc/exu.v
@@ -208,7 +208,7 @@ module ysyx_24080020_EXU
                         else begin
                             // ecall
                             
-                            dnpc = rcsrdata;
+                            // dnpc = rcsrdata;
                         end
                     end
                     default: begin
