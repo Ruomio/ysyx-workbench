a120a74a95b30b0a2f6918adf4413a68efb3dbf5
tracer-ysyx <tracer@ysyx.org>
2024-11-01 11:05:41 +0800

    > run NEMU ysyx_24080020 彭培智 Linux archlinux 6.11.5-arch1-1 #1 SMP PREEMPT_DYNAMIC Tue, 22 Oct 2024 18:31:38 +0000 x86_64 GNU/Linux  11:05:41 up  2:13,  2 users,  load average: 1.26, 1.09, 0.95

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -56,7 +56,7 @@ static vaddr_t *get_csr_reg(word_t csr) {
 }
 static void ecall(Decode *s) {
   bool success;
-#ifdef __riscv_e
+#if __ISA__ == riscv32e
   s->dnpc =  isa_raise_intr(isa_reg_str2val("a5", &success), s->pc);
   assert(0);
 #else
