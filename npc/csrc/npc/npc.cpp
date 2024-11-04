ce3e5bf2ea96b804a6a4b49739df69e674b506f2
tracer-ysyx <tracer@ysyx.org>
2024-11-04 14:42:05 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.11.6-arch1-1 #1 SMP PREEMPT_DYNAMIC Fri, 01 Nov 2024 03:30:41 +0000 x86_64 GNU/Linux  14:42:05 up 14 min,  2 users,  load average: 1.49, 1.03, 0.57

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -70,9 +70,9 @@ void init_npc(int argc, char **argv) {
   contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
   top = new Vtop(contextp);
+  contextp->traceEverOn(true);
 #ifdef CONFIG_WAVEFILE
   tfp = new VerilatedVcdC;
-  contextp->traceEverOn(true);
   top->trace(tfp, 0);
   tfp->open("build/wave.vcd");
 #endif
