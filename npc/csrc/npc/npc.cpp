02fe31e29682e3e24ca71e9ed247ec2b03d6cbce
tracer-ysyx <tracer@ysyx.org>
2024-12-02 15:42:18 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.63-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 15:39:56 +0000 x86_64 GNU/Linux  15:42:18 up  7:10,  2 users,  load average: 0.62, 0.50, 0.42

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -13,7 +13,7 @@
 #include "ringbuffer.h"
 #include <cpu/difftest.h>
 
-#define MAX_WAVE_STEP 100
+#define MAX_WAVE_STEP 10000
 
 // TRACE
 extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
