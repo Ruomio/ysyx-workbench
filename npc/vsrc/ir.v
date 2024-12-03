d8e65d94b34c6fcea0c5426993559d20ce386c37
tracer-ysyx <tracer@ysyx.org>
2024-12-03 16:44:59 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.63-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 15:39:56 +0000 x86_64 GNU/Linux  16:44:59 up 19:56,  2 users,  load average: 0.98, 0.90, 0.72

---
+++ npc/vsrc/ir.v
@@ -0,0 +1,30 @@
+`include "/home/papillon/Documents/All_codes/ysyx-workbench/npc/vsrc/define.v"
+module ysyx_24080020_IR(
+    input clk,
+    input rst,
+    input if_en,
+    input [`ysyx_24080020_WIDTH-1:0] addr,
+
+    output reg [`ysyx_24080020_WIDTH-1:0] inst,
+    output reg inst_fin
+
+);
+    import "DPI-C" function int read_memory(input int addr, input int len);
+
+    reg cnt;
+
+    always @(posedge clk) begin
+        if(!rst) begin
+            inst <= 32'b0;
+        end
+        else if(if_en) begin
+            inst <= read_memory(addr, 32'b100);
+            inst_fin <= 1'b1;
+        end
+        else begin
+            inst <= inst;
+            inst_fin <= 1'b0;
+        end
+    end
+
+endmodule
