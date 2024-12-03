29df1e778b2d2402f392cf8035f2958e7d8035e7
tracer-ysyx <tracer@ysyx.org>
2024-12-03 15:59:40 +0800

    > gdb NPC ysyx_24080020 彭培智 Linux archlinux 6.6.63-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 15:39:56 +0000 x86_64 GNU/Linux  15:59:40 up 19:10,  2 users,  load average: 0.82, 0.39, 0.39

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -91,7 +91,8 @@ void init_mem() {
 word_t paddr_read(paddr_t addr, int len) {
   IFDEF(CONFIG_MTRACE_COND, MtraceBuf_write(addr, len, 0));
   if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+  // IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
+  IFDEF(CONFIG_DEVICE, return 0);
   out_of_bound(addr);
   return 0;
 }
