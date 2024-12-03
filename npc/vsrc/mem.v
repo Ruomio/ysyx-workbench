ce9d9d76bf611498f06eaa3279acdf9cc5f18b05
tracer-ysyx <tracer@ysyx.org>
2024-12-03 15:07:45 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.63-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 15:39:56 +0000 x86_64 GNU/Linux  15:07:45 up 18:18,  2 users,  load average: 1.09, 0.73, 0.85

--- npc/vsrc/mem.v
+++ npc/vsrc/mem.v
@@ -82,7 +82,7 @@ module ysyx_24080020_MEM(
     always @(posedge clk) begin
         if(exu_mem_valid) begin
             if(mem_wb_valid) mem_exu_ready <= 1'b0;
-            else if(mem_exu_ready == 1'b0) begin
+            else begin
                 mem_exu_ready <= 1'b1;
 
                 // update reg
@@ -115,12 +115,6 @@ module ysyx_24080020_MEM(
                 if(!mwen_exu && !mren_exu) begin
                     mem_wb_valid <= 1'b1;
                 end
-                else begin
-                    mem_wb_valid <= 1'b0;
-                end
-            end
-            else begin
-                mem_exu_ready <= 1'b0;
             end
         end
         else if(wb_mem_ready && state) begin
