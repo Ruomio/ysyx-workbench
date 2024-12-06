43263bb7d34f4d2fac139f60dcb6066c365ec61c
tracer-ysyx <tracer@ysyx.org>
2024-12-06 09:12:17 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.63-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 15:39:56 +0000 x86_64 GNU/Linux  09:12:17 up 1 day, 31 min,  2 users,  load average: 0.30, 0.40, 0.43

--- npc/vsrc/arbiter.v
+++ npc/vsrc/arbiter.v
@@ -1,30 +1,30 @@
-`include "/home/papillon/Documents/All_codes/ysyx-workbench/npc/vsrc/define.v"
-module ysyx_24080020_ARBITER(
-    input clk,
-    input rst,
-
-    input [5:0] lfsr, // the number of delay cycle
-
-    input 
-    input arvalid_ifu,
-    input [`ysyx_24080020_WIDTH-1:0] araddr_ifu,
-    output reg arready_ifu,
-
-    input rready_ifu,
-    output reg [`ysyx_24080020_WIDTH-1:0] rdata_ifu,
-    output reg [1:0] rresp_ifu,
-    output reg rvalid_ifu,
-
-    input arvalid_mem,
-    input [`ysyx_24080020_WIDTH-1:0] araddr_mem,
-    output reg arready_mem,
-
-    input rready_mem,
-    output reg [`ysyx_24080020_WIDTH-1:0] rdata_mem,
-    output reg [1:0] rresp_mem,
-    output reg rvalid_mem,
-
-);
-
-
-endmodule
+// `include "/home/papillon/Documents/All_codes/ysyx-workbench/npc/vsrc/define.v"
+// module ysyx_24080020_ARBITER(
+//     input clk,
+//     input rst,
+// 
+//     input [5:0] lfsr, // the number of delay cycle
+// 
+//     input 
+//     input arvalid_ifu,
+//     input [`ysyx_24080020_WIDTH-1:0] araddr_ifu,
+//     output reg arready_ifu,
+// 
+//     input rready_ifu,
+//     output reg [`ysyx_24080020_WIDTH-1:0] rdata_ifu,
+//     output reg [1:0] rresp_ifu,
+//     output reg rvalid_ifu,
+// 
+//     input arvalid_mem,
+//     input [`ysyx_24080020_WIDTH-1:0] araddr_mem,
+//     output reg arready_mem,
+// 
+//     input rready_mem,
+//     output reg [`ysyx_24080020_WIDTH-1:0] rdata_mem,
+//     output reg [1:0] rresp_mem,
+//     output reg rvalid_mem,
+// 
+// );
+// 
+// 
+// endmodule
