06703427a987db1e12928093fb96e6a0de391105
tracer-ysyx <tracer@ysyx.org>
2024-12-06 15:38:08 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.63-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 15:39:56 +0000 x86_64 GNU/Linux  15:38:08 up  4:33,  2 users,  load average: 0.35, 0.45, 0.41

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -257,12 +257,12 @@ void check_trap(npc_state u_npc_state) {
 }
 
 uint32_t g_get_pc() {
-  g_pc =  top->rootp->top__DOT__u_npc__DOT__ifu__DOT__addr;
+  g_pc =  top->rootp->top__DOT__u_npc__DOT__araddr_ifu;
   return g_pc;
 }
 
 void g_set_pc(uint32_t pc) {
-  top->rootp->top__DOT__u_npc__DOT__ifu__DOT__addr = pc;
+  top->rootp->top__DOT__u_npc__DOT__araddr_ifu = pc;
 }
 
 uint32_t g_get_reg(int i) {
