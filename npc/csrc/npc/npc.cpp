5857f814015196109f14cd605195604068099fa7
tracer-ysyx <tracer@ysyx.org>
2024-12-06 16:44:36 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.63-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 15:39:56 +0000 x86_64 GNU/Linux  16:44:36 up  5:40,  2 users,  load average: 0.84, 0.72, 0.53

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -314,4 +314,6 @@ void update_dut() {
 void printf_info() {
   printf("araddr arbiter = 0x%x\n", top->rootp->top__DOT__u_npc__DOT__araddr_arbiter_slave);
   printf("araddr ifu = 0x%x\n", top->rootp->top__DOT__u_npc__DOT__ifu__DOT__addr);
+
+  printf("g_pc = 0x%x", g_pc);
 }
