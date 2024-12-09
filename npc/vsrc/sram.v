f595a41f0f4517f01e44246947012d0070ecce53
tracer-ysyx <tracer@ysyx.org>
2024-12-09 13:16:40 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.63-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 15:39:56 +0000 x86_64 GNU/Linux  13:16:40 up  4:29,  2 users,  load average: 0.43, 0.33, 0.19

--- npc/vsrc/sram.v
+++ npc/vsrc/sram.v
@@ -26,9 +26,10 @@ module ysyx_24080020_SRAM(
     output reg bvalid,
     input bready
 );
-    import "DPI-C" function void printf_info();
-    import "DPI-C" function int read_memory(input int addr, input int len);
-    import "DPI-C" function void write_memory(input int addr, input int len, input int data);
+    // import "DPI-C" function void printf_info();
+    // import "DPI-C" function int read_memory(input int addr, input int len);
+    // import "DPI-C" function void write_memory(input int addr, input int len, input int data);
+    reg [31:0] sram [511:0];
 
     reg [`ysyx_24080020_WIDTH-1:0] paddr;
     reg [`ysyx_24080020_WIDTH-1:0] write_data;
@@ -83,7 +84,8 @@ module ysyx_24080020_SRAM(
             end
             else begin
                 // printf_info();
-                rdata <= read_memory(paddr, 32'd4);
+                // rdata <= read_memory(paddr, 32'd4);
+                rdata <= sram[paddr];
                 if(wvalid) begin
                     read_before_write <= 1'b1;
                     rvalid <= 1'b0;
@@ -152,7 +154,8 @@ module ysyx_24080020_SRAM(
                     write_en <= 1'b1;
                 end
                 else if(!wready) begin
-                    write_memory(paddr, 32'd4, write_data);
+                    // write_memory(paddr, 32'd4, write_data);
+                    sram[paddr] <= write_data;
                     b_en <= 1'b1;
 
                     wready <= 1'b1;
