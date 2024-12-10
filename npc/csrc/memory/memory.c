f9c7f81e4a5701e9d31200ad39c4074ac63e7364
tracer-ysyx <tracer@ysyx.org>
2024-12-10 09:10:31 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.63-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 15:39:56 +0000 x86_64 GNU/Linux  09:10:31 up 10 min,  2 users,  load average: 0.70, 0.67, 0.41

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -1,5 +1,4 @@
 #include "memory/memory.h"
-#include "common.h"
 #include "define.h"
 #include <stdint.h>
 #include <stdio.h>
@@ -101,14 +100,6 @@ static void pmem_write(paddr_t addr, int len, word_t data) {
   host_write(guest_to_host(addr), len, data);
 }
 
-int read_memory(int addr, int len) {
-  return paddr_read(addr, len);
-}
-
-void write_memory(int addr, int len, int data) {
-  paddr_write(addr, len, data);
-}
-
 static uint64_t u_time;
 word_t paddr_read(paddr_t addr, int len) {
   IFDEF(CONFIG_MTRACE_COND, MtraceBuf_write(addr, len, 0));
@@ -131,4 +122,12 @@ void paddr_write(paddr_t addr, int len, word_t data) {
     if(addr == 0xa00003f8) {putchar(data); fflush(stdout); } return;
   );
   out_of_bound(addr);
+}
+
+int read_memory(int addr, int len) {
+  return paddr_read(addr, len);
+}
+
+void write_memory(int addr, int len, int data) {
+  paddr_write(addr, len, data);
 }
