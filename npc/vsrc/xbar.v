74aa532bc820e62c7a1d39400b9fee14d5bf5487
tracer-ysyx <tracer@ysyx.org>
2024-12-13 09:07:08 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.63-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 22 Nov 2024 15:39:56 +0000 x86_64 GNU/Linux  09:07:08 up 3 days, 6 min,  2 users,  load average: 1.47, 1.32, 1.18

--- npc/vsrc/xbar.v
+++ npc/vsrc/xbar.v
@@ -87,7 +87,9 @@ module ysyx_24080020_XBAR(
         :
             ((arvalid_arbiter == 1'b1) ?
                 ((araddr_arbiter >= `ysyx_24080020_MBASE) && (araddr_arbiter < 32'h81000000)) ? 3'd1 :
-                    (araddr_arbiter == `ysyx_24080020_SERIAL_PORT) ? 3'd2 : 3'd0);
+                    (araddr_arbiter == `ysyx_24080020_SERIAL_PORT) ? 3'd2 : 3'd0
+                :
+                3'd0);
 
     /* AR: Xbar -> UART
                 |
