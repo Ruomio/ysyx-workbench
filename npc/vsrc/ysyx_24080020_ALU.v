ca8c7c38d682f82f4a99c4b64db2e20ee6d1636f
tracer-ysyx <tracer@ysyx.org>
2024-12-17 10:32:42 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  10:32:42 up  1:30,  2 users,  load average: 0.60, 0.51, 0.44

--- npc/vsrc/ysyx_24080020_ALU.v
+++ npc/vsrc/ysyx_24080020_ALU.v
@@ -1,4 +1,5 @@
-`include "/home/papillon/Documents/All_codes/ysyx-workbench/npc/vsrc/ysyx_24080020_DEFINE.v"
+// `include "/home/papillon/Documents/All_codes/ysyx-workbench/npc/vsrc/ysyx_24080020_DEFINE.v"
+`include "ysyx_24080020_DEFINE.v"
 module ysyx_24080020_ALU(
     input [`ysyx_24080020_ALU_OP_WIDTH-1:0] alu_op,
     input [`ysyx_24080020_WIDTH-1:0] alu_src1,
