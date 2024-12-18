c1e61cdf48433735627319f7125ed12089e21612
tracer-ysyx <tracer@ysyx.org>
2024-12-18 14:16:09 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  14:16:09 up 1 day,  5:13,  2 users,  load average: 0.36, 0.62, 0.69

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -135,5 +135,6 @@ void write_memory(int addr, int len, int data) {
 extern "C" void flash_read(int32_t addr, int32_t *data) { assert(0); }
 
 extern "C" void mrom_read(int32_t addr, int32_t *data) {
+  assert(addr != 0);
   *data = paddr_read(addr, 4);
 }
