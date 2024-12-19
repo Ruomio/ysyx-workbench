b1800f2446e558885531868be098430225ef1904
tracer-ysyx <tracer@ysyx.org>
2024-12-19 09:43:35 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  09:43:35 up 18:59,  2 users,  load average: 0.75, 0.54, 0.52

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -294,7 +294,7 @@ uint32_t g_get_rd() {
   return BITS(top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__u_npc__DOT__inst_ifu, 19, 7);
 }
 
-const char *regs[] = {
+const char *regs_name[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
@@ -303,7 +303,7 @@ const char *regs[] = {
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i=0; i<sizeof(ref_r->gpr)/sizeof(ref_r->gpr[0]); i++) {
     if(ref_r->gpr[i] != g_get_reg(i)) {
-      printf("The %sth reg is diff, shoud be %#x  but get %#x.\n", regs[i], ref_r->gpr[i], g_get_reg(i));
+      printf("The %sth reg is diff, shoud be %#x  but get %#x.\n", regs_name[i], ref_r->gpr[i], g_get_reg(i));
       return false;
     }
   }
