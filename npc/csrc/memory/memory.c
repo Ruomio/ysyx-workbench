9b84d0241a5a807532c60a9fa939fa716bc656ec
tracer-ysyx <tracer@ysyx.org>
2024-12-19 09:57:46 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  09:57:46 up 19:13,  2 users,  load average: 0.49, 0.59, 0.54

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -95,6 +95,7 @@ paddr_t host_to_guest(uint8_t *haddr) { return haddr - memory + CONFIG_MBASE; }
 static word_t pmem_read(paddr_t addr, int len) {
   if(addr >= CONFIG_SRAM_BASE && addr <= CONFIG_SRAM_BASE + CONFIG_SRAM_SIZE) {
     word_t rdata = *(volatile uint32_t *)(uintptr_t)(addr + CONFIG_SRAM_BASE);
+    printf("sram rdata : 0x%x\n", rdata);
     switch(len){
       case 1: return (char)rdata;
       case 2: return (short)rdata;
