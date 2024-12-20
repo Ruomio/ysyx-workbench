4bba53401a322f594c48753fe99296f8d9b80ba1
tracer-ysyx <tracer@ysyx.org>
2024-12-20 15:13:43 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.4-arch1-1 #1 SMP PREEMPT_DYNAMIC Mon, 09 Dec 2024 14:31:57 +0000 x86_64 GNU/Linux  15:13:43 up  6:52,  2 users,  load average: 0.48, 0.46, 0.52

--- npc/vsrc/ysyx_24080020_MEM.v
+++ npc/vsrc/ysyx_24080020_MEM.v
@@ -139,15 +139,15 @@ module ysyx_24080020_MEM(
     assign awsize = 3'b10;
     assign get_awlen = ({{2{1'b0}},mwaddr_mem[1:0]} + mwmask_mem) > 4'b100 ? 1'b1 : 1'b0;
     assign awaddr = mwaddr_mem;
-    assign wdata_1 =  wstrb == 4'b1111 ? mwdata_mem :
-                    wstrb == 4'b0011 ? mwdata_mem :
-                    wstrb == 4'b0001 ? mwdata_mem :
-                    wstrb == 4'b1110 ? mwdata_mem << 8 :
-                    wstrb == 4'b0110 ? mwdata_mem << 8 :
-                    wstrb == 4'b0010 ? mwdata_mem << 8 :
-                    wstrb == 4'b1100 ? mwdata_mem << 16 :
-                    wstrb == 4'b0100 ? mwdata_mem << 16 :
-                    wstrb == 4'b1000 ? mwdata_mem << 24 :
+    assign wdata_1 =  wstrb_1 == 4'b1111 ? mwdata_mem :
+                    wstrb_1 == 4'b0011 ? mwdata_mem :
+                    wstrb_1 == 4'b0001 ? mwdata_mem :
+                    wstrb_1 == 4'b1110 ? mwdata_mem << 8 :
+                    wstrb_1 == 4'b0110 ? mwdata_mem << 8 :
+                    wstrb_1 == 4'b0010 ? mwdata_mem << 8 :
+                    wstrb_1 == 4'b1100 ? mwdata_mem << 16 :
+                    wstrb_1 == 4'b0100 ? mwdata_mem << 16 :
+                    wstrb_1 == 4'b1000 ? mwdata_mem << 24 :
                     32'b0;
     assign wstrb_1 = mwaddr_mem[1:0] == 2'b00 ?
                         mwmask_mem == 4'b100 ? 4'b1111 :
@@ -171,10 +171,10 @@ module ysyx_24080020_MEM(
                         4'b0000 :
                     4'b0000;
 
-    assign wdata_2 =  wstrb == 4'b1111 ? mwdata_mem :
-                    wstrb == 4'b0001 ? mwdata_mem >> 24 :
-                    wstrb == 4'b0011 ? mwdata_mem >> 16 :
-                    wstrb == 4'b0111 ? mwdata_mem >> 24 :
+    assign wdata_2 =  wstrb_2 == 4'b1111 ? mwdata_mem :
+                    wstrb_2 == 4'b0001 ? mwdata_mem >> 24 :
+                    wstrb_2 == 4'b0011 ? mwdata_mem >> 16 :
+                    wstrb_2 == 4'b0111 ? mwdata_mem >> 24 :
                     32'b0;
     assign wstrb_2 = mwaddr_mem[1:0] == 2'b00 ?
                         mwmask_mem == 4'b100 ? 4'b0000 :
