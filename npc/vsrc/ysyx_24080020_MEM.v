d75457e17bd573a04c7c988bb0a7116f8c466074
tracer-ysyx <tracer@ysyx.org>
2024-12-24 13:38:46 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.67-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 19 Dec 2024 20:39:01 +0000 x86_64 GNU/Linux  13:38:46 up 31 min,  1 user,  load average: 0.58, 0.58, 0.48

--- npc/vsrc/ysyx_24080020_MEM.v
+++ npc/vsrc/ysyx_24080020_MEM.v
@@ -111,7 +111,6 @@ module ysyx_24080020_MEM(
     wire [3:0] wstrb_1, wstrb_2;
     wire [31:0] wdata_1, wdata_2;
 
-    // assign arsize = 3'b10;
     assign arsize = $clog2(mrlen_mem)[2:0];
     assign araddr = mraddr_mem;
     assign arburst = 2'b1;
@@ -137,7 +136,6 @@ module ysyx_24080020_MEM(
 
 
     assign awburst = 2'b1;
-    // assign awsize = 3'b10;
     assign awsize = $clog2(mwmask_mem)[2:0];
     assign get_awlen = ({{2{1'b0}},mwaddr_mem[1:0]} + mwmask_mem) > 4'b100 ? 1'b1 : 1'b0;
     assign awaddr = mwaddr_mem;
