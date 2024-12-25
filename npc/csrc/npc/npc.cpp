742cf2282bae5f292f95945e43638dc34b349434
tracer-ysyx <tracer@ysyx.org>
2024-12-25 09:23:31 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.67-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 19 Dec 2024 20:39:01 +0000 x86_64 GNU/Linux  09:23:31 up 20:15,  1 user,  load average: 0.45, 0.54, 0.54

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -132,7 +132,7 @@ void exec_once_npc(uint32_t pc) {
   // int ilen = g_get_snpc() - last_pc;
   int ilen = 4;
   int i;
-  uint32_t last_inst = read_memory(last_pc, ilen);
+  uint32_t last_inst = paddr_read(last_pc, ilen);
   uint8_t *inst = (uint8_t *)&last_inst;
   for (i = ilen - 1; i >= 0; i --) {
     p += snprintf(p, 4, " %02x", inst[i]);
