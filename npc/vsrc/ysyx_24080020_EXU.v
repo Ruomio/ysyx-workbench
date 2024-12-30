c46a486be84d11136f819fda546bb56abfd36123
tracer-ysyx <tracer@ysyx.org>
2024-12-30 14:22:07 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.67-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 19 Dec 2024 20:39:01 +0000 x86_64 GNU/Linux  14:22:07 up  5:29,  1 user,  load average: 0.67, 0.87, 0.91

--- npc/vsrc/ysyx_24080020_EXU.v
+++ npc/vsrc/ysyx_24080020_EXU.v
@@ -167,6 +167,7 @@ module ysyx_24080020_EXU
                 wcsraddr2_exu <= wcsraddr2_idu;
                 wcsrdata2_exu <= wcsrdata2_idu;
 
+                is_csrtype_exu <= is_csrtype_idu;
 
                 cnt <= 1'b1;
 
@@ -224,7 +225,7 @@ module ysyx_24080020_EXU
 
     assign branch_src1 = is_jalr_exu == 1'b1 ? branch_src1_exu :
                             is_csrtype_exu == 1'b1 ? dnpc_exu : pc_exu;
-    assign branch_src2 = is_csrtype_idu == 1'b1 ? 32'b0 : imm_exu;
+    assign branch_src2 = is_csrtype_exu == 1'b1 ? 32'b0 : imm_exu;
 
     ysyx_24080020_ADDER u_branch(
         .a(branch_src1),
