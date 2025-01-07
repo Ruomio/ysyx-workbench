a83c6bc68677d0f2467ed836c357db1ea7c27569
tracer-ysyx <tracer@ysyx.org>
2025-01-07 11:31:51 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.65-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 11 Dec 2024 15:35:54 +0000 x86_64 GNU/Linux  11:31:51 up 1 day,  2:52,  1 user,  load average: 0.97, 1.29, 1.46

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -120,7 +120,7 @@ void exec_once_npc(uint32_t pc) {
       total_wave_step++;
 #endif
     if(last_pc != g_get_pc()) {
-      printf("exec pc: 0x%x\n", last_pc);
+      // printf("exec pc: 0x%x\n", last_pc);
       Assert(g_pc >= 0x30000000, "pc invalid:0x%x, last pc: 0x%x", g_pc, last_pc);
       break;
     }
