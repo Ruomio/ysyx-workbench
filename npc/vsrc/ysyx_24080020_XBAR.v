724c4915c37c53580bf59c94a52b81c7efb58bdb
24080020-Peizhi Peng <1065940593@qq.com>
2025-01-10 11:35:16 +0800

    npc: change clint addr

--- npc/vsrc/ysyx_24080020_XBAR.v
+++ npc/vsrc/ysyx_24080020_XBAR.v
@@ -194,11 +194,11 @@ module ysyx_24080020_XBAR(
     */
     always @(awvalid_arbiter or arvalid_arbiter) begin
         if(awvalid_arbiter) begin
-            device_addr = (awaddr_arbiter == `ysyx_24080020_RTC_ADDR || awaddr_arbiter == `ysyx_24080020_RTC_ADDR + 32'h4) ? 3'd3 :
+            device_addr = (awaddr_arbiter == `ysyx_24080020_CLINT_ADDR || awaddr_arbiter == `ysyx_24080020_CLINT_ADDR + 32'h4) ? 3'd3 :
                             3'd4;
         end
         else if(arvalid_arbiter) begin
-            device_addr = (araddr_arbiter == `ysyx_24080020_RTC_ADDR || araddr_arbiter == `ysyx_24080020_RTC_ADDR + 32'h4) ? 3'd3 :
+            device_addr = (araddr_arbiter == `ysyx_24080020_CLINT_ADDR || araddr_arbiter == `ysyx_24080020_CLINT_ADDR + 32'h4) ? 3'd3 :
                             3'd4;
         end
         else begin
