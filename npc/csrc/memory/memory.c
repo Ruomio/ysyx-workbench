b45c69a3babe00fb42b29e042400f1e0fea2493a
tracer-ysyx <tracer@ysyx.org>
2025-01-15 14:44:21 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.65-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 11 Dec 2024 15:35:54 +0000 x86_64 GNU/Linux  14:44:21 up 2 days,  6:37,  1 user,  load average: 1.00, 0.67, 0.64

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -179,13 +179,13 @@ extern "C" void psram_write(int waddr, int wdata, int wstrb) {
 
 extern "C" void sdram_read(char ba, int row_addr, int col_addr, short int wstrb, short int *rdata) {
   uint32_t addr = ba * 8192 * 512 * 2 + ((row_addr-1) * 512)*2 + col_addr * 2;
-  addr |= 0xa0000000;
+  // addr |= 0xa0000000;
   *rdata = paddr_read(addr, 2) & wstrb;
   printf("sdram read, addr: 0x%x,  rdata: 0x%x\n", addr, *rdata);
 }
 extern "C" void sdram_write(char ba, int row_addr, int col_addr, short int wstrb, short int wdata) {
-  uint32_t addr = (ba * 8192 * 512 * 0) + ((row_addr * 512)*2) + col_addr * 2;
-  addr |= 0xa0000000;
+  uint32_t addr = (ba * 8192 * 512 * 2) + ((row_addr * 512)*2) + col_addr * 2;
+  // addr |= 0xa0000000;
   printf("wstrb : 0x%x, ba: 0x%x, row: 0x%x, col: 0x%x\n",wstrb, ba, row_addr, col_addr);
   if((wstrb & 0xffff) == 0xffff) {
     paddr_write(addr, 2, wdata);
