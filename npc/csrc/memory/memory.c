140878e7b9ad98c51859785489faba3bbf2896b6
tracer-ysyx <tracer@ysyx.org>
2025-01-15 13:45:49 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.65-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 11 Dec 2024 15:35:54 +0000 x86_64 GNU/Linux  13:45:49 up 2 days,  5:38,  1 user,  load average: 0.27, 0.46, 0.57

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -5,6 +5,7 @@
 #include <assert.h>
 #include <string.h>
 #include "debug.h"
+#include "difftest-def.h"
 #include "memory/paddr.h"
 
 
@@ -198,4 +199,5 @@ extern "C" void sdram_write(char ba, int row_addr, int col_addr, short int wstrb
     paddr_write(addr + 0x1, 1, wdata >> 8);
     // printf("sdram write, addr: 0x%x,  data: 0x%x\n", addr, wdata);
   }
+  Assert(addr <= CONFIG_SDRAM_BASE + CONFIG_SDRAM_SIZE, "OUT OF SDARM ADDR");
 }
