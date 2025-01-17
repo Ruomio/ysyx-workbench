d57f72e8f258294a3e22bdbb2507dd09c1a1cc07
tracer-ysyx <tracer@ysyx.org>
2025-01-17 08:51:47 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.65-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 11 Dec 2024 15:35:54 +0000 x86_64 GNU/Linux  08:51:47 up 4 days, 44 min,  1 user,  load average: 1.18, 0.85, 0.59

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -181,7 +181,7 @@ extern "C" void sdram_read(char ba, int row_addr, int col_addr, short int wstrb,
   uint32_t addr = (ba << 10) | (row_addr << 12) | (col_addr << 1);
   addr |= 0xa0000000;
   *rdata = paddr_read(addr, 2) & wstrb;
-  // printf("sdram read, addr: 0x%x,  rdata: 0x%x\n", addr, *rdata);
+  printf("sdram read, addr: 0x%x,  rdata: 0x%x\n", addr, *rdata);
 }
 extern "C" void sdram_write(char ba, int row_addr, int col_addr, short int wstrb, int wdata) {
   static char h_or_l = 0;
