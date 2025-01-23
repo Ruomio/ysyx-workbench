4d7bef4d1b0d476fda113d13ccaee7d56a4b52d9
tracer-ysyx <tracer@ysyx.org>
2025-01-23 09:02:10 +0800

    > run NPC ysyx_24080020 彭培智 Linux archlinux 6.6.65-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 11 Dec 2024 15:35:54 +0000 x86_64 GNU/Linux  09:02:10 up 21:05,  1 user,  load average: 3.42, 2.96, 1.74

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -191,11 +191,7 @@ extern "C" void sdram_read(char ba, int row_addr, int col_addr, short int wstrb,
 
   *rdata = (uint16_t)paddr_read(addr, 2) & wstrb;
 
-  if(!sw_sdram_r)
-    printf("sdram read, addr: 0x%x,  rdata: 0x%x\n", addr, *rdata & wstrb);
-  else if(sw_sdram_r == 1)
-    printf("sdram read, addr: 0x%x,  rdata: 0x%x\n", addr|0x1000000, *rdata & wstrb);
-
+  // printf("sdram read, addr: 0x%x,  rdata: 0x%x\n", addr, *rdata & wstrb);
 
   switch(sw_sdram_r) {
     case 0: sw_sdram_r = 1; break;
@@ -232,7 +228,7 @@ extern "C" void sdram_write(char ba, int row_addr, int col_addr, short int wstrb
   }
   if(len) {
     paddr_write(addr, len, wdata);
-    printf("sdram write, addr: 0x%x,  data: 0x%x\n", addr, wdata);
+    // printf("sdram write, addr: 0x%x,  data: 0x%x\n", addr, wdata);
   }
   assert(col_addr < 512);
   Assert((addr >= CONFIG_SDRAM_BASE && addr < CONFIG_SDRAM_BASE + CONFIG_SDRAM_SIZE), "OUT OF SDARM ADDR");
