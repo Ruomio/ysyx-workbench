f4ee2659da9744789f7706333c8cbdfa071a43e8
tracer-ysyx <tracer@ysyx.org>
2025-02-08 10:07:43 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.65-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 11 Dec 2024 15:35:54 +0000 x86_64 GNU/Linux  10:07:43 up  2:06,  1 user,  load average: 0.55, 0.52, 0.45

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -203,7 +203,10 @@ extern "C" void sdram_write(char id, char ba, int row_addr, int col_addr, short 
   uint32_t addr = (ba << 10) | (row_addr << 12) | (col_addr << 1);
   addr |= 0xa0000000;
 
-  if((wstrb & 0xffff) == 0xffff && ((addr & 0x3) == 0x0)) {
+  paddr_write(addr, len, wdata);
+  printf("trans mode!     sw_sdram_w: %d, id:%d,  sdram write, addr: 0x%x,  data: 0x%x,  wstrb:0x%x,  len:%d\n", sw_sdram_w, id, addr, wdata, wstrb, len);
+
+/*   if((wstrb & 0xffff) == 0xffff && ((addr & 0x3) == 0x0)) {
     switch(sw_sdram_w) {
       case 0: break;
       case 1: addr += 0x2; break;
@@ -257,7 +260,7 @@ extern "C" void sdram_write(char id, char ba, int row_addr, int col_addr, short 
     }
 
   }
-
+ */
   assert(col_addr < 512);
   Assert((addr >= CONFIG_SDRAM_BASE && addr < CONFIG_SDRAM_BASE + CONFIG_SDRAM_SIZE), "OUT OF SDARM ADDR");
 }
