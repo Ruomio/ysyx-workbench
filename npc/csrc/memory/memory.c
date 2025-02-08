e6831e01316e5ad6bd2f194a16bb2eea688dc2dc
tracer-ysyx <tracer@ysyx.org>
2025-02-08 09:39:15 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.65-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 11 Dec 2024 15:35:54 +0000 x86_64 GNU/Linux  09:39:15 up  1:38,  1 user,  load average: 0.49, 0.56, 0.44

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -223,7 +223,7 @@ extern "C" void sdram_write(char ba, int row_addr, int col_addr, short int wstrb
     }
     if(len) {
       paddr_write(addr, len, wdata);
-      printf("sw_sdram_w: %d, sdram write, addr: 0x%x,  data: 0x%x\n", sw_sdram_w, addr, wdata);
+      printf("trans mode!    sw_sdram_w: %d, sdram write, addr: 0x%x,  data: 0x%x\n", sw_sdram_w, addr, wdata);
 
       switch(sw_sdram_w) {
         case 0: sw_sdram_w = 1; break;
@@ -246,7 +246,7 @@ extern "C" void sdram_write(char ba, int row_addr, int col_addr, short int wstrb
     }
     if(len) {
       paddr_write(addr, len, wdata);
-      printf("sw_sdram_w: %d, sdram write, addr: 0x%x,  data: 0x%x\n", sw_sdram_w, addr, wdata);
+      printf("normal mode!    sw_sdram_w: %d, sdram write, addr: 0x%x,  data: 0x%x\n", sw_sdram_w, addr, wdata);
     }
 
     switch(sw_sdram_w) {
