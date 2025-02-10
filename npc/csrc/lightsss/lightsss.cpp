14eb833aa7984bd03d832bf50939fc9e814e3b04
tracer-ysyx <tracer@ysyx.org>
2025-02-10 13:37:21 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.65-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 11 Dec 2024 15:35:54 +0000 x86_64 GNU/Linux  13:37:21 up 2 days,  5:36,  1 user,  load average: 0.38, 29.47, 45.96

--- npc/csrc/lightsss/lightsss.cpp
+++ npc/csrc/lightsss/lightsss.cpp
@@ -83,8 +83,8 @@ int LightSSS::do_fork() {
 }
 
 int LightSSS::wakeup_child(uint64_t cycles) {
+  if(pidSlot.empty()) return 0;
   forkshm.info->endCycles = cycles;
-  assert(!pidSlot.empty());
   forkshm.info->oldest = pidSlot.back();
 
   // only the oldest is wantted, so kill others by parent process.
