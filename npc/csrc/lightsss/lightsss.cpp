17a37f6a5efbba93c7fd0a2d6dbae62dd61c9aac
tracer-ysyx <tracer@ysyx.org>
2025-02-10 14:46:20 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.65-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 11 Dec 2024 15:35:54 +0000 x86_64 GNU/Linux  14:46:20 up 2 days,  6:45,  1 user,  load average: 0.61, 0.56, 1.07

--- npc/csrc/lightsss/lightsss.cpp
+++ npc/csrc/lightsss/lightsss.cpp
@@ -107,12 +107,8 @@ int LightSSS::wakeup_child(uint64_t cycles) {
   forkshm.info->notgood = true;
   forkshm.info->flag = true;
   int status = -1;
-  printf("old pid:%d\n", pidSlot.back());
-  while (waitpid(forkshm.info->oldest, &status, WNOHANG) == 0) {
-        // std::this_thread::sleep_for(std::chrono::milliseconds(10)); // 短暂休眠以避免CPU过度占用
-        sleep(1);
-    }
-  // waitpid(pidSlot.back(), &status, 0);
+  // printf("old pid:%d\n", pidSlot.back());
+  waitpid(pidSlot.back(), &status, 0);
 
   printf("finish wakeup_child\n");
   return 0;
