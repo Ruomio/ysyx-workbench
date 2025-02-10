954052225be1939d3aaee6f271bb01a7b7a82c8b
tracer-ysyx <tracer@ysyx.org>
2025-02-10 13:12:52 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.65-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 11 Dec 2024 15:35:54 +0000 x86_64 GNU/Linux  13:12:52 up 2 days,  5:11,  1 user,  load average: 1.22, 0.75, 0.43

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -244,7 +244,9 @@ void exec_npc(int n) {
       // break;
       // 检测到结束或异常状态，通知最近的子进程生成波形
       if (u_npc_state.state == NPC_ABORT) {
+        printf("1\n");
         lightsss.wakeup_child(timer_end); // 使用当前的时间作为cycles参数
+        printf("2\n");
       }
 
     case NPC_QUIT: statistic(); break;;
