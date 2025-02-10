61cd24396c7e31c6b574504a1b048b046c0d6e7e
tracer-ysyx <tracer@ysyx.org>
2025-02-10 13:21:41 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.65-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 11 Dec 2024 15:35:54 +0000 x86_64 GNU/Linux  13:21:41 up 2 days,  5:20,  1 user,  load average: 0.46, 0.80, 0.61

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -177,7 +177,7 @@ void exec_once_npc(uint32_t pc) {
 
 void exec_all_npc() {
   while(u_npc_state.state == NPC_RUNNING) {
-    int snapshot_interval_seconds = 5; // 快照间隔时间（秒）
+    int snapshot_interval_seconds = 1; // 快照间隔时间（秒）
       
     auto current_time = std::chrono::steady_clock::now();
     auto elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(current_time - last_snapshot_time).count();
