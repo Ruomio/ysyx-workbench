29b46822233c350d36236bdb66e94e55a6759abb
tracer-ysyx <tracer@ysyx.org>
2025-02-17 15:11:06 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.6.65-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 11 Dec 2024 15:35:54 +0000 x86_64 GNU/Linux  15:11:06 up  5:21,  2 users,  load average: 0.98, 0.94, 0.97

--- npc/vsrc/ysyx_24080020_MEM.v
+++ npc/vsrc/ysyx_24080020_MEM.v
@@ -302,9 +302,8 @@ module ysyx_24080020_MEM(
             arvalid <= 1'b1;
             arid <= 4'b0;
             arlen <= {{7{1'b0}},get_arlen};
-            if((araddr >= 32'h10000000 && araddr < 32'h10001000) || (araddr >= 32'h10002000 && araddr < 32'h1000200f)) begin
-                $display("skip dut");
-                // skip uart, gpio etc.
+            if(araddr >= 32'h10000000 && araddr < 32'h10001000) begin
+                // skip uart
                 npc_difftest_skip_ref();
             end
             else begin
