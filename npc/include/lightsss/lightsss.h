424191f89e97d78ea83cd405ce738622aac1fca6
tracer-ysyx <tracer@ysyx.org>
2025-02-28 09:14:47 +0800

    > run NPC ysyx_24080020 彭培智 Linux archlinux 6.12.16-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 21 Feb 2025 19:20:31 +0000 x86_64 GNU/Linux  09:14:47 up 2 days, 18:58,  3 users,  load average: 1.21, 1.28, 1.18

--- npc/include/lightsss/lightsss.h
+++ npc/include/lightsss/lightsss.h
@@ -71,7 +71,7 @@ public:
   LightSSS() { 
       p_pid = getpid();
       signal(SIGINT, signal_handler); // 注册SIGINT处理器
-      // signal(SIGTERM, signal_handler); // 注册SIGTERM处理器
+      signal(SIGTERM, signal_handler); // 注册SIGTERM处理器
 
   }
   ~LightSSS() {}
