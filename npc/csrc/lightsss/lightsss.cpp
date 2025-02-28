48dbe3f31032b1865b45209b18822fd125095844
tracer-ysyx <tracer@ysyx.org>
2025-02-28 08:51:40 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.16-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 21 Feb 2025 19:20:31 +0000 x86_64 GNU/Linux  08:51:40 up 2 days, 18:35,  3 users,  load average: 0.78, 0.99, 0.81

--- npc/csrc/lightsss/lightsss.cpp
+++ npc/csrc/lightsss/lightsss.cpp
@@ -55,7 +55,7 @@ void ForkShareMemory::shwait() {
 }
 
 void LightSSS::signal_handler(int signum) {
-    forkshm.info->is_p_dead = true;
+    // forkshm.info->is_p_dead = true;
     exit(EXIT_SUCCESS); // 退出当前进程
 }
 
