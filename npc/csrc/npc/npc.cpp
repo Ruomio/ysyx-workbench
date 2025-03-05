403e4ecbeed2064a244f4d64979d906a5cb5f2d0
tracer-ysyx <tracer@ysyx.org>
2025-03-05 17:10:27 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.16-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 21 Feb 2025 19:20:31 +0000 x86_64 GNU/Linux  17:10:27 up 2 days,  8:10,  2 users,  load average: 1.65, 1.68, 1.76

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -260,11 +260,12 @@ void exec_npc(int n) {
       auto elapsed_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - last_snapshot_time).count();
       if (elapsed_seconds >= snapshot_interval_seconds && getpid() == lightsss.get_p_pid()) {
         lightsss.do_fork(); // 创建子进程快照
+        if(getpid() != lightsss.get_p_pid()) {
+          FORK_PRINTF("after shwait to npc pid: %d\n", getpid());
+        }
         last_snapshot_time = current_time;
       }
-      if(getpid() != lightsss.get_p_pid()) {
-        FORK_PRINTF("after shwait to npc pid: %d\n", getpid());
-      }
+
       if(getpid() != lightsss.get_p_pid()) {
         printf("child process , will exit\n");
         // exit(-1);
