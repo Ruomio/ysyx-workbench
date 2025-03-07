eb2617c614786f41725e06f252499c5f93a417b0
tracer-ysyx <tracer@ysyx.org>
2025-03-07 09:59:16 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.16-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 21 Feb 2025 19:20:31 +0000 x86_64 GNU/Linux  09:59:16 up 4 days, 59 min,  2 users,  load average: 1.55, 1.37, 1.20

--- digital-circuit-experiments/PS2_keyboard/vsrc/ps2.v
+++ digital-circuit-experiments/PS2_keyboard/vsrc/ps2.v
@@ -1,65 +1,120 @@
-module ps2_keyboard(
-    input clk, clrn, ps2_clk, ps2_data,
-    input nextdata_n,
-    output reg [7:0] data,
-    output reg ready,
-    output reg overflow
-);
-
-    reg [9:0] buffer;
-    reg [7:0] fifo[7:0];
-    reg [2:0] w_ptr, r_ptr;
-    reg [3:0] count;
+module ps2_keyboard(clk,clrn,ps2_clk,ps2_data,data,
+                    ready,nextdata_n,overflow);
+    input clk,clrn,ps2_clk,ps2_data;
+    input nextdata_n;
+    output [7:0] data;
+    output reg ready;
+    output reg overflow;     // fifo overflow
+    // internal signal, for test
+    reg [9:0] buffer;        // ps2_data bits
+    reg [7:0] fifo[7:0];     // data fifo
+    reg [2:0] w_ptr,r_ptr;   // fifo write and read pointers
+    reg [3:0] count;  // count ps2_data bits
+    // detect falling edge of ps2_clk
     reg [2:0] ps2_clk_sync;
 
-
     always @(posedge clk) begin
-        ps2_clk_sync <= {ps2_clk_sync[1:0], ps2_clk};
+        ps2_clk_sync <=  {ps2_clk_sync[1:0],ps2_clk};
     end
-
 
     wire sampling = ps2_clk_sync[2] & ~ps2_clk_sync[1];
 
     always @(posedge clk) begin
-        if(clrn ==0 ) begin
-            count <= 0;
-            w_ptr <= 0;
-            r_ptr <= 0;
-            overflow <= 0;
-            ready <= 0;
+        if (clrn == 0) begin // reset
+            count <= 0; w_ptr <= 0; r_ptr <= 0; overflow <= 0; ready<= 0;
         end
         else begin
-            if( ready ) begin
-                if(nextdata_n == 1'b0) begin
-                    r_ptr <= r_ptr + 1'b1;
-                    if(r_ptr + 1'b1 == w_ptr) begin
+            if ( ready ) begin // read to output next data
+                if(nextdata_n == 1'b0) //read next data
+                begin
+                    r_ptr <= r_ptr + 3'b1;
+                    if(w_ptr==(r_ptr+1'b1)) //empty
                         ready <= 1'b0;
-                    end
                 end
             end
-
-            if(sampling) begin
-                if(count == 4'b1010) begin
-                    if((buffer[0] == 0) && ps2_data && (^buffer[9:1])) begin
-                        fifo[w_ptr] <= buffer[8:1];
-                        w_ptr <= w_ptr + 3'b1;
-                        ready <= 1'b1;
-                        overflow <= (r_ptr == w_ptr + 3'b1);
-
-                    end
-                count <= 4'b0;
+            if (sampling) begin
+              if (count == 4'd10) begin
+                if ((buffer[0] == 0) &&  // start bit
+                    (ps2_data)       &&  // stop bit
+                    (^buffer[9:1])) begin      // odd  parity
+                    fifo[w_ptr] <= buffer[8:1];  // kbd scan code
+                    w_ptr <= w_ptr+3'b1;
+                    ready <= 1'b1;
+                    overflow <= overflow | (r_ptr == (w_ptr + 3'b1));
                 end
-                else begin 
-                    buffer[count] <= ps2_data;
-                    count <= count + 4'b1;
-                end
+                count <= 0;     // for next
+              end else begin
+                buffer[count] <= ps2_data;  // store ps2_data
+                count <= count + 3'b1;
+              end
             end
         end
     end
-
-    assign data = fifo[r_ptr];
+    assign data = fifo[r_ptr]; //always set output data
 
 endmodule
+// module ps2_keyboard(
+//     input clk, clrn, ps2_clk, ps2_data,
+//     input nextdata_n,
+//     output reg [7:0] data,
+//     output reg ready,
+//     output reg overflow
+// );
+// 
+//     reg [9:0] buffer;
+//     reg [7:0] fifo[7:0];
+//     reg [2:0] w_ptr, r_ptr;
+//     reg [3:0] count;
+//     reg [2:0] ps2_clk_sync;
+// 
+// 
+//     always @(posedge clk) begin
+//         ps2_clk_sync <= {ps2_clk_sync[1:0], ps2_clk};
+//     end
+// 
+// 
+//     wire sampling = ps2_clk_sync[2] & ~ps2_clk_sync[1];
+// 
+//     always @(posedge clk) begin
+//         if(clrn ==0 ) begin
+//             count <= 0;
+//             w_ptr <= 0;
+//             r_ptr <= 0;
+//             overflow <= 0;
+//             ready <= 0;
+//         end
+//         else begin
+//             if( ready ) begin
+//                 if(nextdata_n == 1'b0) begin
+//                     r_ptr <= r_ptr + 1'b1;
+//                     if(r_ptr + 1'b1 == w_ptr) begin
+//                         ready <= 1'b0;
+//                     end
+//                 end
+//             end
+// 
+//             if(sampling) begin
+//                 if(count == 4'b1010) begin
+//                     if((buffer[0] == 0) && ps2_data && (^buffer[9:1])) begin
+//                         fifo[w_ptr] <= buffer[8:1];
+//                         w_ptr <= w_ptr + 3'b1;
+//                         ready <= 1'b1;
+//                         overflow <= (r_ptr == w_ptr + 3'b1);
+// 
+//                     end
+//                 count <= 4'b0;
+//                 end
+//                 else begin 
+//                     buffer[count] <= ps2_data;
+//                     count <= count + 4'b1;
+//                 end
+//             end
+//         end
+//     end
+// 
+//     assign data = fifo[r_ptr];
+// 
+// endmodule
 
 
 // module ps2_model(
