208c5f5f60616b09a66e1fcb7cc9531bfa7957dd
tracer-ysyx <tracer@ysyx.org>
2025-03-12 15:19:24 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.16-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 21 Feb 2025 19:20:31 +0000 x86_64 GNU/Linux  15:19:24 up 2 days,  6:31,  1 user,  load average: 0.35, 0.62, 0.65

--- npc/vsrc/ysyx_24080020_MEM.v
+++ npc/vsrc/ysyx_24080020_MEM.v
@@ -305,6 +305,7 @@ module ysyx_24080020_MEM(
             if(araddr >= 32'h10000000 && araddr < 32'h10001000 
                 || araddr >= 32'h10011000 && araddr < 32'h10011008
                 || araddr >= 32'h21000000 && araddr < 32'h21200000
+                || araddr >= 32'h02000000 && araddr < 32'h02000008
                 ) begin
                 // skip uart keyboard etc.
                 npc_difftest_skip_ref();
@@ -408,6 +409,7 @@ module ysyx_24080020_MEM(
             if(awaddr >= 32'h10000000 && awaddr < 32'h10001000
                 || araddr >= 32'h10011000 && araddr < 32'h10011008
                 || araddr >= 32'h21000000 && araddr < 32'h21200000 
+                || araddr >= 32'h02000000 && araddr < 32'h02000008
                 ) begin
                 // skip uart keyboard etc.
                 npc_difftest_skip_ref();
