5b2f4dbfa2c0b6b48f544daaa155676afe9323fb
tracer-ysyx <tracer@ysyx.org>
2025-03-12 13:15:53 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.16-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 21 Feb 2025 19:20:31 +0000 x86_64 GNU/Linux  13:15:53 up 2 days,  4:27,  1 user,  load average: 0.69, 0.57, 0.41

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -206,24 +206,6 @@ void exec_once_npc(uint32_t pc) {
   trace_and_difftest(g_pc);
 }
 
-// void exec_all_npc() {
-//   while(u_npc_state.state == NPC_RUNNING) {
-// #ifdef CONFIG_LIGHTSSS
-//     int snapshot_interval_seconds = 200; // 快照间隔时间（ms）
-//       
-//     auto current_time = std::chrono::steady_clock::now();
-//     auto elapsed_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - last_snapshot_time).count();
-//     // printf("%ld, %ld, %ld\n", last_snapshot_time, current_time, elapsed_seconds);
-//     if (elapsed_seconds >= snapshot_interval_seconds && getpid() == lightsss.get_p_pid()) {
-//       lightsss.do_fork(); // 创建子进程快照
-//       last_snapshot_time = current_time;
-//     }
-// #endif
-// 
-//     exec_once_npc(g_pc);
-//   }
-// }
-
 static void statistic() {
   IFNDEF(CONFIG_TARGET_AM, setlocale(LC_NUMERIC, ""));
   IFDEF(CONFIG_DTRACE_COND, dtrace_free());
