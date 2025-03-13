b6bc6ceac1c6c83886fc3d7d00c863e98b1c737c
tracer-ysyx <tracer@ysyx.org>
2025-03-13 16:37:26 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.16-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 21 Feb 2025 19:20:31 +0000 x86_64 GNU/Linux  16:37:26 up 3 days,  7:49,  1 user,  load average: 2.32, 2.65, 2.30

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -226,6 +226,7 @@ static void statistic() {
   Log("total guest instructions = " NUMBERIC_FMT, g_nr_guest_inst);
   if (g_timer > 0) Log("simulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
   else Log("Finish running in less than 1 us and can not calculate the simulation frequency");
+  Log("IPC = " NUMBERIC_FMT, g_nr_guest_inst / total_cycles);
 }
 
 void exec_npc(uint64_t n) {
