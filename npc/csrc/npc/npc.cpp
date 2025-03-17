60ef4d34e3373f9284ee0b93fd81c92e13095e33
tracer-ysyx <tracer@ysyx.org>
2025-03-17 16:37:03 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.19-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 15:20:33 +0000 x86_64 GNU/Linux  16:37:03 up  8:01,  1 user,  load average: 0.62, 0.78, 0.73

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -252,9 +252,9 @@ static void statistic() {
   Log("lsu_get_data_cnt = " NUMBERIC_FMT, lsu_get_data_cnt);
   Log("exu_complete_culca_cnt = " NUMBERIC_FMT, exu_complete_calcu_cnt);
   Log("idu_calcu_type_cnt = " NUMBERIC_FMT " Percentage: %.2f%%  Average: %ld", idu_calculate_type_cnt, idu_calculate_type_cnt * 100.0 / ifu_get_inst_cnt, idu_calculate_cycles / idu_calculate_type_cnt);
-  Log("idu_load_store_type_cnt = " NUMBERIC_FMT "Percentage: %.2f%%  Average: %ld", idu_load_store_type_cnt, idu_load_store_type_cnt * 100.0 / ifu_get_inst_cnt, idu_load_store_cycles / idu_load_store_type_cnt);
-  Log("idu_csr_type_cnt = " NUMBERIC_FMT "Percentage: %.2f%%,  Average: %ld", idu_csr_type_cnt, idu_csr_type_cnt * 100.0 / ifu_get_inst_cnt, idu_csr_cycles / idu_csr_type_cnt);
-  Log("idu_jump_type_cnt = " NUMBERIC_FMT "Percentage: %.2f%%,  Average: %ld", idu_jump_type_cnt, idu_jump_type_cnt * 100.0 / ifu_get_inst_cnt, idu_jump_cycles / idu_jump_type_cnt);
+  Log("idu_load_store_type_cnt = " NUMBERIC_FMT " Percentage: %.2f%%  Average: %ld", idu_load_store_type_cnt, idu_load_store_type_cnt * 100.0 / ifu_get_inst_cnt, idu_load_store_cycles / idu_load_store_type_cnt);
+  Log("idu_csr_type_cnt = " NUMBERIC_FMT " Percentage: %.2f%%,  Average: %ld", idu_csr_type_cnt, idu_csr_type_cnt * 100.0 / ifu_get_inst_cnt, idu_csr_cycles / idu_csr_type_cnt);
+  Log("idu_jump_type_cnt = " NUMBERIC_FMT " Percentage: %.2f%%,  Average: %ld", idu_jump_type_cnt, idu_jump_type_cnt * 100.0 / ifu_get_inst_cnt, idu_jump_cycles / idu_jump_type_cnt);
 }
 
 void exec_npc(uint64_t n) {
