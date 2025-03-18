5a3b66bf3e45bf7b1685eac25393d5046a7520c8
tracer-ysyx <tracer@ysyx.org>
2025-03-18 13:33:32 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.19-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 15:20:33 +0000 x86_64 GNU/Linux  13:33:32 up 1 day,  4:57,  2 users,  load average: 0.61, 0.59, 0.55

--- npc/vsrc/ysyx_24080020_PC.v
+++ npc/vsrc/ysyx_24080020_PC.v
@@ -9,28 +9,28 @@ module ysyx_24080020_PC (
     output reg [`ysyx_24080020_WIDTH-1:0] addr
 );
 
-    // reg cnt;
+    reg delay;
     reg first_if;
 
     always @(posedge clk) begin
         if(!rst) begin
             addr <= `ysyx_24080020_MBASE;
-            // cnt <= 1'b0;
+            delay <= 1'b0;
             first_if <= 1'b0;
         end
         else if(is_update_pc) begin
-            // if(cnt == 1'b1) begin
+            if(delay == 1'b1) begin
                 addr <= is_dnpc ? dnpc :
                         ~first_if ? addr : addr + 32'd4;
                 if_en <= 1'b1;
                 first_if <= 1'b1;
 
                 // pc_ifu <= addr;
-                // cnt <= 1'b0;
-            // end
-            // else begin
-            //     // cnt <= 1'b1;
-            // end
+                delay <= 1'b0;
+            end
+            else begin
+                delay <= 1'b1;
+            end
         end
         else begin
             // addr <= addr;
