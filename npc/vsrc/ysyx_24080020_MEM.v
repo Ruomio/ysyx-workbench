bde73da9b1d37c2227d836c9908ecd2a42aac663
tracer-ysyx <tracer@ysyx.org>
2025-03-19 11:00:43 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.19-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 15:20:33 +0000 x86_64 GNU/Linux  11:00:43 up 2 days,  2:24,  2 users,  load average: 1.75, 1.31, 1.18

--- npc/vsrc/ysyx_24080020_MEM.v
+++ npc/vsrc/ysyx_24080020_MEM.v
@@ -109,6 +109,8 @@ module ysyx_24080020_MEM(
 
     reg state; // 0: idle;   1: wait_ready
 
+    reg delay;
+
     wire [31:0] rdata_shift_1, rdata_shift_2, rdata_shift;
     wire get_arlen;
     wire get_awlen;
@@ -339,10 +341,12 @@ module ysyx_24080020_MEM(
             rready <= 1'b0;
             mrdata_mem <= 32'b0;
             arlen_cnt <= 1'b0;
+            delay <= 1'b0;
         end
-        else if(rvalid && rlast) begin
+        else if(rvalid && rlast && !delay) begin
             // finish all read
             rready <= 1'b1;
+            delay <= 1'b1;
 
             if(rresp != 2'b0) begin
                 // rresp fault;
@@ -406,6 +410,7 @@ module ysyx_24080020_MEM(
         end
         else begin
             rready <= 1'b0;
+            delay <= 1'b0;
         end
     end
 
