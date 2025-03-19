ff84767c355bc539717be77b51e5790e79c33560
24080020-Peizhi Peng <1065940593@qq.com>
2025-03-19 11:06:13 +0800

    npc: fix lsu_get_data_cnt

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -251,7 +251,7 @@ static void statistic() {
   else Log("Finish running in less than 1 us and can not calculate the simulation frequency");
   Log("total_cycles = " NUMBERIC_FMT "  IPC = %lf", total_cycles, ((double)g_nr_guest_inst / total_cycles));
   Log("ifu_get_inst_cnt = " NUMBERIC_FMT, ifu_get_inst_cnt);
-  Log("lsu_get_data_cnt = " NUMBERIC_FMT, lsu_get_data_cnt);
+  Log("lsu_get_data_cnt = " NUMBERIC_FMT, lsu_get_data_cnt / 2);
   Log("exu_complete_culca_cnt = " NUMBERIC_FMT, exu_complete_calcu_cnt);
   Log("idu_calcu_type_cnt = " NUMBERIC_FMT " Percentage: %.2f%%  Average: %ld", idu_calculate_type_cnt, idu_calculate_type_cnt * 100.0 / ifu_get_inst_cnt, idu_calculate_cycles / idu_calculate_type_cnt);
   Log("idu_load_type_cnt = " NUMBERIC_FMT " Percentage: %.2f%%  Average: %ld", idu_load_type_cnt, idu_load_type_cnt * 100.0 / ifu_get_inst_cnt, idu_load_cycles / idu_load_type_cnt);
