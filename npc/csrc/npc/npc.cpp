d6553705cd0b5f4ffff5d89b5f640650272d3115
tracer-ysyx <tracer@ysyx.org>
2025-03-19 10:46:27 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.19-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 15:20:33 +0000 x86_64 GNU/Linux  10:46:27 up 2 days,  2:10,  2 users,  load average: 0.79, 0.85, 1.01

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -459,6 +459,7 @@ extern "C" void statistics_ifu_get_inst() {
 }
 
 extern "C" void statistics_lsu_get_data() {
+  printf("lsu_get_data_cnt: %ld  pc: 0x%x \n", lsu_get_data_cnt, g_pc);
   lsu_get_data_cnt ++;
 }
 
