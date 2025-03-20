839f291bcda996d6409433a524781e1f9f57f59d
tracer-ysyx <tracer@ysyx.org>
2025-03-20 13:26:10 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.19-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 15:20:33 +0000 x86_64 GNU/Linux  13:26:10 up 3 days,  4:50,  2 users,  load average: 0.74, 0.79, 0.72

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -154,7 +154,7 @@ void exec_once_npc(uint32_t pc) {
     top->clock ^= 1;
     top->eval();
 #ifdef CONFIG_WAVEFILE
-    if(total_wave_step >= CONFIG_BASE_WAVE_STEP && total_wave_step < CONFIG_BASE_WAVE_STEP + CONFIG_MAX_WAVE_STEP) {
+    if(total_wave_step > CONFIG_BASE_WAVE_STEP && total_wave_step < CONFIG_BASE_WAVE_STEP + CONFIG_MAX_WAVE_STEP) {
       total_wave_step++;
       tfp->dump(contextp->time());
       contextp->timeInc(1);
@@ -327,6 +327,7 @@ void exec_npc(uint64_t n) {
 }
 
 void free_npc() {
+  printf("free npc\n");
   IFDEF(CONFIG_MTRACE, MtraceBuf_add_arrow(); MtraceBuf_save());
   IFDEF(CONFIG_FTRACE, close_ftrace());
   if(top) {
