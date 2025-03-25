20a45ce098822729333402433f19a27725de1244
tracer-ysyx <tracer@ysyx.org>
2025-03-25 14:03:37 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.19-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 15:20:33 +0000 x86_64 GNU/Linux  14:03:37 up 1 day,  5:43,  1 user,  load average: 0.61, 0.66, 0.62

--- npc/vsrc/ysyx_24080020_MEM.v
+++ npc/vsrc/ysyx_24080020_MEM.v
@@ -327,6 +327,7 @@ module ysyx_24080020_MEM(
             `endif
             `ifdef ysyx_24080020_NPC
             if(araddr >= 32'ha00003f8 && araddr < 32'ha0000400 
+                araddr >= 32'ha0000048 && araddr < 32'ha000050
                 ) begin
                 // skip uart keyboard etc.
                 `ifdef CONFIG_DPIC
@@ -460,6 +461,7 @@ module ysyx_24080020_MEM(
             `endif
             `ifdef ysyx_24080020_NPC
             if(awaddr >= 32'ha00003f8 && awaddr < 32'ha0000400
+                awaddr >= 32'ha0000048 && awaddr < 32'ha000050
                 ) begin
                 // skip uart keyboard etc.
                 `ifdef CONFIG_DPIC
