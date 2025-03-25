e7d0d7d602916683369b0968a57943c6ff0a536f
tracer-ysyx <tracer@ysyx.org>
2025-03-25 13:17:06 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.19-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 15:20:33 +0000 x86_64 GNU/Linux  13:17:06 up 1 day,  4:56,  1 user,  load average: 0.47, 0.58, 0.46

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -79,6 +79,7 @@ paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
+  printf("addr: 0x%x, len: %d, rdata: 0x%x\n", addr, len, ret);
   return ret;
 }
 
