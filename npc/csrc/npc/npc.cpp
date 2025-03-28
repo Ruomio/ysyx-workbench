893cf5555499edf9b08dd6b6617ed7cf61a202a0
tracer-ysyx <tracer@ysyx.org>
2025-03-28 10:20:55 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.19-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 13 Mar 2025 15:20:33 +0000 x86_64 GNU/Linux  10:20:55 up 4 days,  2:00,  1 user,  load average: 1.15, 1.12, 0.93

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -292,7 +292,7 @@ static void statistic() {
 
   if(!ifu_get_inst_cnt || !idu_jump_type_cnt || !idu_csr_type_cnt || !idu_store_type_cnt || !idu_load_type_cnt || !idu_calculate_type_cnt ) return;
   Log("ifu_get_inst_cnt = " NUMBERIC_FMT " Average: %ld", ifu_get_inst_cnt, ifu_get_inst_cycles / ifu_get_inst_cnt);
-  Log("ifu_icache_hit_cnt = " NUMBERIC_FMT " Percentage: %.2f%%, Amat: %.2lf", ifu_icache_hit_cnt / 2, ifu_icache_hit_cnt * 100.0 / (2 * ifu_get_inst_cnt), 3*(ifu_icache_hit_cnt * 100.0 / (2 * ifu_get_inst_cnt)) + 4932*(1-(ifu_icache_hit_cnt * 1.0 / (2 * ifu_get_inst_cnt))) );
+  Log("ifu_icache_hit_cnt = " NUMBERIC_FMT " Percentage: %.2f%%, AMAT: %.2lf", ifu_icache_hit_cnt / 2, ifu_icache_hit_cnt * 100.0 / (2 * ifu_get_inst_cnt), 3*(ifu_icache_hit_cnt * 100.0 / (2 * ifu_get_inst_cnt)) + 4932*(1-(ifu_icache_hit_cnt * 1.0 / (2 * ifu_get_inst_cnt))) );
   Log("lsu_get_data_cnt = " NUMBERIC_FMT, lsu_get_data_cnt / 2);
   Log("exu_complete_culca_cnt = " NUMBERIC_FMT, exu_complete_calcu_cnt);
   Log("idu_calcu_type_cnt = " NUMBERIC_FMT " Percentage: %.2f%%  Average: %ld", idu_calculate_type_cnt, idu_calculate_type_cnt * 100.0 / ifu_get_inst_cnt, idu_calculate_cycles / idu_calculate_type_cnt);
