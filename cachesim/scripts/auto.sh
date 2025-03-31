7ec3f161b94f4fe4f04d312d771494f51eb9aceb
tracer-ysyx <tracer@ysyx.org>
2025-03-31 15:01:51 +0800

    > run CacheSim ysyx_24080020 彭培智 Linux archlinux 6.12.21-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 29 Mar 2025 06:39:46 +0000 x86_64 GNU/Linux  15:01:51 up  6:52,  1 user,  load average: 1.08, 0.75, 0.62

---
+++ cachesim/scripts/auto.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# set -x
+
+# 获取脚本所在的目录
+SCRIPT_DIR="${NEMU_HOME}/../cachesim"
+
+BIN=${SCRIPT_DIR}/build/cachesim
+INSTS=${NEMU_HOME}/build/insts.txt
+
+SIZES=(4 8 16 32 64)
+NUMS=(16 32 64 128)
+# SIZES=(4)
+# NUMS=(16)
+
+# 批量运行
+for size in "${SIZES[@]}"; do
+    for num in "${NUMS[@]}"; do
+        echo "Running with SIZE=$size and NUM=$num..."
+        
+        # 运行命令
+        "$BIN" "${INSTS}" $size $num
+        
+        echo ""
+    done
+done
+
+echo "All instances completed."
