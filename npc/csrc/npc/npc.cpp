3f886cba9897d14c7fd15c07e2fccde5bccf7d14
tracer-ysyx <tracer@ysyx.org>
2025-04-01 14:03:50 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.21-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 29 Mar 2025 06:39:46 +0000 x86_64 GNU/Linux  14:03:50 up 1 day,  5:54,  1 user,  load average: 1.47, 1.39, 1.29

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -326,7 +326,6 @@ void exec_npc(uint64_t n) {
   uint64_t timer_start = get_time();
 
 
-  if(lightsss.is_child()) FORK_PRINTF("child exec\n");
 
   for(; n>0; n--) {
 #ifdef CONFIG_LIGHTSSS
@@ -339,6 +338,7 @@ void exec_npc(uint64_t n) {
       last_snapshot_time = current_time;
     }
 #endif
+    if(lightsss.is_child()) FORK_PRINTF("child exec\n");
 
     if (u_npc_state.state != NPC_RUNNING) break;
     exec_once_npc(g_pc);
