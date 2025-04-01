b2617b134acc85c1565ae58e5727238dac4a8a35
tracer-ysyx <tracer@ysyx.org>
2025-04-01 11:19:49 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.21-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 29 Mar 2025 06:39:46 +0000 x86_64 GNU/Linux  11:19:49 up 1 day,  3:10,  1 user,  load average: 1.11, 1.10, 1.07

--- npc/vsrc/ysyx_24080020_IR.v
+++ npc/vsrc/ysyx_24080020_IR.v
@@ -67,7 +67,7 @@ module ysyx_24080020_IR(
     assign cache_index_tmp = addr[cache_num_bits+cache_size_bits-1 : cache_size_bits];
     assign cache_offset_tmp = addr[cache_size_bits-1 : 0];
 
-    assign clear_mask = {cache_data_width{1'b1}} & {{data_complete_bits{1'b0}} ,32'b0} << (cache_offset_tmp * 8);
+    assign clear_mask = {{data_complete_bits{1'b0}} ,~32'b0} << (cache_offset_tmp * 8);
 
     assign shift_rdata = cache_data[cache_index_tmp] >> (cache_offset_tmp * 8);
     assign shift_wdata = ({{data_complete_bits{1'b0}}, rdata} << (cache_offset_tmp * 8));
@@ -207,7 +207,7 @@ module ysyx_24080020_IR(
                         if(use_icache) begin
                             // update cache
                             cache_tag[cache_index_tmp] <= cache_tag_tmp;
-                            cache_data[cache_index_tmp] <= (cache_data[cache_index_tmp] & clear_mask) | shift_wdata;
+                            cache_data[cache_index_tmp] <= (cache_data[cache_index_tmp] & ~clear_mask) | shift_wdata;
                             cache_valid[cache_index_tmp] <= cache_valid[cache_index_tmp] | (1 << cache_offset_tmp);
                         end
 
