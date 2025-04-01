5520d11689e48338f08530bfbbc7b13d2affdcae
tracer-ysyx <tracer@ysyx.org>
2025-04-01 10:51:43 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.21-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 29 Mar 2025 06:39:46 +0000 x86_64 GNU/Linux  10:51:43 up 1 day,  2:42,  1 user,  load average: 1.32, 1.13, 1.07

--- npc/vsrc/ysyx_24080020_IR.v
+++ npc/vsrc/ysyx_24080020_IR.v
@@ -66,8 +66,8 @@ module ysyx_24080020_IR(
     assign cache_index_tmp = addr[cache_num_bits+cache_size_bits-1 : cache_size_bits];
     assign cache_offset_tmp = addr[cache_size_bits-1 : 0];
 
-    assign shift_rdata = cache_data[cache_index_tmp] >> (cache_offset_tmp * 4);
-    assign shift_wdata = ({{data_complete_bits{1'b0}}, rdata} << (cache_offset_tmp * 4));
+    assign shift_rdata = cache_data[cache_index_tmp] >> (cache_offset_tmp * 32);
+    assign shift_wdata = ({{data_complete_bits{1'b0}}, rdata} << (cache_offset_tmp * 32));
 
     assign cache_hit = ((cache_tag[cache_index_tmp] == cache_tag_tmp)
                         && (cache_valid[cache_index_tmp] >> cache_offset_tmp) == ({cache_size_bits{1'b0}} | 'b1));
