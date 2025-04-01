7c5d218bc1461a0948efd67ce977baeb333bd4b3
tracer-ysyx <tracer@ysyx.org>
2025-04-01 10:16:32 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.21-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 29 Mar 2025 06:39:46 +0000 x86_64 GNU/Linux  10:16:32 up 1 day,  2:06,  1 user,  load average: 1.19, 1.07, 0.95

--- npc/vsrc/ysyx_24080020_IR.v
+++ npc/vsrc/ysyx_24080020_IR.v
@@ -48,6 +48,8 @@ module ysyx_24080020_IR(
     localparam cache_tag_size = 32 - cache_size_bits - cache_num_bits;
     localparam cache_data_width = 8 * `ysyx_24080020_CACHE_SIZE;
 
+    wire [cache_data_width-1 : 0] shift_data;
+
     wire                                    cache_hit;
     wire [cache_tag_size-1:0]               cache_tag_tmp;
     wire [cache_num_bits-1:0]               cache_index_tmp;
@@ -61,6 +63,8 @@ module ysyx_24080020_IR(
     assign cache_tag_tmp = addr[31 : cache_num_bits+cache_size_bits];
     assign cache_index_tmp = addr[cache_num_bits+cache_size_bits-1 : cache_size_bits];
     assign cache_offset_tmp = addr[cache_size_bits-1 : 0];
+
+    assign shift_data = cache_data[cache_index_tmp] >> cache_offset_tmp;
 
     assign cache_hit = ((cache_tag[cache_index_tmp] == cache_tag_tmp)
                         && (cache_valid[cache_index_tmp] != 'b0));
@@ -164,7 +168,8 @@ module ysyx_24080020_IR(
                 `endif
             end
             CHIT: begin
-                inst <= (cache_data[cache_index_tmp] >> cache_offset_tmp);
+                // inst <= (cache_data[cache_index_tmp] >> cache_offset_tmp);
+                inst <= shift_data[31:0];
                 inst_fin <= 'b1;
 
                 `ifdef CONFIG_DPIC
