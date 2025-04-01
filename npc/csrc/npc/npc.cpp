fb2f9c2e91508175fbe49f9191c3144e24ff65c7
tracer-ysyx <tracer@ysyx.org>
2025-04-01 11:30:50 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.21-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 29 Mar 2025 06:39:46 +0000 x86_64 GNU/Linux  11:30:50 up 1 day,  3:21,  1 user,  load average: 1.31, 1.27, 1.24

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -77,7 +77,7 @@ static uint32_t last_pc;
 static bool g_print_step = false;
 uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
-static uint32_t total_wave_step = 0; 
+static uint32_t total_wave_step = 0;
 static uint64_t total_cycles = 0;
 static uint64_t wait_cycles = 0;
 static uint64_t ifu_get_inst_cnt = 0;
@@ -198,7 +198,7 @@ void exec_once_npc(uint32_t pc) {
       tfp->dump(contextp->time());
       contextp->timeInc(1);
     }
-    else 
+    else
       total_wave_step++;
 #endif
 #ifdef CONFIG_LIGHTSSS
@@ -328,7 +328,7 @@ void exec_npc(uint64_t n) {
   for(; n>0; n--) {
 #ifdef CONFIG_LIGHTSSS
     int snapshot_interval_seconds = 200; // 快照间隔时间（ms）
-    
+
     auto current_time = std::chrono::steady_clock::now();
     auto elapsed_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - last_snapshot_time).count();
     if (elapsed_seconds >= snapshot_interval_seconds && !lightsss.is_child()) {
@@ -359,8 +359,8 @@ void exec_npc(uint64_t n) {
       }
 #endif
 
-    case NPC_QUIT: 
-      statistic(); 
+    case NPC_QUIT:
+      statistic();
 #if NVBOARD_ENABLE
     nvboard_quit();
 #endif
@@ -370,7 +370,10 @@ void exec_npc(uint64_t n) {
         lightsss.do_clear();
       }
       else {
-        exit(-1);
+        u_npc_state.state = NPC_QUIT;
+        u_npc_state.ret = true;
+        u_npc_state.pc = g_pc;
+        // exit(-1);
       }
 #endif
       break;
