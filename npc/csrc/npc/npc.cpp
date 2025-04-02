f5e47ea0f04ff649afcad7c3b0f79bd7d03729f7
tracer-ysyx <tracer@ysyx.org>
2025-04-02 15:11:53 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.21-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 29 Mar 2025 06:39:46 +0000 x86_64 GNU/Linux  15:11:53 up 2 days,  7:02,  1 user,  load average: 1.27, 0.97, 0.88

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -377,7 +377,7 @@ void exec_npc(uint64_t n) {
       else {
         FORK_PRINTF("Line: %d\n", __LINE__);
         // exit(-1);
-        return;
+        // return;
       }
 #endif
 #if NVBOARD_ENABLE
