0e4927ea597d15b240dfbeda67c8f67f6fd40601
tracer-ysyx <tracer@ysyx.org>
2025-04-02 14:33:37 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.21-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 29 Mar 2025 06:39:46 +0000 x86_64 GNU/Linux  14:33:37 up 2 days,  6:23,  1 user,  load average: 1.14, 1.02, 0.91

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -222,7 +222,7 @@ void exec_once_npc(uint32_t pc) {
       } 
 
       // test lightsss
-      if(total_cycles > 25000) {
+      if(total_cycles > 100000) {
         printf("Warning: total_cycles is 15000\n");
         u_npc_state.state = NPC_ABORT;
         u_npc_state.pc = pc;
