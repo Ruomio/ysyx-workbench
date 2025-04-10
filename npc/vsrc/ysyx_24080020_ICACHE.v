e422fc5a50eccbf119bd10397fed57c5820f28ef
24080020-Peizhi Peng <1065940593@qq.com>
2025-04-10 16:36:41 +0800

    npc: update icache bugs

--- npc/vsrc/ysyx_24080020_ICACHE.v
+++ npc/vsrc/ysyx_24080020_ICACHE.v
@@ -265,11 +265,6 @@ module ysyx_24080020_ICACHE(
               end
               else if(!fin_ar) begin
                   arvalid_icache_o <= 1'b1;
-                  // araddr_icache_o <= ;
-                  arid_icache_o <= 4'b0;
-                  arlen_icache_o <= 8'b0;
-                  arsize_icache_o <= 3'b10; // 4Byte
-                  arburst_icache_o <= 2'b00; // FIXED
               end
           end
           AXIR: begin
