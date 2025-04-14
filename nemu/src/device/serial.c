4bee3840ad7044a27ff9dfa2800214550cecb54f
tracer-ysyx <tracer@ysyx.org>
2025-04-14 11:06:39 +0800

    > run NEMU ysyx_24080020 彭培智 Linux archlinux 6.12.23-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 10 Apr 2025 13:28:36 +0000 x86_64 GNU/Linux  11:06:39 up 17:07,  2 users,  load average: 1.72, 1.39, 1.30

--- nemu/src/device/serial.c
+++ nemu/src/device/serial.c
@@ -27,6 +27,9 @@ static uint8_t *serial_base = NULL;
 static void serial_putc(char ch) {
   MUXDEF(CONFIG_TARGET_AM, putch(ch), putc(ch, stderr));
 }
+static void serial_getch(uint8_t *ch) {
+  scanf("%c", ch);
+}
 
 static void serial_io_handler(uint32_t offset, int len, bool is_write) {
   assert(len == 1);
@@ -34,7 +37,10 @@ static void serial_io_handler(uint32_t offset, int len, bool is_write) {
     /* We bind the serial port with the host stderr in NEMU. */
     case CH_OFFSET:
       if (is_write) serial_putc(serial_base[0]);
-      else panic("do not support read");
+      else {
+        serial_getch(&(serial_base[0]));
+        // panic("do not support read");
+      }
       break;
     default: panic("do not support offset = %d", offset);
   }
