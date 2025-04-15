ba4885d37f46f8a4229e429ee4018eff345788de
tracer-ysyx <tracer@ysyx.org>
2025-04-15 11:07:38 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.23-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 10 Apr 2025 13:28:36 +0000 x86_64 GNU/Linux  11:07:38 up 1 day, 17:08,  2 users,  load average: 1.19, 1.01, 0.96

--- npc/vsrc/ysyx_24080020_MEM.v
+++ npc/vsrc/ysyx_24080020_MEM.v
@@ -426,14 +426,14 @@ module ysyx_24080020_MEM(
         else if(awvalid && awready) begin
             awvalid <= 1'b0;
 
-            mwen_mem <= 1'b0;
+            // mwen_mem <= 1'b0;
         end
         else if(mwen_mem) begin
             awvalid <= 1'b1;
             awid <= 4'b0;
             awlen <= {{7{1'b0}}, get_awlen};
 
-            // mwen_mem <= 1'b0;
+            mwen_mem <= 1'b0;
             `ifdef ysyxSoCFull
             if(awaddr >= 32'h10000000 && awaddr < 32'h10001000
                 || awaddr >= 32'h10011000 && awaddr < 32'h10011008
