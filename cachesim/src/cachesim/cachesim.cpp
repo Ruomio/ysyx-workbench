ad16715dca5ba2d6e5f7b3f7a328d5ddeb858e8d
tracer-ysyx <tracer@ysyx.org>
2025-04-17 14:38:58 +0800

    > run CacheSim ysyx_24080020 彭培智 Linux archlinux 6.12.23-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 10 Apr 2025 13:28:36 +0000 x86_64 GNU/Linux  14:38:58 up 3 days, 20:39,  2 users,  load average: 1.59, 1.55, 1.45

--- cachesim/src/cachesim/cachesim.cpp
+++ cachesim/src/cachesim/cachesim.cpp
@@ -70,7 +70,7 @@ bool CacheSim::is_cachehit(uint32_t address) {
       }
     }
     else {
-      printf("cache miss! address: 0x%x, fifo_index:%d, valid: %d, tag: %d, data: 0x%x\n",address, i, (int)cache_valid[index][i], tag, (*cache_data)[index][i][offset]);
+      printf("cache miss! address: 0x%x, fifo_index:%d \n",address, i);
     }
   }
   return valid && is_tag_same;
