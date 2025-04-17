cd5055bd66b657402d035993d498a5fd2d017988
tracer-ysyx <tracer@ysyx.org>
2025-04-17 13:28:49 +0800

    > run CacheSim ysyx_24080020 彭培智 Linux archlinux 6.12.23-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 10 Apr 2025 13:28:36 +0000 x86_64 GNU/Linux  13:28:49 up 3 days, 19:29,  2 users,  load average: 1.64, 1.43, 1.41

--- cachesim/scripts/auto.sh
+++ cachesim/scripts/auto.sh
@@ -9,14 +9,14 @@ INSTS=${NEMU_HOME}/build/insts.txt
 
 SIZES=(4 8 16 32)
 NUMS=(4 8 16 32)
-WAYS(4 8 12 16)
+WAYS=(4 8 12 16)
 # SIZES=(4)
 # NUMS=(16)
 
 # 批量运行
 for size in "${SIZES[@]}"; do
     for num in "${NUMS[@]}"; do
-        for way in "${WAYS[@}}"; do
+        for way in "${WAYS[@]}"; do
           echo "Running with SIZE=$size and NUM=$num and WAY=$way..."
 
           # 运行命令
