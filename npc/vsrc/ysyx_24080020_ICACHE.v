cffa69e6508d85b9678dbe90c2001f39fa910491
tracer-ysyx <tracer@ysyx.org>
2025-04-18 17:23:06 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.23-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 10 Apr 2025 13:28:36 +0000 x86_64 GNU/Linux  17:23:06 up 4 days, 23:23,  2 users,  load average: 1.49, 1.20, 1.10

--- npc/vsrc/ysyx_24080020_ICACHE.v
+++ npc/vsrc/ysyx_24080020_ICACHE.v
@@ -286,6 +286,9 @@ module ysyx_24080020_ICACHE(
                 fin_judge <= 'b1;
               end
 
+              if(fin_judge) begin
+                fin_judge <= 'b0;
+              end
               // after cache, update fin_r
               if(fin_r) begin
                 fin_r <= 0;
@@ -295,9 +298,9 @@ module ysyx_24080020_ICACHE(
               rdata_tmp <= shift_rdata[31:0];
               all_fin <= 'b1;
 
-              if(fin_judge) begin
-                fin_judge <= 'b0;
-              end
+              // if(fin_judge) begin
+              //   fin_judge <= 'b0;
+              // end
               `ifdef CONFIG_DPIC
               // hit cache and not by axi
               if(!fin_r) begin
@@ -314,9 +317,9 @@ module ysyx_24080020_ICACHE(
           end
           CMISS: begin
               // do nothing
-              if(fin_judge) begin
-                fin_judge <= 'b0;
-              end
+              // if(fin_judge) begin
+              //   fin_judge <= 'b0;
+              // end
           end
           AXIAR: begin
               if(arready_soc_i && arvalid_icache_o) begin
