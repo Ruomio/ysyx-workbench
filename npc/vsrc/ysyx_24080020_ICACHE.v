1ff4b2648b3b857b61c3443c173574164ed7f62a
tracer-ysyx <tracer@ysyx.org>
2025-04-18 14:52:09 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.23-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 10 Apr 2025 13:28:36 +0000 x86_64 GNU/Linux  14:52:09 up 4 days, 20:52,  2 users,  load average: 0.65, 0.77, 0.88

--- npc/vsrc/ysyx_24080020_ICACHE.v
+++ npc/vsrc/ysyx_24080020_ICACHE.v
@@ -408,7 +408,7 @@ module ysyx_24080020_ICACHE(
 
       arready_icache_o <= 1'b1;
 
-      if(use_icache || 'b1) begin
+      if(use_icache) begin
         araddr_tmp <= araddr_xbar_i;
       end
 
