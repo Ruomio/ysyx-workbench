f37fa06685e0644bfcbf47d33bc72e24645fe638
tracer-ysyx <tracer@ysyx.org>
2025-04-21 09:32:11 +0800

    > run CacheSim ysyx_24080020 彭培智 Linux archlinux 6.12.23-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 10 Apr 2025 13:28:36 +0000 x86_64 GNU/Linux  09:32:11 up  1:23,  1 user,  load average: 1.25, 1.35, 1.26

--- cachesim/src/cachesim/cachesim.cpp
+++ cachesim/src/cachesim/cachesim.cpp
@@ -124,7 +124,7 @@ void CacheSim::run_simulation() {
               address += 0x4;
             }
             fifo_index[index] = (fifo_index[index]+1) % cacheway;
-            printf("fifo_index[index]: %d\n", fifo_index[index]);
+            printf("fifo_index[%d]: %d\n", index, fifo_index[index]);
             // printf("access soc done\n");
         }
     }
