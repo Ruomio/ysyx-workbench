123c6d6296670d4af45195e41be12f245b67aad2
tracer-ysyx <tracer@ysyx.org>
2025-04-22 09:29:42 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.20-1-lts #1 SMP PREEMPT_DYNAMIC Sun, 23 Mar 2025 08:02:10 +0000 x86_64 GNU/Linux  09:29:42 up 18:26,  1 user,  load average: 0.26, 0.49, 0.58

--- npc/vsrc/ysyx_24080020_ICACHE.v
+++ npc/vsrc/ysyx_24080020_ICACHE.v
@@ -110,7 +110,7 @@ module ysyx_24080020_ICACHE(
   localparam cache_num = `ysyx_24080020_CACHE_NUM;
 
   localparam cache_size_bits = $clog2(cache_size);
-  localparam cache_size_shift = $clog2(cache_size_bits) - 1;
+  localparam cache_size_shift = $clog2(cache_data_width);
   localparam cache_num_bits = $clog2(cache_num);
 
   localparam cache_tag_size = 32 - cache_size_bits - cache_num_bits;
@@ -148,9 +148,9 @@ module ysyx_24080020_ICACHE(
   assign cache_offset_tmp = araddr_tmp[cache_size_bits-1 : 0];
 
 
-  assign shift_rdata = cache_data[cache_index_tmp] >> ({ {(cache_data_ingroup_width-cache_way){1'b0}}, tag_index} << (cache_data_width) ) >> ({{(32-cache_size_bits){1'b0}}, cache_offset_tmp} << 3);
-  assign shift_wdata = ({{data_complete_bits{1'b0}}, rdata_soc_i} << (({{(cache_data_ingroup_width-cache_way){1'b0}}, fifo_index[cache_index_tmp]}) << cache_data_width) << ({{(32-cache_size_bits){1'b0}}, cache_offset_tmp} << 3));
-  assign data_mask = ({{data_complete_bits{1'b0}}, ~32'b0} << (({{(cache_data_ingroup_width-cache_way){1'b0}}, fifo_index[cache_index_tmp]}) << cache_data_width) << ({{(32-cache_size_bits){1'b0}}, cache_offset_tmp} << 3));
+  assign shift_rdata = cache_data[cache_index_tmp] >> ({ {(cache_data_ingroup_width-cache_way){1'b0}}, tag_index} << (cache_size_shift) ) >> ({{(32-cache_size_bits){1'b0}}, cache_offset_tmp} << 3);
+  assign shift_wdata = ({{data_complete_bits{1'b0}}, rdata_soc_i} << (({{(cache_data_ingroup_width-cache_way){1'b0}}, fifo_index[cache_index_tmp]}) << cache_size_shift) << ({{(32-cache_size_bits){1'b0}}, cache_offset_tmp} << 3));
+  assign data_mask = ({{data_complete_bits{1'b0}}, ~32'b0} << (({{(cache_data_ingroup_width-cache_way){1'b0}}, fifo_index[cache_index_tmp]}) << cache_size_shift) << ({{(32-cache_size_bits){1'b0}}, cache_offset_tmp} << 3));
 
 
   // assign shift_rtag = (cache_tag[cache_index_tmp] & tag_mask) >> (({{32-cache_size_bits{1'b0}}, cache_offset_tmp} >> 2) * cache_tag_size);
