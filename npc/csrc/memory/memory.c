28f3bbced146d39823255f724a06fd5bcdf690ea
tracer-ysyx <tracer@ysyx.org>
2025-05-06 13:55:12 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.25-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 25 Apr 2025 10:36:32 +0000 x86_64 GNU/Linux  13:55:12 up  5:43,  2 users,  load average: 1.02, 0.93, 1.01

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -159,14 +159,14 @@ extern "C" void flash_read(int32_t addr, int32_t *data) {
 }
 extern "C" void flash_write(int32_t addr, int32_t strb, int32_t data) {
   int len = 0;
-  if((strb & 0b1) == 0b1) {
-    len = 1;
+  if((strb & 0b1111) == 0b1111) {
+    len = 4;
   }
   else if((strb & 0b11) == 0b11) {
     len = 2;
   }
-  else if((strb & 0b1111) == 0b1111) {
-    len = 4;
+  else if((strb & 0b1) == 0b1) {
+    len = 1;
   }
   paddr_write(addr | 0x30000000, len, data);
   printf("flash_write addr: 0x%x, len: %d, data: 0x%x\n", addr | 0x30000000, len, data);
