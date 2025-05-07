00650257bea89e2a6d3731c71591cd720805c097
tracer-ysyx <tracer@ysyx.org>
2025-05-07 10:40:02 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.25-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 25 Apr 2025 10:36:32 +0000 x86_64 GNU/Linux  10:40:02 up 1 day,  2:28,  2 users,  load average: 1.37, 1.65, 1.48

--- npc/csrc/memory/memory.c
+++ npc/csrc/memory/memory.c
@@ -33,6 +33,7 @@ uint8_t sdram[CONFIG_SDRAM_SIZE] = {};
     memset(MtraceBuf.m_buffer[idx], 0, 64);
     memset(MtraceBuf.m_buffer[idx], ' ', 3);
     sprintf((char *)MtraceBuf.m_buffer[idx]+3, "0x%08x    %d    0x%08x", addr, len, data);
+    printf("%s\n", MtraceBuf.m_buffer[idx]);
     MtraceBuf.idx = (idx+1)%64;
   }
 
