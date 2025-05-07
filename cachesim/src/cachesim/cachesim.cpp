54b93272a70daef7b95d9d9376f285a7d0913e5b
tracer-ysyx <tracer@ysyx.org>
2025-05-07 14:49:01 +0800

    > run CacheSim ysyx_24080020 彭培智 Linux archlinux 6.12.25-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 25 Apr 2025 10:36:32 +0000 x86_64 GNU/Linux  14:49:01 up 1 day,  6:37,  2 users,  load average: 1.23, 1.11, 1.19

--- cachesim/src/cachesim/cachesim.cpp
+++ cachesim/src/cachesim/cachesim.cpp
@@ -118,7 +118,6 @@ void CacheSim::run_simulation() {
         // printf("line: %s\n", line.c_str());
         if(std::regex_search(line, matches, addr_pattern)) {
             address = static_cast<uint32_t>(std::stoul(matches[0], nullptr, 16));
-            printf("address: 0x%x\n", address);
         }
         if(std::regex_match(line, r_pattern)) {
             rw_ = 0;
@@ -126,6 +125,7 @@ void CacheSim::run_simulation() {
         else {
             rw_ = 1;
         }
+        printf("address: 0x%x   r/w: %d\n", address, rw_);
 
         inst_cnt++;
 
