e4c575a4b1cc69aab121e349bbe8d0882cc80504
tracer-ysyx <tracer@ysyx.org>
2025-05-08 16:31:56 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.25-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 25 Apr 2025 10:36:32 +0000 x86_64 GNU/Linux  16:31:56 up 2 days,  8:20,  2 users,  load average: 0.86, 0.92, 0.98

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -72,7 +72,7 @@ VerilatedContext *contextp = NULL;
 
 npc_state u_npc_state = {.state=NPC_RUNNING, .pc=CONFIG_MBASE, .ret = true};
 
-uint32_t g_pc;
+uint32_t g_pc = 0;
 static uint32_t last_pc;
 static bool g_print_step = false;
 uint64_t g_nr_guest_inst = 0;
