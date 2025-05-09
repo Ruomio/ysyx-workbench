54fb39b75fff4a842de8d9de170416687d2eeb5a
tracer-ysyx <tracer@ysyx.org>
2025-05-09 16:06:00 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.25-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 25 Apr 2025 10:36:32 +0000 x86_64 GNU/Linux  16:06:00 up 3 days,  7:54,  2 users,  load average: 0.76, 0.73, 0.71

--- npc/csrc/npc/difftest/dut.c
+++ npc/csrc/npc/difftest/dut.c
@@ -141,6 +141,7 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
   }
 
   if (is_skip_ref) {
+    printf("skip ref\n");
     // to skip the checking of an instruction, just copy the reg state to reference design
     update_npc_cpu();
     ref_difftest_regcpy(&npc_cpu, DIFFTEST_TO_REF);
