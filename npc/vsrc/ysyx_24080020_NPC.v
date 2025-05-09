9e49e7a2230fa75029686b4f805df393abafa1b0
tracer-ysyx <tracer@ysyx.org>
2025-05-09 09:13:26 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.25-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 25 Apr 2025 10:36:32 +0000 x86_64 GNU/Linux  09:13:26 up 3 days,  1:02,  2 users,  load average: 0.67, 0.61, 0.56

--- npc/vsrc/ysyx_24080020_NPC.v
+++ npc/vsrc/ysyx_24080020_NPC.v
@@ -1103,7 +1103,7 @@ module ysyx_24080020_NPC(
 
         // Structural adventures, between ifu and lsu
         .in_flash(in_flash),
-        .arvalid_icache_o(arvalid_icache_o),
+        .arvalid_xbar_i(arvalid_xbar_i),
         .rvalid_soc_i(rvalid_soc_i),
         .structural_adventure(structural_adventure),
 
