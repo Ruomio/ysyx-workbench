8b457675b82b3731c73c9a3f1d85a25f0a4f01b9
tracer-ysyx <tracer@ysyx.org>
2025-05-12 13:40:59 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.27-1-lts #1 SMP PREEMPT_DYNAMIC Mon, 05 May 2025 14:13:34 +0000 x86_64 GNU/Linux  13:40:59 up  5:27,  1 user,  load average: 0.70, 0.39, 0.44

--- npc/vsrc/ysyx_24080020_IDU.v
+++ npc/vsrc/ysyx_24080020_IDU.v
@@ -3,7 +3,6 @@ module ysyx_24080020_IDU (
     input clk,
     input rst,
     input data_adventure,
-    input control_adventure,
 
     input [`ysyx_24080020_WIDTH-1:0] inst_ifu,
     input [`ysyx_24080020_WIDTH-1:0] val_raddr1,
@@ -95,7 +94,7 @@ module ysyx_24080020_IDU (
     assign rs2 = inst_idu[`ysyx_24080020_RS2];
     assign funct7 = inst_idu[`ysyx_24080020_FUNCT7];
 
-    assign idu_exu_valid_reg = idu_exu_valid && !data_adventure && !control_adventure;
+    assign idu_exu_valid_reg = idu_exu_valid && !data_adventure;
 
     always @(posedge clk) begin
         if(!rst) begin
--- npc/vsrc/ysyx_24080020_IFU.v
+++ npc/vsrc/ysyx_24080020_IFU.v
@@ -69,7 +69,7 @@ module ysyx_24080020_IFU (
         if(!rst) begin
             ifu_idu_valid <= 1'b0;
         end
-        else if(inst_fin) begin
+        else if(inst_fin && !control_adventure) begin
             ifu_idu_valid <= 1'b1;
             pc_ifu <= addr;
         end
@@ -143,7 +143,7 @@ module ysyx_24080020_IFU (
         if(!rst) begin
             next_inst <= 'b1;
         end
-        if(idu_ifu_ready && ifu_idu_valid) begin
+        if((idu_ifu_ready && ifu_idu_valid) || control_adventure) begin
             next_inst <= 'b1;
             is_dnpc <= 1'b0;
         end
