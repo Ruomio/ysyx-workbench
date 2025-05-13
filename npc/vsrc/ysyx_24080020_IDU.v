08916f369668b5d0dace1446524f1e4401805e98
tracer-ysyx <tracer@ysyx.org>
2025-05-13 15:52:57 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.28-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 09 May 2025 08:29:37 +0000 x86_64 GNU/Linux  15:52:57 up 18:18,  1 user,  load average: 0.87, 0.90, 0.90

--- npc/vsrc/ysyx_24080020_IDU.v
+++ npc/vsrc/ysyx_24080020_IDU.v
@@ -455,7 +455,7 @@ module ysyx_24080020_IDU (
                             is_dnpc_idu = 1'b1;
                             `ifdef CONFIG_DPIC
                             npc_difftest_skip_ref();
-                            // $display("ecall, skip_ref pc:0x%x", pc_idu);
+                            $display("ecall, skip_ref pc:0x%x", pc_idu);
                             `endif
                         end
                         else if(imm_idu == 32'b1100000010) begin
