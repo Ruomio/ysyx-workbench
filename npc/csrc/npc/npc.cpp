2a966c05ac2869e278589da5eaf149d4a0282358
tracer-ysyx <tracer@ysyx.org>
2025-05-13 14:04:01 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.28-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 09 May 2025 08:29:37 +0000 x86_64 GNU/Linux  14:04:01 up 16:29,  1 user,  load average: 1.30, 1.31, 1.29

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -542,6 +542,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i=0; i<sizeof(ref_r->gpr)/sizeof(ref_r->gpr[0]); i++) {
     if(ref_r->gpr[i] != g_get_reg(i)) {
       printf("The %s reg is diff, shoud be %#x  but get %#x.\n", regs_name[i], ref_r->gpr[i], g_get_reg(i));
+      printf("ref a5: 0x%x, a7:0x%x\n", ref_r->gpr[15], ref_r->gpr[17]);
       return false;
     }
   }
