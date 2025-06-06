f5a8790c08f3441458506b0968ce848184bbc63e
tracer-ysyx <tracer@ysyx.org>
2025-06-06 17:34:06 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.31-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 29 May 2025 09:39:59 +0000 x86_64 GNU/Linux  17:34:06 up  3:05,  1 user,  load average: 0.89, 0.89, 0.91

--- npc/csrc/lightsss/lightsss.cpp
+++ npc/csrc/lightsss/lightsss.cpp
@@ -49,7 +49,7 @@ void ForkShareMemory::shwait() {
       else
         exit(0);
     } else {
-      FORK_PRINTF("parent not dead, I'm sleep: pid: %d\n", getpid());
+      FORK_PRINTF("parent not dead, I'm sleep: pid: %d, ppid: %d\n", getpid(), getppid());
       sleep(WAIT_INTERVAL);
       if(!is_p_alive) exit(0);
     }
