78927d96a8a5b182655da8ac6c0e9fb49b69c383
24080020-Peizhi Peng <1065940593@qq.com>
2025-06-08 18:17:24 +0800

    npc: update lightsss

--- npc/include/lightsss/lightsss.h
+++ npc/include/lightsss/lightsss.h
@@ -72,7 +72,7 @@ public:
       signal(SIGABRT, signal_handler_abort); // 注册SIGTERM处理器 assert faile
       signal(SIGUSR1, signal_handler_abort); // 注册USR1处理器
   }
-  ~LightSSS() {}
+  ~LightSSS() { do_clear(); }
   int do_fork();
   int wakeup_child(uint64_t cycles);
   static bool is_child();
