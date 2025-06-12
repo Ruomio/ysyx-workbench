998b0618639c62fd6114f36f8aaa7384bdc2c0a1
tracer-ysyx <tracer@ysyx.org>
2025-06-12 16:14:33 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.32-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 04 Jun 2025 14:14:48 +0000 x86_64 GNU/Linux  16:14:33 up 3 days,  2:35,  1 user,  load average: 0.77, 0.98, 1.11

--- npc/vsrc/ysyx_24080020_HAZARD.v
+++ npc/vsrc/ysyx_24080020_HAZARD.v
@@ -41,30 +41,19 @@ module ysyx_24080020_HAZARD(
                          || (((rs2_idu == rd_exu) || (rs2_idu == rd_lsu) || (rs2_idu == rd_wbu)) && (rs2_idu != 'b0));
 
 
-    always @(posedge clk) begin
-        if(!rst) begin
-            control_adventure <= 'b0;
-            en <= 'b1;
-        end
-        // else if(en) begin
-        //     if(is_dnpc) begin
-        //         control_adventure <= 'b1;
-        //         en <= 'b0;
-        //     end
-        // end
-        // else if(exu_lsu_shake_hands) begin
-        //     en <= 'b1;
-        // end
-        // if(update_pc && control_adventure) begin
-        //     control_adventure <= 'b0;
-        // end
-        else if(exu_lsu_shake_hands) begin
-            control_adventure <= 'b0;
-        end
-        else if(is_dnpc) begin
-            control_adventure <= 'b1;
-        end
-    end
+    // always @(posedge clk) begin
+    //     if(!rst) begin
+    //         control_adventure <= 'b0;
+    //     end
+    //     else if(exu_lsu_shake_hands) begin
+    //         control_adventure <= 'b0;
+    //     end
+    //     else if(is_dnpc) begin
+    //         control_adventure <= 'b1;
+    //     end
+    // end
+
+    assign control_adventure = is_dnpc ? 'b1 : 'b0;
 
 
 endmodule
