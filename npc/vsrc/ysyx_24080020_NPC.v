3659dfc31e1b0517f0dd6da7dbfc45ad38e973d2
tracer-ysyx <tracer@ysyx.org>
2025-06-13 14:29:19 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.33-1-lts #1 SMP PREEMPT_DYNAMIC Tue, 10 Jun 2025 13:16:59 +0000 x86_64 GNU/Linux  14:29:19 up  6:06,  1 user,  load average: 0.91, 0.92, 1.09

--- npc/vsrc/ysyx_24080020_NPC.v
+++ npc/vsrc/ysyx_24080020_NPC.v
@@ -334,7 +334,7 @@ module ysyx_24080020_NPC(
         .inst_ifu(inst_ifu),
 
         .lsu_busy(lsu_busy),
-        .update_pc(arvalid_icache && arready_icache),
+        .update_pc(rvalid_ifu && rready_ifu),
         .control_adventure(control_adventure),
         .inst_fin(inst_fin),
         .flush_pipeline(flush_pipeline),
