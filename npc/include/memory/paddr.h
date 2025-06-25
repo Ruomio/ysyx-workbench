1d0dba85552e0c1979437d73ad70012f46aa7d38
tracer-ysyx <tracer@ysyx.org>
2025-06-25 16:13:09 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.34-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 19 Jun 2025 15:05:14 +0000 x86_64 GNU/Linux  16:13:09 up 2 days, 39 min,  1 user,  load average: 1.74, 1.23, 1.00

--- npc/include/memory/paddr.h
+++ npc/include/memory/paddr.h
@@ -30,6 +30,7 @@ paddr_t host_to_guest(uint8_t *haddr);
 
 static inline bool in_pmem(paddr_t addr) {
 #if defined(CONFIG_PSRAM) || defined(CONFIG_SDRAM) || defined(CONFIG_SRAM)
+  printf("in defins \n");
   return addr - CONFIG_MBASE < CONFIG_MSIZE |
          addr - CONFIG_PSRAM_BASE < CONFIG_PSRAM_SIZE |
          addr - CONFIG_SRAM_BASE < CONFIG_SRAM_SIZE |
