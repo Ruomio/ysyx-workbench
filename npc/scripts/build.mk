5e4e407d9fefb36113b217193e91a431330ae008
tracer-ysyx <tracer@ysyx.org>
2025-06-26 10:45:05 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.34-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 19 Jun 2025 15:05:14 +0000 x86_64 GNU/Linux  10:45:05 up 2 days, 19:11,  1 user,  load average: 1.11, 1.26, 1.39

--- npc/scripts/build.mk
+++ npc/scripts/build.mk
@@ -155,6 +155,7 @@ ifeq ($(NVBOARD_ENABLE), 1)
 link: $(OBJS) $(V_OBJS) $(NVBOARD_ARCHIVE)
 	$(call git_commit, "sim RTL") # DO NOT REMOVE THIS LINE!!!
 	@echo + LD
+	@echo csrc $(CSRC)
 	@echo objs $(OBJS) --- $(V_OBJS)
 	@g++ $(LDFLAGS) $^ -o build/$(TOPNAME)
 
