e97d17a9d61991dd8e1f1d7f511df5bf44d415cf
tracer-ysyx <tracer@ysyx.org>
2025-06-26 10:53:36 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.34-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 19 Jun 2025 15:05:14 +0000 x86_64 GNU/Linux  10:53:36 up 2 days, 19:20,  1 user,  load average: 1.95, 1.62, 1.49

--- npc/scripts/build.mk
+++ npc/scripts/build.mk
@@ -125,7 +125,6 @@ $(OBJ_DIR)/%.o: %.cc
 	$(call call_fixdep, $(@:.o=.d), $@)
 $(OBJ_DIR)/%.o: %.cpp
 	@echo + CXX $<
-	@echo + $(CXXFLAGS)
 	@mkdir -p $(dir $@)
 	@g++ $(CXXFLAGS) -c $< -o $@
 	$(call call_fixdep, $(@:.o=.d), $@)
