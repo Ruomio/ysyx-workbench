998053fd4f9b1dbf8c24622a52c0c4f325767c4e
tracer-ysyx <tracer@ysyx.org>
2025-07-01 16:58:18 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.35-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Jun 2025 10:50:50 +0000 x86_64 GNU/Linux  16:58:18 up  8:31,  1 user,  load average: 1.13, 1.19, 1.18

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -277,7 +277,7 @@ void exec_once_npc(uint32_t pc) {
       // Assert(g_pc >= CONFIG_MBASE, "pc invalid:0x%x, last pc: 0x%x", g_pc, last_pc);
       idu_type = None;
       if(g_pc < CONFIG_MBASE) {
-        printf("pc invalid:0x%x, last pc: 0x%x", g_pc, last_pc);
+        printf("pc invalid:0x%x, last pc: 0x%x\n", g_pc, last_pc);
         u_npc_state.state = NPC_ABORT;
         u_npc_state.pc = pc;
         u_npc_state.ret = true;
