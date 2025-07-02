f03da0a67ea8763f6f0bc9adff41a65d6f30f032
tracer-ysyx <tracer@ysyx.org>
2025-07-02 10:23:53 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.35-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Jun 2025 10:50:50 +0000 x86_64 GNU/Linux  10:23:53 up 1 day,  1:56,  1 user,  load average: 1.62, 1.24, 1.08

--- npc/vsrc/ysyx_24080020_EXU.v
+++ npc/vsrc/ysyx_24080020_EXU.v
@@ -156,7 +156,7 @@ module ysyx_24080020_EXU
             `endif
         end
         else if(idu_exu_valid) begin
-            if(exu_mem_valid && need_stall) exu_idu_ready <=  1'b0;
+            if(exu_mem_valid) exu_idu_ready <=  1'b0;
             else if(exu_idu_ready) begin
                 exu_idu_ready <= 'b0;
                 idu_exu_shake_hand <= 'b1;
@@ -206,7 +206,7 @@ module ysyx_24080020_EXU
 
 
             end
-            else begin
+            else if(!need_stall) begin
                 // shake hand successfully
                 exu_idu_ready <= 1'b1;
 
