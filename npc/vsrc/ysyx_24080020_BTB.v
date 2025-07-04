82928c39cdab6c9f8067dcc25ed5af03a0e0b31e
tracer-ysyx <tracer@ysyx.org>
2025-07-04 13:07:30 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.35-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Jun 2025 10:50:50 +0000 x86_64 GNU/Linux  13:07:30 up 3 days,  4:40,  1 user,  load average: 1.36, 0.64, 0.53

--- npc/vsrc/ysyx_24080020_BTB.v
+++ npc/vsrc/ysyx_24080020_BTB.v
@@ -48,10 +48,13 @@ module ysyx_24080020_BTB(
     wire [branch_tag_ingroup_width-1 : 0]  shift_wtag;
     wire [branch_tag_ingroup_width-1 : 0]  tag_mask;
 
-    wire                                     branch_hit;
     wire [branch_tag_size-1:0]               branch_tag_tmp;
     wire [branch_num_bits-1:0]               branch_index_tmp;
     wire [branch_size_bits-1:0]              branch_offset_tmp;
+
+    wire [branch_tag_size-1:0]               branch_tag_new;
+    wire [branch_num_bits-1:0]               branch_index_new;
+    wire [branch_size_bits-1:0]              branch_offset_new;
 
     reg [branch_data_ingroup_width - 1 : 0]  branch_data  [0 : branch_num - 1];
     reg [branch_tag_ingroup_width - 1 : 0]   branch_tag   [0 : branch_num - 1];
@@ -63,19 +66,37 @@ module ysyx_24080020_BTB(
     wire [2:0] next_state;
 
     reg [2:0] current_state;
-    reg [`ysyx_24080020_WIDTH-1:0] pc;
+    reg [`ysyx_24080020_WIDTH-1:0] pc, pc_new;
+
+    logic has_hit;
+    logic total_hits [0 : branch_way - 1];
+    logic [branch_way-1:0] tmp_tag_index;
 
 
     assign branch_tag_tmp = pc_tmp[31 : branch_num_bits+branch_size_bits];
     assign branch_index_tmp = pc_tmp[branch_num_bits+branch_size_bits-1 : branch_size_bits];
     assign branch_offset_tmp = pc_tmp[branch_size_bits-1 : 0];
 
-    assign branch_hit = 'b1;
+    assign branch_tag_new = pc_new[31 : branch_num_bits+branch_size_bits];
+    assign branch_index_new = pc_new[branch_num_bits+branch_size_bits-1 : branch_size_bits];
+    assign branch_offset_new = pc_new[branch_size_bits-1 : 0];
+
+    assign shift_rdata = branch_data[branch_index_tmp] >> ({ {(branch_data_ingroup_width-branch_way){1'b0}}, tag_index} << (branch_size_shift) ) >> ({{(32-branch_size_bits){1'b0}}, branch_offset_tmp} << 3);
+
+    assign shift_wdata = ({{data_complete_bits{1'b0}}, dnpc_new} << (({{(branch_data_ingroup_width-branch_way){1'b0}}, fifo_index[branch_index_new]}) << branch_size_shift) << ({{(32-branch_size_bits){1'b0}}, branch_offset_new} << 3));
+    assign data_mask = ({{data_complete_bits{1'b0}}, ~32'b0} << (({{(branch_data_ingroup_width-branch_way){1'b0}}, fifo_index[branch_index_new]}) << branch_size_shift) << ({{(32-branch_size_bits){1'b0}}, branch_offset_new} << 3));
+
+
+    // assign shift_rtag = (branch_tag[branch_index_tmp] >> ({ {(branch_tag_ingroup_width-branch_way){1'b0}}, tag_index} * branch_tag_width)) & ({ {tag_complete_bits{1'b0}}, {branch_tag_width{1'b1}} });
+
+    assign shift_wtag = {{tag_complete_bits{1'b0}}, branch_tag_new} << ({ {(branch_tag_ingroup_width-branch_way){1'b0}}, fifo_index[branch_index_new]} * branch_tag_width);
+    assign tag_mask = {{tag_complete_bits{1'b0}}, ~{branch_tag_size{1'b0}}} << ({ {(branch_tag_ingroup_width-branch_way){1'b0}}, fifo_index[branch_index_new]} * branch_tag_width);
+
 
     generate
       genvar j;
       for (j = 0; j < branch_way; j++) begin
-          assign total_hits[j] = (branch_tag[branch_index_s2][(j+1)*branch_tag_width-1:j*branch_tag_width] == branch_tag_s2) && ((branch_valid[branch_index_s2] & ({ {(branch_way-1){1'b0}}, 1'b1} << j)) != 'b0);
+          assign total_hits[j] = (branch_tag[branch_index_tmp][(j+1)*branch_tag_width-1:j*branch_tag_width] == branch_tag_tmp) && ((branch_valid[branch_index_tmp] & ({ {(branch_way-1){1'b0}}, 1'b1} << j)) != 'b0);
       end
     endgenerate
 
@@ -91,8 +112,15 @@ module ysyx_24080020_BTB(
     end
 
     always @(posedge clk or posedge rst) begin
-        if (rst) begin
+        if (!rst) begin
             current_state <= 'b0;
+
+            for (integer i = 'b0; i < `ysyx_24080020_BRANCH_NUM; i = i + 'b1 ) begin
+                branch_data[i]   <= 'b0;
+                branch_tag[i]    <= 'b0;
+                branch_valid[i]  <= 'b0;
+                fifo_index[i]   <= 'b0;
+            end
         end else begin
             current_state <= next_state;
         end
@@ -108,7 +136,7 @@ module ysyx_24080020_BTB(
                 end
             end
             JUDGE: begin
-                if(hit) begin
+                if(is_hit) begin
                     next_state = HIT;
                 end else begin
                     next_state = MISS;
@@ -135,7 +163,7 @@ module ysyx_24080020_BTB(
             is_hit <= 'b0;
         end
         else if(current_state == JUDGE) begin
-            if(branch_hit) begin
+            if(has_hit) begin
                 is_hit <= 'b1;
             end
             else begin
@@ -143,26 +171,61 @@ module ysyx_24080020_BTB(
             end
         end
         else if(current_state == HIT) begin
+            predict_pc <= shift_rdata[31:0];
         end
         else if(current_state == MISS) begin
+            predict_pc <= pc;
         end
         else if(current_state == Done) begin
+            out_valid <= 'b1;
+        end
+    end
+
+    // update BTB
+    always @(posedge clk) begin
+        if(!rst) begin
+
+            dnpc_tmp <= 'b0;
+            is_dnpc_tmp <= 'b0;
+        end
+        else if(is_dnpc_tmp) begin
+            is_dnpc_tmp <= 'b0;
+            pc <= pc_new;
+
+            // write BTB
+            fifo_index[branch_index_new] <= (fifo_index[branch_index_new] + 'b1) % branch_way;
+            branch_tag[branch_index_new] <= branch_tag[branch_index_new] & ~tag_mask | shift_wtag;
+            branch_data[branch_index_new] <= (branch_data[branch_index_new] & ~data_mask) | shift_wdata;
+            branch_valid[branch_index_new] <= branch_valid[branch_index_new] | (1 << fifo_index[branch_index_new]);
+
+        end
+        else if(is_dnpc && !is_dnpc_next) begin
+            pc_new <= pc_exu;
+            is_dnpc_tmp <= is_dnpc;
+            dnpc_tmp <= dnpc;
         end
     end
 
     always @(posedge clk) begin
         if(!rst) begin
-            pc <= `ysyx_24080020_MBASE - 32'd4;
+            is_dnpc_next <= 'b0;
+        end
+        else if(is_dnpc) begin
+            is_dnpc_next <= 'b1;
+        end
+        else begin
+            is_dnpc_next <= 'b0;
+        end
+    end
+
+    always @(posedge clk) begin
+        if(!rst) begin
             pc_tmp <= 'b0;
-            dnpc_tmp <= 'b0;
-            is_dnpc_tmp <= 'b0;
             in_ready <= 'b0;
         end
         else if(in_valid && in_ready) begin
             in_ready <= 'b0;
             pc_tmp <= pc;
-            dnpc_tmp <= pc;
-            is_dnpc_tmp <= is_dnpc;
             btb_busy <= 'b1;
         end
         else if(in_valid && (current_state == IDLE)) begin
@@ -173,10 +236,11 @@ module ysyx_24080020_BTB(
     always @(posedge clk)begin
         if(!rst) begin
             out_valid <= 'b0;
+            pc <= `ysyx_24080020_MBASE;
         end
         else if(out_valid && out_ready) begin
             out_valid <= 'b0;
+            pc <= pc + 32'd4;
         end
-
     end
 endmodule
