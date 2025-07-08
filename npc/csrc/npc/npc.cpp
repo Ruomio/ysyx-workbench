292405d549a21aa744a1984efcd77a1933834761
tracer-ysyx <tracer@ysyx.org>
2025-07-08 10:10:54 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.35-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 27 Jun 2025 10:50:50 +0000 x86_64 GNU/Linux  10:10:54 up 7 days,  1:43,  1 user,  load average: 1.64, 1.72, 1.88

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -368,8 +368,7 @@ static void statistic() {
 
   // BTB
   if(btb_total_cnt == 0) return;
-  Log("btb_total_cnt = " NUMBERIC_FMT " Percentage: %.2f%%", btb_total_cnt, btb_total_cnt * 100.0 / ifu_get_inst_cnt);
-  Log("btb_hit_cnt = " NUMBERIC_FMT " Percentage: %.2f%%", btb_hit_cnt, btb_hit_cnt * 100.0 / ifu_get_inst_cnt);
+  Log("btb_hit_cnt = " NUMBERIC_FMT " btb_total_cnt = " NUMBERIC_FMT " Percentage: %.2f%%", btb_hit_cnt, btb_total_cnt, btb_hit_cnt * 100.0 / btb_total_cnt);
 }
 
 void exec_npc(uint64_t n) {
