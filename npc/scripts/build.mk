9e852390728a1949fd619c5c3fc52d2e9b776782
tracer-ysyx <tracer@ysyx.org>
2025-07-29 17:07:03 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.40-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 24 Jul 2025 08:20:57 +0000 x86_64 GNU/Linux  17:07:03 up  3:38,  1 user,  load average: 1.28, 1.33, 1.39

--- npc/scripts/build.mk
+++ npc/scripts/build.mk
@@ -185,6 +185,7 @@ endif
 
 classic: $(VSRC) $(CSRC) $(CPPSRC) $(HSRC)
 	@echo $(NVBOARD_ENABLE) $(TOPNAME)
+	@echo $^
 	@$(call git_commit, "sim NPC") # DO NOT REMOVE THIS LINE!!!
 	@$(VERILATOR) $(VERILATOR_CFLAGS) \
 		--top-module $(TOPNAME) $(VSRC) $(CSRC) $(CPPSRC) \
