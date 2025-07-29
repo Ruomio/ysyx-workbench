ef11ef6dceba5d5afb181d9bae21faed3428c831
tracer-ysyx <tracer@ysyx.org>
2025-07-29 14:28:23 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.40-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 24 Jul 2025 08:20:57 +0000 x86_64 GNU/Linux  14:28:23 up 59 min,  1 user,  load average: 1.35, 1.42, 1.47

--- npc/vsrc/ysyx_24080020_NPC.v
+++ npc/vsrc/ysyx_24080020_NPC.v
@@ -4,7 +4,6 @@ module ysyx_24080020_NPC(
     input rst,
 
     output [31:0] pc_ifu_out,
-    output [31:0] inst_out,
     // master
     // AR
     input io_master_arready,
@@ -77,8 +76,7 @@ module ysyx_24080020_NPC(
     output [1:0] io_slave_bresp,
     output [3:0] io_slave_bid
 );
-    assign pc_ifu_out = pc_wbu;
-    assign inst_out = inst_ifu;
+    assign pc_ifu_out = pc_ifu;
 
   // pc
   wire [`ysyx_24080020_WIDTH-1:0] pc_ifu, pc_idu, pc_exu, pc_lsu, pc_wbu;
