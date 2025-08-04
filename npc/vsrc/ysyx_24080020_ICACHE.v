6d15b25ecf9fd9054698001c3afa0534f1eae06a
tracer-ysyx <tracer@ysyx.org>
2025-08-04 14:56:17 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.40-2-lts #1 SMP PREEMPT_DYNAMIC Tue, 29 Jul 2025 15:21:52 +0000 x86_64 GNU/Linux  14:56:17 up 2 days, 22:05,  1 user,  load average: 1.44, 1.23, 1.09

--- npc/vsrc/ysyx_24080020_ICACHE.v
+++ npc/vsrc/ysyx_24080020_ICACHE.v
@@ -800,6 +800,7 @@ module ysyx_24080020_ICACHE(
 
 // s3: axi read and get inst data
     wire [cache_data_ingroup_width-1 : 0] shift_rdata_s3;
+    wire [cache_data_ingroup_width-1 : 0] shift_rdata_s3_prev;
     reg [31:0] inst_s3;
 
     reg [31:0] araddr_s3;
@@ -906,7 +907,7 @@ module ysyx_24080020_ICACHE(
                 inst_s3 <= inst_s2;
             end
             else if(has_hit_s3) begin
-                inst_s3 <= shift_rdata_s3[31:0];
+                inst_s3 <= shift_rdata_s3_prev[31:0];
             end
 
             is_hit_s3 <= is_hit_s2;
@@ -977,6 +978,7 @@ module ysyx_24080020_ICACHE(
     assign cache_offset_s3 = araddr_s3[cache_size_bits-1 : 0];
     assign cache_offset_s3_base = araddr_s3_base[cache_size_bits-1 : 0];
     assign shift_rdata_s3 = cache_data[cache_index_s3] >> ({ {(cache_data_ingroup_width-cache_way){1'b0}}, fifo_index[cache_index_s3]} << (cache_size_shift) ) >> ({{(32-cache_size_bits){1'b0}}, cache_offset_s3_base} << 3);
+    assign shift_rdata_s3_prev = cache_data[cache_index_s3] >> ({ {(cache_data_ingroup_width-cache_way){1'b0}}, last_fifo_index_s3} << (cache_size_shift) ) >> ({{(32-cache_size_bits){1'b0}}, cache_offset_s3_base} << 3);
 
     assign shift_wdata = ({{data_complete_bits{1'b0}}, rdata_o} << (({{(cache_data_ingroup_width-cache_way){1'b0}}, fifo_index[cache_index_s3]}) << cache_size_shift) << ({{(32-cache_size_bits){1'b0}}, cache_offset_s3} << 3));
     assign data_mask = ({{data_complete_bits{1'b0}}, ~32'b0} << (({{(cache_data_ingroup_width-cache_way){1'b0}}, fifo_index[cache_index_s3]}) << cache_size_shift) << ({{(32-cache_size_bits){1'b0}}, cache_offset_s3} << 3));
