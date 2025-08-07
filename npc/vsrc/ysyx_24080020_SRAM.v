b2e47e5386098d43bea0b926dff70477f454bcdf
24080020-Peizhi Peng <1065940593@qq.com>
2025-08-07 15:39:02 +0800

    npc: fix sram to suport burst transmit

--- npc/vsrc/ysyx_24080020_SRAM.v
+++ npc/vsrc/ysyx_24080020_SRAM.v
@@ -44,22 +44,24 @@ module ysyx_24080020_SRAM(
     import "DPI-C" function void write_memory(input int addr, input int len, input int data);
 `endif
 
-    reg [`ysyx_24080020_WIDTH-1:0] paddr;
+    reg [`ysyx_24080020_WIDTH-1:0] paddr_r, paddr_w;
     reg [`ysyx_24080020_WIDTH-1:0] write_data;
     reg read_en, write_en, b_en;
-    reg read_before_write;
+    // reg read_before_write;
 
     reg [5:0] ar_cnt, aw_cnt, w_cnt;
     reg [5:0] r_cnt, b_cnt;
 
-    wire [31:0] rdata_shift;
+    reg [7:0] arlen_cnt;
+    reg [31:0] w_rdata;
+
 
 
     wire [`ysyx_24080020_WIDTH-1:0] wstrb_full;
     wire [5:0] lfsr;    // the number of delay cycle
 
     assign wstrb_full = {{8{wstrb[3]}}, {8{wstrb[2]}}, {8{wstrb[1]}}, {8{wstrb[0]}}};
-    assign lfsr = 6'd5;
+    assign lfsr = 6'd32;
 
     // AR
     always @(posedge clk) begin
@@ -67,23 +69,23 @@ module ysyx_24080020_SRAM(
             arready <= 1'b0;
             read_en <= 1'b0;
             ar_cnt <= 6'b0;
+            paddr_r <= 'b0;
+        end
+        else if(arvalid && arready) begin
+            arready <= 'b0;
         end
         else if(arvalid) begin
             if(ar_cnt < lfsr) begin
                 ar_cnt <= ar_cnt + 6'b1;
             end
             else begin
-                paddr <= {araddr[31:2],2'b0};
+                paddr_r <= {araddr[31:2],2'b0};
                 read_en <= 1'b1;
                 arready <= 1'b1;
 
                 ar_cnt <= 6'b0;
             end
         end
-        else begin
-            arready <= 1'b0;
-        end
-
     end
 
     // R
@@ -92,37 +94,44 @@ module ysyx_24080020_SRAM(
             rvalid <= 1'b0;
             rresp <= 2'b0;
             rdata <= 32'b0;
+            arlen_cnt <= 'b0;
+            paddr_r <= 'b0;
         end
         else if(read_en) begin
             if(r_cnt < lfsr) begin
                 r_cnt <= r_cnt + 6'b1;
             end
-            else begin
-                `ifdef CONFIG_DPIC
-                // printf_info();
-                rdata <= read_memory(paddr, 32'd4);
-                `endif
-                if(wvalid) begin
-                    read_before_write <= 1'b1;
-                    rvalid <= 1'b0;
-                    rresp <= 2'b0;
+            else if(rready && rvalid) begin
+                rvalid <= 1'b0;
+                rresp <= 2'b0;
+                if(rlast) begin
+                    r_cnt <= 'b0;
+                    read_en <= 1'b0;
+                    arlen_cnt <= 'b0;
+                    rlast <= 'b0;
                 end
                 else begin
-                    rvalid <= 1'b1;
-                    rresp <= 2'b0;
-                    read_en <= 1'b0;
+                    arlen_cnt <= arlen_cnt + 'b1;
+                    if(arburst == 'b00) begin
+                        paddr_r <= paddr_r;
+                    end
+                    else if(arburst == 'b01) begin
+                        paddr_r <= paddr_r + 'd4;
+                    end
+                end
+            end
+            else if(arlen_cnt <= arlen) begin
+                `ifdef CONFIG_DPIC
+                // printf_info();
+                rdata <= read_memory(paddr_r, 32'd4);
+                `endif
+
+                rvalid <= 1'b1;
+                rresp <= 2'b0;
+                if(arlen_cnt == arlen) begin
                     rlast <= 1'b1;
                 end
-                r_cnt <= 6'b0;
             end
-        end
-        else if(rready) begin
-            rvalid <= 1'b0;
-            rresp <= 2'b0;
-        end
-        else begin
-            rvalid <= 1'b0;
-            rresp <= rresp;
         end
     end
 
@@ -131,20 +140,21 @@ module ysyx_24080020_SRAM(
     always @(posedge clk) begin
         if(!rst) begin
             awready <= 1'b0;
+            paddr_w <= 'b0;
+        end
+        else if(awvalid && awready) begin
+            awready <= 1'b0;
         end
         else if(awvalid) begin
             if(aw_cnt < lfsr) begin
                 aw_cnt <= aw_cnt + 6'b1;
             end
             else begin
-                paddr <= {awaddr[31:2], 2'b0};
+                paddr_w <= {awaddr[31:2], 2'b0};
                 awready <= 1'b1;
 
                 aw_cnt <= 6'b0;
             end
-        end
-        else begin
-            awready <= 1'b0;
         end
     end
 
@@ -153,42 +163,38 @@ module ysyx_24080020_SRAM(
         if(!rst) begin
             w_cnt <= 6'b0;
             wready <= 1'b0;
-            read_before_write <= 1'b0;
+            write_en <= 1'b0;
+            w_rdata <= 'b0;
+        end
+        else if(wvalid && wready) begin
+            wready <= 'b0;
+            w_cnt <= 6'b0;
             write_en <= 1'b0;
         end
         else if(wvalid) begin
             if(w_cnt < lfsr) begin
                 w_cnt <= w_cnt + 6'b1;
+                if(w_cnt == lfsr - 'b1) begin
+                    `ifdef CONFIG_DPIC
+                    // printf_info();
+                    w_rdata <= read_memory({awaddr[31:2], 2'b0}, 32'd4);
+                    `endif
+                end
             end
             else begin
-                if(!read_before_write) begin
-                    read_en <= 1'b1;
-                end
-                else if(!write_en) begin
-                    read_en <= 1'b0;
-
-                    write_data <= wdata & wstrb_full | (rdata & ~wstrb_full);
+                if(!write_en) begin
+                    write_data <= wdata & wstrb_full | (w_rdata & ~wstrb_full);
                     write_en <= 1'b1;
                 end
                 else if(!wready) begin
                     `ifdef CONFIG_DPIC
-                    write_memory(paddr, 32'd4, write_data);
+                    write_memory(paddr_w, 32'd4, write_data);
                     `endif
                     b_en <= 1'b1;
 
                     wready <= 1'b1;
                 end
-                else begin
-
-                    w_cnt <= 6'b0;
-                    write_en <= 1'b0;
-                    read_before_write <= 1'b0;
-                end
-
             end
-        end
-        else begin
-            wready <= 1'b0;
         end
     end
 
@@ -212,15 +218,10 @@ module ysyx_24080020_SRAM(
                 b_cnt <= 6'b0;
             end
         end
-        else if(bready) begin
+        else if(bready && bvalid) begin
             bvalid <= 1'b0;
             bresp <= 2'b0;
         end
-        else begin
-            bvalid <= 1'b0;
-            bresp <= bresp;
-        end
-
     end
 
 endmodule
