29e9ad64450df1df75a4e64f938307a92f0064c4
tracer-ysyx <tracer@ysyx.org>
2025-08-13 16:35:12 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.41-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 01 Aug 2025 20:42:03 +0000 x86_64 GNU/Linux  16:35:12 up 6 days, 23:58,  1 user,  load average: 0.35, 0.30, 0.32

--- npc/vsrc/ysyx_24080020_BTB.v
+++ npc/vsrc/ysyx_24080020_BTB.v
@@ -368,9 +368,6 @@ module ysyx_24080020_BTB(
                 end
             end
             else begin
-                if(out_valid && out_ready) begin
-                    out_valid <= 'b0;
-                end
                 `ifdef CONFIG_DPIC
                     statistics_btb_hit();
                 `endif
