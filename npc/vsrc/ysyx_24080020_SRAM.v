5548fc7ba410818ae9f821cf69a4f35f8174dc58
tracer-ysyx <tracer@ysyx.org>
2025-08-14 10:03:15 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.41-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 01 Aug 2025 20:42:03 +0000 x86_64 GNU/Linux  10:03:15 up 7 days, 17:26,  1 user,  load average: 0.37, 0.38, 0.57

--- npc/vsrc/ysyx_24080020_SRAM.v
+++ npc/vsrc/ysyx_24080020_SRAM.v
@@ -171,6 +171,11 @@ module ysyx_24080020_SRAM(
             wready <= 1'b0;
             write_en <= 1'b0;
             w_rdata <= 'b0;
+
+            b_en <= 1'b0;
+        end
+        else if(b_en && b_cnt >= lfsr) begin
+            b_en <= 'b0;
         end
         else if(wvalid && wready) begin
             wready <= 'b0;
@@ -209,7 +214,7 @@ module ysyx_24080020_SRAM(
         if(!rst) begin
             bvalid <= 1'b0;
             bresp <= 2'b0;
-            b_en <= 1'b0;
+            // b_en <= 1'b0;
             w_cnt <= 6'b0;
         end
         else if(b_en) begin
@@ -220,7 +225,7 @@ module ysyx_24080020_SRAM(
                 bvalid <= 1'b1;
                 bresp <= 2'b0;
 
-                b_en <= 1'b0;
+                // b_en <= 1'b0;
                 b_cnt <= 6'b0;
             end
         end
