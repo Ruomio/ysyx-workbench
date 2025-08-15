abce9efbd54ec339631d7ba2f99ca57f3c655562
tracer-ysyx <tracer@ysyx.org>
2025-08-15 10:32:08 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.41-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 01 Aug 2025 20:42:03 +0000 x86_64 GNU/Linux  10:32:08 up 8 days, 17:54,  1 user,  load average: 0.98, 0.75, 0.66

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -223,7 +223,7 @@ void exec_once_npc(uint32_t pc) {
     if(is_clk_high) {
       g_get_pc();
       total_cycles++;
-      if(wait_cycles++ > 50000) {
+      if(wait_cycles++ > 70000) {
         printf("wait too many cycles, maybe dead loop\n");
         u_npc_state.state = NPC_ABORT;
         u_npc_state.pc = pc;
