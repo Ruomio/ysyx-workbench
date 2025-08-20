737b4c2aefce8fe43ee6332ab7e31c30f1c21007
24080020-Peizhi Peng <1065940593@qq.com>
2025-08-20 11:15:11 +0800

    npc: update uart to flush when writting

--- npc/vsrc/ysyx_24080020_UART.v
+++ npc/vsrc/ysyx_24080020_UART.v
@@ -107,6 +107,7 @@ module ysyx_24080020_UART(
         end
         else if(wvalid && wready) begin
             $write("%c", wdata[7:0]);
+            $fflush;
             if(wfin) wfin <= 'b0;
         end
         else if(wvalid) begin
