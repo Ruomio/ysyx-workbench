263b13156b8c96f0ca4a991a66e70390a31bfc78
tracer-ysyx <tracer@ysyx.org>
2025-08-20 11:11:15 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.42-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 15 Aug 2025 15:23:40 +0000 x86_64 GNU/Linux  11:11:15 up 2 days,  2:01,  1 user,  load average: 1.56, 2.04, 1.63

--- npc/vsrc/ysyx_24080020_UART.v
+++ npc/vsrc/ysyx_24080020_UART.v
@@ -106,7 +106,7 @@ module ysyx_24080020_UART(
             wready <= 1'b0;
         end
         else if(wvalid && wready) begin
-            $write("%c", wdata[7:0]);
+            $write("%c\n", wdata[7:0]);
             if(wfin) wfin <= 'b0;
         end
         else if(wvalid) begin
