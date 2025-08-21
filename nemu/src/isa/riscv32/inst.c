ca44ab045cb3809dffa01bb2c49a47f01375f009
tracer-ysyx <tracer@ysyx.org>
2025-08-21 09:23:31 +0800

    > sim RTL ysyx_24080020 彭培智 Linux archlinux 6.12.42-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 15 Aug 2025 15:23:40 +0000 x86_64 GNU/Linux  09:23:31 up 3 days, 13 min,  1 user,  load average: 0.59, 0.39, 0.38

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -70,13 +70,20 @@ static void ecall(Decode *s) {
 static void mret(Decode *s) {
   s->dnpc = CSR(MEPC_ADDR);
   uint32_t tmp_mstatus = CSR(MSTATUS_ADDR);
-  // 保留 MPP bit[12:11]
-  tmp_mstatus = (3 << 11);
+
+  // only M mode
+  tmp_mstatus = 3 << 11;
+
+  // privilege_mode = tmp_mstatus & (3 << 11);
+
+  // 改变 MPP 为 U/M mode. bit[12:11]
+  // tmp_mstatus |= (3 << 11); // M mode
+  // tmp_mstatus &= ~(3 << 11); // U mode
 
   // MIE = MPIE; MPIE = 1;   MIE: bit[3],  MPIE: bit[7]
   // bool flag = (tmp_mstatus & 0x80) == 0x80 ? 1 : 0;
   // tmp_mstatus |= flag << 3;
-  tmp_mstatus |= 0x80;
+  // tmp_mstatus |= 0x80;
 
   CSR(MSTATUS_ADDR) = tmp_mstatus;
 }
