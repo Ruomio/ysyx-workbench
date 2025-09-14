3a4eebcc034bca64744ec5842417842d3edc3325
tracer-ysyx <tracer@ysyx.org>
2025-09-14 17:37:26 +0800

    > iverilog NPC ysyx_24080020 彭培智 Linux archlinux 6.12.44-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 28 Aug 2025 15:07:21 +0000 x86_64 GNU/Linux  17:37:26 up 10 days,  8:35,  1 user,  load average: 0.80, 1.11, 1.17

--- npc/vsrc/ysyx_24080020_SRAM.v
+++ npc/vsrc/ysyx_24080020_SRAM.v
@@ -60,7 +60,7 @@ module ysyx_24080020_SRAM(
         if(fd == 0)
         begin
             $display ("Could not open File");
-            $finish(1);
+            $finish(0);
         end
 
     end
