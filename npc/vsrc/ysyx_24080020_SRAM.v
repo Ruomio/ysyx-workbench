ce6c8811d94d80146a562f979cb195b6dcdc8f76
tracer-ysyx <tracer@ysyx.org>
2025-09-15 18:34:55 +0800

    > iverilog NPC ysyx_24080020 彭培智 Linux archlinux 6.12.44-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 28 Aug 2025 15:07:21 +0000 x86_64 GNU/Linux  18:34:55 up 11 days,  9:32,  1 user,  load average: 3.81, 2.56, 1.83

--- npc/vsrc/ysyx_24080020_SRAM.v
+++ npc/vsrc/ysyx_24080020_SRAM.v
@@ -47,12 +47,10 @@ module ysyx_24080020_SRAM(
 `ifdef __ICARUS__
 
 
-    localparam memory_len = 1 << 24;
+    localparam memory_len = 1 << 27; // 128M
     reg [7:0] memory [0:memory_len-1];
 
     initial begin
-        for(int i = 0; i < memory_len; i++)
-            memory[i] = 'b0;
         // $display("show : %s", `MEM_FILE);
         $readmemh(`MEM_FILE, memory);
     end
