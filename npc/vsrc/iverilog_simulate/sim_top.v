f54f7b7c9f15a75584b40a042bf27b7fbd33b9bb
tracer-ysyx <tracer@ysyx.org>
2025-09-16 19:07:41 +0800

    > iverilog NPC ysyx_24080020 彭培智 Linux archlinux 6.12.47-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 12 Sep 2025 08:06:51 +0000 x86_64 GNU/Linux  19:07:41 up  6:27,  1 user,  load average: 2.28, 1.71, 1.28

--- npc/vsrc/iverilog_simulate/sim_top.v
+++ npc/vsrc/iverilog_simulate/sim_top.v
@@ -20,7 +20,7 @@ module sim_top;
 
 
     initial begin
-        $dumpfile("build/tb_wave.vcd"); // 指定 VCD 文件名
+        $dumpfile("build/tb_wave.fst"); // 指定 VCD 文件名
         // #200000;
         $dumpvars(0, sim_top); // 0 表示记录该模块及其所有子模块的所有信号。也可以指定特定层级或信号。
         #200 $finish;
