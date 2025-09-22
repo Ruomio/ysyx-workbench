51b158b899f1b902dea82dfc476073c2c2248537
tracer-ysyx <tracer@ysyx.org>
2025-09-22 15:00:18 +0800

    > iverilog NPC ysyx_24080020 彭培智 Linux archlinux 6.12.47-1-lts #1 SMP PREEMPT_DYNAMIC Fri, 12 Sep 2025 08:06:51 +0000 x86_64 GNU/Linux  15:00:18 up  5:56,  1 user,  load average: 3.96, 5.20, 5.25

--- npc/vsrc/iverilog_simulate/sim_top.v
+++ npc/vsrc/iverilog_simulate/sim_top.v
@@ -439,7 +439,7 @@ module sim_top;
         end
         else if(u_cpu.u_npc.exu.exu_mem_valid && u_cpu.u_npc.exu.mem_exu_ready) begin
             pc_delay_cnt <= 'h0;
-            // $display("%h", u_cpu.u_npc.exu.pc_exu);
+            $display("%h", u_cpu.u_npc.exu.pc_exu);
         end
         else begin
             pc_delay_cnt <= pc_delay_cnt + 1'b1;
@@ -456,8 +456,8 @@ module sim_top;
         if(reset) begin
         end
         else if(u_cpu.u_npc.u_uart.wvalid  && u_cpu.u_npc.u_uart.wready ) begin
-            $write("%c", u_cpu.u_npc.u_uart.wdata[7:0] );
-            $fflush();
+            // $write("%c", u_cpu.u_npc.u_uart.wdata[7:0] );
+            // $fflush();
         end
     end
 
