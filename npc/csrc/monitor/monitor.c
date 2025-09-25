ac20c3033066df763d4ee0b14fff071859226648
24080020-Peizhi Peng <1065940593@qq.com>
2025-09-25 18:30:13 +0800

    npc: set batch mode

--- npc/csrc/monitor/monitor.c
+++ npc/csrc/monitor/monitor.c
@@ -37,7 +37,6 @@ static void welcome() {
   printf("Welcome to %s-NPC!\n", ANSI_FMT(str(__GUEST_ISA__), ANSI_FG_YELLOW ANSI_BG_RED));
   printf("For help, type \"help\"\n");
  //  Log("Exercise: Please remove me in the source code and compile NEMU again.");
-  assert(1);
 }
 
 #ifndef CONFIG_TARGET_AM
