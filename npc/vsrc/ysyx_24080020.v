425ef619e6977b4f71a2f37c7ce5f394863ef1d4
tracer-ysyx <tracer@ysyx.org>
2025-09-25 10:35:34 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.48-1-lts #1 SMP PREEMPT_DYNAMIC Sun, 21 Sep 2025 17:47:58 +0000 x86_64 GNU/Linux  10:35:34 up  1:41,  1 user,  load average: 2.40, 2.02, 1.81

--- npc/vsrc/ysyx_24080020.v
+++ npc/vsrc/ysyx_24080020.v
@@ -78,13 +78,11 @@ module ysyx_24080020(
 
 );
 
-    wire [31:0] pc_ifu;
     // encapsulation to change pins' name
     ysyx_24080020_NPC u_npc(
         .clk(clock),
         .rst(!reset),
 
-        .pc_ifu_out(pc_ifu),
         // master
         // AR
         .io_master_arready(io_master_arready),
