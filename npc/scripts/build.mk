92319c3df0d347693da71ce4a6d2cab8b20171c9
24080020-Peizhi Peng <1065940593@qq.com>
2025-09-25 16:58:37 +0800

    npc: update makefile for iverilog-netlist

--- npc/scripts/build.mk
+++ npc/scripts/build.mk
@@ -274,7 +274,7 @@ $(VVP_FILE): $(VERILOG_TARGET) $(TB_FILE)
 # $(VVP_NETLIST_FILE): $(TB_NETLIST_FILE) $(NETLIST_FILE) $(CLEES_FILE)
 $(VVP_NETLIST_FILE): $(TB_NETLIST_FILE)
 	@echo "+ iverilog -> $@"
-	@iverilog -g2012 -o $@ $^ $(NETLIST) $(CELLS)
+	@iverilog -g2012 -D ysyx_24080020_NPC -o $@ $^ $(NETLIST) $(CELLS)
 
 iverilog: $(VVP_FILE)
 	@$(call git_commit, "iverilog NPC")
