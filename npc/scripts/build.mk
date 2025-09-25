b767cce4c69e124faeab5acc2ca06ea42cb1fbb0
tracer-ysyx <tracer@ysyx.org>
2025-09-25 15:27:00 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.48-1-lts #1 SMP PREEMPT_DYNAMIC Sun, 21 Sep 2025 17:47:58 +0000 x86_64 GNU/Linux  15:27:00 up  6:32,  1 user,  load average: 1.46, 1.29, 0.97

--- npc/scripts/build.mk
+++ npc/scripts/build.mk
@@ -170,7 +170,7 @@ $(BIN): modify-config clean_obj classic
 modify-config: $(CONF)
 ifeq ($(ARCH), riscv32e-npc)
 	@scripts/config --enable CONFIG_ISA_riscv32=y
-	@scripts/config --set-str CONFIG_ISA "riscv32"
+	@scripts/config --set-str CONFIG_ISA "riscv33"
 	@scripts/config --set-val CONFIG_MBASE 0x80000000
 	@scripts/config --set-val CONFIG_MSIZE 0x20000000
 	@scripts/config --disable CONFIG_PSRAM
