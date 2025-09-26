9e00a09319bb94409b0ee217bc130098a0ccffe0
tracer-ysyx <tracer@ysyx.org>
2025-09-26 15:36:23 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.48-1-lts #1 SMP PREEMPT_DYNAMIC Sun, 21 Sep 2025 17:47:58 +0000 x86_64 GNU/Linux  15:36:23 up  5:55,  2 users,  load average: 1.67, 1.84, 2.10

--- npc/vsrc/ysyx_24080020_DEFINE.v
+++ npc/vsrc/ysyx_24080020_DEFINE.v
@@ -23,8 +23,8 @@
 
 // cache
 `define ysyx_24080020_CACHE_SIZE 16
-`define ysyx_24080020_CACHE_NUM 2
-`define ysyx_24080020_CACHE_WAY 2
+`define ysyx_24080020_CACHE_NUM 4
+`define ysyx_24080020_CACHE_WAY 0
 
 // branch
 `define ysyx_24080020_BRANCH_SIZE 4
