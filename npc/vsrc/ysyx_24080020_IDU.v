406fe0b03d41ec05fc8f11616e640322535b0f8c
tracer-ysyx <tracer@ysyx.org>
2025-10-04 16:14:37 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.50-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 02 Oct 2025 13:18:36 +0000 x86_64 GNU/Linux  16:14:37 up  6:52,  1 user,  load average: 1.58, 1.29, 1.13

--- npc/vsrc/ysyx_24080020_IDU.v
+++ npc/vsrc/ysyx_24080020_IDU.v
@@ -135,7 +135,10 @@ module ysyx_24080020_IDU (
             idu_exu_valid <= 1'b0;
             inst_idu <= 'b0;
         end
-        else if((cnt == 'd2) && !need_stall) begin
+        else if(need_stall) begin
+            idu_exu_valid <= 1'b0;
+        end
+        else if(cnt == 'd2) begin
             idu_exu_valid <= 1'b1;
         end
         else if(ifu_idu_valid && idu_ifu_ready) begin
@@ -162,26 +165,17 @@ module ysyx_24080020_IDU (
         if(!rst) begin
             cnt <= 'b0;
         end
-        else if((idu_exu_valid_reg && exu_idu_ready) || need_stall) begin
+        else if((idu_exu_valid_reg && exu_idu_ready)) begin
             cnt <= 'b0;
+        end
+        else if(need_stall) begin
+            cnt <= ''b1;
         end
         else if(cnt == 'b1) begin
             cnt <= 'd2;
         end
-        else if((ifu_idu_valid && idu_ifu_ready) || (need_stall_next && !need_stall)) begin
+        else if((ifu_idu_valid && idu_ifu_ready)) begin
             cnt <= 'b1;
-        end
-    end
-
-    // after need_stall, need decode again
-    reg need_stall_next;
-
-    always @(posedge clk) begin
-        if(!rst) begin
-            need_stall_next <= 1'b0;
-        end
-        else begin
-            need_stall_next <= need_stall;
         end
     end
 
