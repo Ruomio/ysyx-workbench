af57523d958f7e700734ce6bc2cdced4ae5c1cb0
tracer-ysyx <tracer@ysyx.org>
2025-10-05 19:04:20 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.50-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 02 Oct 2025 13:18:36 +0000 x86_64 GNU/Linux  19:04:20 up 1 day,  9:42,  1 user,  load average: 1.98, 1.38, 1.11

--- npc/vsrc/ysyx_24080020_ADDER.v
+++
@@ -1,9 +0,0 @@
-module ysyx_24080020_ADDER (
-    input [31:0] a,
-    input [31:0] b,
-    output [31:0] c
-);
-
-    assign c = a + b;
-
-endmodule
--- npc/vsrc/ysyx_24080020_EXU.v
+++ npc/vsrc/ysyx_24080020_EXU.v
@@ -337,11 +337,7 @@ module ysyx_24080020_EXU
                             is_csrtype_exu == 1'b1 ? dnpc_exu : pc_exu;
     assign branch_src2 = is_csrtype_exu == 1'b1 ? 32'b0 : imm_exu;
 
-    ysyx_24080020_ADDER u_branch(
-        .a(branch_src1),
-        .b(branch_src2),
-        .c(branch_dnpc)
-    );
+    assign branch_dnpc = branch_src1 + branch_src2;
 
 
 endmodule
