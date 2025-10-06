2d9a1871daf69b2c7813fd7ef089ab771530fc04
tracer-ysyx <tracer@ysyx.org>
2025-10-06 10:23:06 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.50-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 02 Oct 2025 13:18:36 +0000 x86_64 GNU/Linux  10:23:06 up 2 days,  1:00,  2 users,  load average: 1.96, 1.46, 1.07

--- npc/vsrc/ysyx_24080020_IDU.v
+++ npc/vsrc/ysyx_24080020_IDU.v
@@ -272,37 +272,37 @@ module ysyx_24080020_IDU (
             // branch_src1_idu <= 'b0;
 
             // mem
-            mren_idu <= 1'b0;
-            mwen_idu <= 1'b0;
-            mren_idu <= 'b0;
-            mrlen_idu <= 'b0;
-            mrtype_idu <= 'b0;
-            mwmask_idu <= 'b0;
+            // mren_idu <= 1'b0;
+            // mwen_idu <= 1'b0;
+            // mren_idu <= 'b0;
+            // mrlen_idu <= 'b0;
+            // mrtype_idu <= 'b0;
+            // mwmask_idu <= 'b0;
 
-            // reg
-            wen_idu <= 1'b0;
-            waddr_idu <= 'b0;
-            wdata_idu <= 'b0;
+            // // reg
+            // wen_idu <= 1'b0;
+            // waddr_idu <= 'b0;
+            // wdata_idu <= 'b0;
 
-            // csr
-            is_csrtype_idu <= 1'b0;
-            wcsren_idu <= 1'b0;
-            wcsren2_idu <= 1'b0;
-            rcsraddr <= 'b0;
-            wcsraddr_idu <= 'b0;
-            wcsrdata_idu <= 'b0;
-            wcsraddr2_idu <= 'b0;
-            wcsrdata2_idu <= 'b0;
-            is_ebreak <= 'b0;
+            // // csr
+            // is_csrtype_idu <= 1'b0;
+            // wcsren_idu <= 1'b0;
+            // wcsren2_idu <= 1'b0;
+            // rcsraddr <= 'b0;
+            // wcsraddr_idu <= 'b0;
+            // wcsrdata_idu <= 'b0;
+            // wcsraddr2_idu <= 'b0;
+            // wcsrdata2_idu <= 'b0;
+            // is_ebreak <= 'b0;
 
-            // alu
-            alu_src2_con_idu <= 'b0;
-            alu_op_idu <= 'b0;
-            src1_idu <= 'b0;
-            src2_idu <= 'b0;
+            // // alu
+            // alu_src2_con_idu <= 'b0;
+            // alu_op_idu <= 'b0;
+            // src1_idu <= 'b0;
+            // src2_idu <= 'b0;
 
-            // other
-            fencei_idu <= 'b0;
+            // // other
+            // fencei_idu <= 'b0;
             // imm_idu <= 'd0;
         end
         else if(cnt == 'b0) begin
