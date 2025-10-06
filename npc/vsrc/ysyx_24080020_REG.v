f2d2300d23fc3f88f995434482ed17718fbf6653
tracer-ysyx <tracer@ysyx.org>
2025-10-06 14:55:47 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.50-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 02 Oct 2025 13:18:36 +0000 x86_64 GNU/Linux  14:55:47 up 2 days,  5:33,  2 users,  load average: 3.14, 3.44, 3.24

--- npc/vsrc/ysyx_24080020_REG.v
+++ npc/vsrc/ysyx_24080020_REG.v
@@ -124,9 +124,8 @@ module ysyx_24080020_REG
     //         regs[0] <= 'b0;
     //     end
     // end
-    assign regs[0] = 'b0;
     always @(posedge clk) begin
-        if(wen_wb) begin
+        if(wen_wb && waddr_wb) begin
             regs[waddr_wb] <= rd_data_wb;
         end
     end
