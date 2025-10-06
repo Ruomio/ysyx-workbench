a379e9d3a58684a6098a99e1f5462a6c7a8190df
tracer-ysyx <tracer@ysyx.org>
2025-10-06 10:43:56 +0800

    > iverilog NPC ysyx_24080020 彭培智 Linux archlinux 6.12.50-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 02 Oct 2025 13:18:36 +0000 x86_64 GNU/Linux  10:43:56 up 2 days,  1:21,  2 users,  load average: 1.43, 1.53, 1.46

--- npc/vsrc/iverilog_simulate/iverilog_top.v
+++ npc/vsrc/iverilog_simulate/iverilog_top.v
@@ -437,14 +437,14 @@ module sim_top;
         if(reset) begin
             pc_delay_cnt <= 32'h0;
         end
-        else if(u_cpu.u_npc.exu.exu_mem_valid && u_cpu.u_npc.exu.mem_exu_ready) begin
+        else if(u_cpu.u_npc.u_exu.exu_mem_valid && u_cpu.u_npc.u_exu.mem_exu_ready) begin
             pc_delay_cnt <= 'h0;
             // $display("%h", u_cpu.u_npc.exu.pc_exu);
         end
         else begin
             pc_delay_cnt <= pc_delay_cnt + 1'b1;
             if(pc_delay_cnt > 32'h200) begin
-                $display("EXU lag at pc: 0x%h", u_cpu.u_npc.exu.pc_exu);
+                $display("EXU lag at pc: 0x%h", u_cpu.u_npc.u_exu.pc_exu);
                 $display("PC delay count exceeded");
                 $finish;
             end
--- npc/vsrc/ysyx_24080020_EXU.v
+++ npc/vsrc/ysyx_24080020_EXU.v
@@ -160,49 +160,6 @@ module ysyx_24080020_EXU
 
             idu_exu_shake_hand <= 1'b0;
 
-            // wen_exu <= 'b0;
-            // waddr_exu <= 'b0;
-            // wdata_exu <= 'b0;
-
-            // mwen_exu <= 'b0;
-            // mwmask_exu <= 'b0;
-            // mren_exu <= 'b0;
-            // mrtype_exu <= 'b0;
-            // mrlen_exu <= 'b0;
-
-            // is_load_exu <= 'b0;
-            // is_dnpc_exu <= 'b0;
-            // branch_src1_exu <= 'b0;
-
-            // alu_op_exu <= 'b0;
-            // alu_src2_con_exu <= 'b0;
-            // reg_dst_con_exu <= 'b0;
-            // imm_exu <= 'b0;
-
-            src1_exu <= 'b0;
-            src2_exu <= 'b0;
-            pc_exu <= 'b0;
-            dnpc_exu <= 'b0;
-            is_jalr_exu <= 'b0;
-            is_jal_exu <= 'b0;
-            is_btype_exu <= 'b0;
-
-            wcsren_exu <= 'b0;
-            wcsraddr_exu <= 'b0;
-            wcsrdata_exu <= 'b0;
-            wcsren2_exu <= 'b0;
-            wcsraddr2_exu <= 'b0;
-            wcsrdata2_exu <= 'b0;
-
-            is_csrtype_exu <= 'b0;
-
-
-            fencei_exu <= 'b0;
-
-
-            is_ebreak_exu <= 'b0;
-            // exu_mem_valid <= 'b0;
-
         end
         else if(mem_exu_ready && exu_mem_valid && state) begin
             // exu_mem_valid <= 1'b0;
