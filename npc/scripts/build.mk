e6fcfd9fd0849cbcd52cc29f0223cc17451af393
tracer-ysyx <tracer@ysyx.org>
2025-10-06 11:26:26 +0800

    > iverilog NPC ysyx_24080020 彭培智 Linux archlinux 6.12.50-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 02 Oct 2025 13:18:36 +0000 x86_64 GNU/Linux  11:26:26 up 2 days,  2:04,  2 users,  load average: 4.38, 2.91, 2.53

--- npc/scripts/build.mk
+++ npc/scripts/build.mk
@@ -281,11 +281,14 @@ iverilog: $(VVP_FILE)
 	@echo "+ exec vvp $^"
 	@vvp $^ +MEM_FILE=$(IMG)
 
-iverilog-netlist: $(VVP_NETLIST_FILE)
+iverilog-netlist: generate_netlist $(VVP_NETLIST_FILE)
 	@$(call git_commit, "iverilog NPC")
 	@echo "+ exec vvp $^"
 	@vvp $^ +MEM_FILE=$(IMG)
 	@vcd2fst build/tb_netlist_wave.vcd build/tb_netlist_wave.fst
+
+generate_netlist:
+	@make -s -C $(YSYX_HOME)/yosys-sta sta
 
 # CI TEST
 VVP_CI_FILE = $(BUILD_DIR)/ci_sim_top.v
