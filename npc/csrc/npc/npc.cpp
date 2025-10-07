10bac9eb539e1ec6fd41a8ff61e656f993bb98e3
tracer-ysyx <tracer@ysyx.org>
2025-10-07 12:37:52 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.50-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 02 Oct 2025 13:18:36 +0000 x86_64 GNU/Linux  12:37:52 up 3 days,  3:15,  2 users,  load average: 1.65, 1.26, 1.14

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -528,9 +528,9 @@ uint32_t g_get_reg(int i) {
 }
 uint32_t g_get_csrs(int i) {
 #if defined (ysyxSoCFull)
-  return (top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__u_npc__DOT__u_reg__DOT__csrs[i]);
+  return (top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__u_npc__DOT__u_csr__DOT__csrs[i]);
 #elif defined (ysyx_24080020_NPC)
-  return top->rootp->ysyx_24080020_NPC__DOT__u_reg__DOT__csrs[i];
+  return top->rootp->ysyx_24080020_NPC__DOT__u_csr__DOT__csrs[i];
 #else
   return 0;
 #endif
