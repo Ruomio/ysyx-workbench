6cbb0d0d72a82ed36f376d342853113ba62bfad6
tracer-ysyx <tracer@ysyx.org>
2025-10-08 10:28:29 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.50-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 02 Oct 2025 13:18:36 +0000 x86_64 GNU/Linux  10:28:29 up 4 days,  1:06,  2 users,  load average: 1.37, 1.22, 1.18

--- npc/csrc/npc/difftest/dut.c
+++ npc/csrc/npc/difftest/dut.c
@@ -131,14 +131,14 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   bool flag = true;
   if(g_pc != ref_r->pc) { printf("pc is diff, should be: 0x%x, but get: 0x%x\n", ref_r->pc, g_pc); flag = false;}
   for(int i=0; i<sizeof(ref_r->gpr)/sizeof(ref_r->gpr[0]); i++) {
-    if(ref_r->gpr[i] != g_get_reg(i)) {
+    if(ref_r->gpr[i] != npc_cpu.gpr[i]) {
       printf("The %s reg is diff, shoud be %#x  but get %#x.\n", regs_name[i], ref_r->gpr[i], g_get_reg(i));
       flag = false;
       // break;
     }
   }
-  for(int i=0; i<sizeof(ref_r->csrs)/sizeof(ref_r->csrs[0]) - 2; i++) {
-    if(ref_r->csrs[i] != g_get_csrs(i)) {
+  for(int i=0; i<sizeof(ref_r->csrs)/sizeof(ref_r->csrs[0]); i++) {
+    if(ref_r->csrs[i] != npc_cpu.csrs[i]) {
       printf("The %s csr is diff, shoud be %#x  but get %#x.\n", csrs_name[i], ref_r->csrs[i], g_get_csrs(i));
       flag = false;
       // break;
