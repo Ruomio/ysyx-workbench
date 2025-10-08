da7b5a37e9cb9693eb2685b63993fd899dfe370a
tracer-ysyx <tracer@ysyx.org>
2025-10-08 15:58:49 +0800

    > iverilog NPC ysyx_24080020 彭培智 Linux archlinux 6.12.50-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 02 Oct 2025 13:18:36 +0000 x86_64 GNU/Linux  15:58:49 up 4 days,  6:36,  2 users,  load average: 1.25, 1.24, 1.45

--- npc/vsrc/iverilog_simulate/iverilog_top.v
+++ npc/vsrc/iverilog_simulate/iverilog_top.v
@@ -455,7 +455,7 @@ module sim_top;
     always @(posedge clock) begin
         if(reset) begin
         end
-        else if(u_cpu.u_npc.idu_exu_valid_reg && u_cpu.u_npc.exu_idu_ready &&
+        else if(u_cpu.u_npc.idu_exu_valid && u_cpu.u_npc.exu_idu_ready &&
         u_cpu.u_npc.inst_idu == 32'h00100073) begin
             $display("ebreak inst!");
             $finish;
