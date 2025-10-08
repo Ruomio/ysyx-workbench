dec53528e51e49cb7b14ef4fbc7ec8ee54cc7e15
tracer-ysyx <tracer@ysyx.org>
2025-10-08 09:43:57 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.50-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 02 Oct 2025 13:18:36 +0000 x86_64 GNU/Linux  09:43:57 up 4 days, 21 min,  2 users,  load average: 1.32, 1.20, 1.03

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -581,10 +581,15 @@ uint32_t g_get_rd() {
 void update_npc_cpu() {
   for(int i=0; i<CONFIG_REGS_NUM; i++) {
     npc_cpu.gpr[i] = g_get_reg(i);
-    if(i<6) {
+    if(i<4) {
       npc_cpu.csrs[i] = g_get_csrs(i);
     }
   }
+
+  // assign csr_mvendorid = 32'h79737978;
+  // assign csr_marchid = 32'h16f6e94;
+  npc_cpu.csrs[4] = 0x79737978;
+  npc_cpu.csrs[5] = 0x16f6e94;
   npc_cpu.pc = g_pc;
 }
 
--- npc/vsrc/ysyx_24080020_CSR.v
+++ npc/vsrc/ysyx_24080020_CSR.v
@@ -19,13 +19,18 @@ module ysyx_24080020_CSR
     input mem_wb_valid,
     input wb_mem_ready
 );
-    // csrs[0] = mepc, csrs[1] = mstatus, csrs[2] = mcause, csrs[3] = mtvec, csrs[4] = MVENDORID, csrs[5] = MARCHID
-    reg [`ysyx_24080020_WIDTH-1:0] csrs[0:5];
+    // csrs[0] = mepc, csrs[1] = mstatus, csrs[2] = mcause, csrs[3] = mtvec, // csrs[4] = MVENDORID, csrs[5] = MARCHID
+    reg [`ysyx_24080020_WIDTH-1:0] csrs[0:3];
+
+    wire [`ysyx_24080020_WIDTH-1:0] csr_mvendorid, csr_marchid;
 
     // 精简的流水线寄存器
     reg csr1_wen_wb, csr2_wen_wb;
     reg [2:0] csr1_addr_wb, csr2_addr_wb;
     reg [`ysyx_24080020_WIDTH-1:0] csr1_data_wb, csr2_data_wb;
+
+    assign csr_mvendorid = 32'h79737978;
+    assign csr_marchid = 32'h16f6e94;
 
     always @(posedge clk) begin
         if(!rst) begin
@@ -45,8 +50,6 @@ module ysyx_24080020_CSR
     always @(posedge clk) begin
         if(!rst) begin
             csrs[1] <= 32'h1800;        // mstatus
-            csrs[4] <= 32'h79737978;    // MVENDORID
-            csrs[5] <= 32'h16f6e94;     // MARCHID
         end else begin
             if(csr1_wen_wb) csrs[csr1_addr_wb] <= csr1_data_wb;
             if(csr2_wen_wb) csrs[csr2_addr_wb] <= csr2_data_wb;
@@ -54,5 +57,16 @@ module ysyx_24080020_CSR
     end
 
     // 读取端口
-    assign rcsrdata = csrs[rcsraddr];
+    // assign rcsrdata = csrs[rcsraddr];
+    always @(*) begin
+        case(rcsraddr)
+            0: rcsrdata = csrs[0];
+            1: rcsrdata = csrs[1];
+            2: rcsrdata = csrs[2];
+            3: rcsrdata = csrs[3];
+            4: rcsrdata = csr_mvendorid;
+            5: rcsrdata = csr_marchid;
+            default: rcsrdata = 32'h0;
+        endcase
+    end
 endmodule
