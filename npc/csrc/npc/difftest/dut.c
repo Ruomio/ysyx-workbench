d93463c41f8c85b5b5e6fcb4f06ba169b4ea5d51
tracer-ysyx <tracer@ysyx.org>
2025-10-08 10:09:56 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.50-1-lts #1 SMP PREEMPT_DYNAMIC Thu, 02 Oct 2025 13:18:36 +0000 x86_64 GNU/Linux  10:09:56 up 4 days, 47 min,  2 users,  load average: 1.49, 1.31, 1.17

--- npc/csrc/npc/difftest/dut.c
+++ npc/csrc/npc/difftest/dut.c
@@ -137,7 +137,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
       // break;
     }
   }
-  for(int i=0; i<sizeof(ref_r->csrs)/sizeof(ref_r->csrs[0]); i++) {
+  for(int i=0; i<sizeof(ref_r->csrs)/sizeof(ref_r->csrs[0]) - 2; i++) {
     if(ref_r->csrs[i] != g_get_csrs(i)) {
       printf("The %s csr is diff, shoud be %#x  but get %#x.\n", csrs_name[i], ref_r->csrs[i], g_get_csrs(i));
       flag = false;
