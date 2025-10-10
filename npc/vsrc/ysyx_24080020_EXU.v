c5d3f1d3fb5eeebff999bfc6a2aaea700b29de82
tracer-ysyx <tracer@ysyx.org>
2025-10-10 16:44:09 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.51-1-lts #1 SMP PREEMPT_DYNAMIC Mon, 06 Oct 2025 11:05:41 +0000 x86_64 GNU/Linux  16:44:09 up  7:31,  1 user,  load average: 1.68, 1.46, 1.32

--- npc/vsrc/ysyx_24080020_EXU.v
+++ npc/vsrc/ysyx_24080020_EXU.v
@@ -31,6 +31,7 @@ module ysyx_24080020_EXU (
     input  wire        is_ebreak_idu,
     input  wire        is_ecall_idu,
     input  wire        is_auipc_idu,
+    input  wire        is_i_type_idu,
     input  wire        fencei_idu,
     input  wire [2:0]  mem_len_idu,
     input  wire [2:0]  mem_wmask_idu,
@@ -86,9 +87,9 @@ module ysyx_24080020_EXU (
 //=========================================================================
 // 流水线控制与寄存器
 //=========================================================================
-localparam PIPE_CTRL_W = 18;
+localparam PIPE_CTRL_W = 19;
 wire [PIPE_CTRL_W-1:0] ctrl_comb = {
-    is_auipc_idu,
+    is_i_type_idu, is_auipc_idu,
     is_ecall_idu, is_branch_idu, is_jal_idu, is_jalr_idu, is_load_idu, is_store_idu,
     is_csr_idu, is_ebreak_idu, fencei_idu,
     mem_len_idu, alu_op_idu
@@ -157,7 +158,9 @@ wire        is_branch_exu;
 wire        is_jal_exu;
 wire        is_jalr_exu;
 wire        is_auipc_exu;
+wire        is_i_type_exu;
 
+assign is_i_type_exu = ctrl_q_reg[18];
 assign is_auipc_exu = ctrl_q_reg[17];
 assign is_ecall_exu = ctrl_q_reg[16];
 assign is_branch_exu = ctrl_q_reg[15];
@@ -180,8 +183,7 @@ assign alu_src1 = (is_jal_exu || is_jalr_exu || is_auipc_exu) ? pc_q_reg : val1_
 
 // src2选择：根据指令类型选择立即数或rs2
 wire use_imm = is_load_exu || is_store_exu || is_branch_exu ||
-               is_jal_exu || is_jalr_exu || is_auipc_exu;
-              // (alu_op_exu != `ysyx_24080020_ALU_ADD || ctrl_q_reg[12:11] == 2'b00); // I-type指令
+               is_jal_exu || is_jalr_exu || is_auipc_exu || is_i_type_exu;
 
 assign alu_src2 = use_imm ? imm_q_reg : val2_q_reg;
 
--- npc/vsrc/ysyx_24080020_IDU.v
+++ npc/vsrc/ysyx_24080020_IDU.v
@@ -50,6 +50,7 @@ module ysyx_24080020_IDU (
     output wire        is_store_idu,
     output wire        is_csr_idu,
     output wire        is_auipc_idu,
+    output wire        is_i_type_idu,
 
     output wire        fencei_idu,
     output wire [2:0]  mem_len_idu,
@@ -185,6 +186,7 @@ assign wcsren_idu      = (is_csr && (funct3 != 3'b000 && funct3 != 3'b100)) // C
 // 5. 其余控制信号（从寄存器化的ctrl_q中获取）
 //=========================================================================
 
+assign is_i_type_idu = ctrl_q[50];
 assign is_auipc_idu = ctrl_q[49];
 assign is_ecall_idu = ctrl_q[48];
 assign is_branch_idu = ctrl_q[47];
@@ -219,9 +221,9 @@ assign csr_hold = ecall_phase;
 //=========================================================================
 // 6. 极简流水线握手 & 锁存（）
 //=========================================================================
-localparam PIPE_CTRL_W = 50;
+localparam PIPE_CTRL_W = 51;
 wire [PIPE_CTRL_W-1:0] ctrl_comb = {
-    is_auipc,
+    is_i_type, is_auipc,
     is_ecall, is_b_type, is_j_type, is_jr_type,
     is_load_type, is_s_type, is_csr, is_fencei, is_ebreak,
     funct3, imm_comb, alu_op_comb
