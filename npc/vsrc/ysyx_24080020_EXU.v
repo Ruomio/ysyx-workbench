f10d8ee7e54e903800870a9ee154e19c6e557450
tracer-ysyx <tracer@ysyx.org>
2025-10-11 11:25:49 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.51-1-lts #1 SMP PREEMPT_DYNAMIC Mon, 06 Oct 2025 11:05:41 +0000 x86_64 GNU/Linux  11:25:49 up 1 day,  2:13,  1 user,  load average: 0.59, 0.59, 0.53

--- npc/vsrc/ysyx_24080020_EXU.v
+++ npc/vsrc/ysyx_24080020_EXU.v
@@ -244,7 +244,7 @@ assign store_data_exu = val2_q_reg;
 // 写回数据选择：JAL/JALR写回PC+4，load指令后续由MEM阶段写回，其他写回ALU结果
 wire [31:0] pc_plus_4 = pc_q_reg + 32'd4;
 assign wdata_exu = (is_jal_exu || is_jalr_exu) ? pc_plus_4 :
-                   is_csr_exu ? val1_q_reg :  // CSR指令写回原寄存器值
+                   is_csr_exu ? rcsrdata_q_reg :  // CSR指令写回原寄存器值
                    alu_result;                // ALU指令写回计算结果
 
 assign waddr_exu = waddr_q_reg;
