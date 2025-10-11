4554d573126e320bd1c18f1c5304b5625a8da9fa
tracer-ysyx <tracer@ysyx.org>
2025-10-11 12:23:05 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.51-1-lts #1 SMP PREEMPT_DYNAMIC Mon, 06 Oct 2025 11:05:41 +0000 x86_64 GNU/Linux  12:23:05 up 1 day,  3:10,  1 user,  load average: 0.48, 0.38, 0.27

--- npc/vsrc/ysyx_24080020_BTB.v
+++ npc/vsrc/ysyx_24080020_BTB.v
@@ -526,7 +526,7 @@ module ysyx_24080020_BTB(
         if(!rst) begin
             branch_not_taken_exu_next <= 'b0;
         end
-        else if(branch_not_taken_exu_next && (pc_exu != hit_pc)) begin
+        else if(branch_not_taken_exu_next && (dnpc != dnpc_tmp)) begin
             branch_not_taken_exu_next <= 'b0;
         end
         else if(branch_not_taken_exu) begin
