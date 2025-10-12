4e541b178e1ffc2c192180c47c1ad55615a931b8
tracer-ysyx <tracer@ysyx.org>
2025-10-12 09:45:12 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.51-1-lts #1 SMP PREEMPT_DYNAMIC Mon, 06 Oct 2025 11:05:41 +0000 x86_64 GNU/Linux  09:45:12 up 10:54,  1 user,  load average: 1.58, 1.49, 1.33

--- npc/csrc/npc/npc.cpp
+++ npc/csrc/npc/npc.cpp
@@ -579,9 +579,11 @@ uint32_t g_get_dnpc() {
 
 uint32_t g_get_rs1() {
 #if defined (ysyxSoCFull)
-  return BITS(top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__u_npc__DOT__inst_ifu, 19, 15);
+  // return BITS(top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__u_npc__DOT__inst_ifu, 19, 15);
+  return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__u_npc__DOT__rs1;
 #elif defined (ysyx_24080020_NPC)
-  return BITS(top->rootp->ysyx_24080020_NPC__DOT__inst_ifu, 19, 15);
+  // return BITS(top->rootp->ysyx_24080020_NPC__DOT__inst_ifu, 19, 15);
+  return top->rootp->ysyx_24080020_NPC__DOT__rs1;
 #else
   return 0;
 #endif
@@ -589,10 +591,10 @@ uint32_t g_get_rs1() {
 uint32_t g_get_rd() {
 #if defined (ysyxSoCFull)
   // return BITS(top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__u_npc__DOT__inst_ifu, 19, 7);
-  return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__u_npc__DOT__rs1;
+  return top->rootp->ysyxSoCFull__DOT__asic__DOT__cpu__DOT__cpu__DOT__u_npc__DOT__waddr_idu;
 #elif defined (ysyx_24080020_NPC)
   // return BITS(top->rootp->ysyx_24080020_NPC__DOT__inst_ifu, 19, 15);
-  return top->rootp->ysyx_24080020_NPC__DOT__rs1;
+  return top->rootp->ysyx_24080020_NPC__DOT__waddr_idu;
 #else
 
 #endif
