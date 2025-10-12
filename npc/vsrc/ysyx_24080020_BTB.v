631d313fad4fded9c1cc16c3183961b8ddb471d1
tracer-ysyx <tracer@ysyx.org>
2025-10-12 17:44:57 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.51-1-lts #1 SMP PREEMPT_DYNAMIC Mon, 06 Oct 2025 11:05:41 +0000 x86_64 GNU/Linux  17:44:57 up 18:54,  1 user,  load average: 1.37, 1.32, 1.32

--- npc/vsrc/ysyx_24080020_BTB.v
+++ npc/vsrc/ysyx_24080020_BTB.v
@@ -104,7 +104,7 @@ always @(posedge clk) begin
     end
     else if (is_dnpc) begin
         // 同一拍完成写
-        target[index][ (({ {(branch_data_group_bits-branch_way){1'b0}} , way}+ 'b1) << 5 )-:branch_size ] <= dnpc;
+        target[index][ (({ {(branch_data_group_bits-branch_way){1'b0}} , way}+ 'b1) << 5 )-:(branch_size<<3) ] <= dnpc;
         tag_r[index][( { {(branch_tag_size-branch_way){1'b0}}, way} +'b1 )-:branch_tag_size]  <= tag;
         valid[index][1 << way]  <= 1'b1;
         fifo_ptr[index]    <= (fifo_ptr[index] + 1) % branch_way;
