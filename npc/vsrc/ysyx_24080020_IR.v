414a0eaaa536c93278176e7169c9c8bc36bc9ea8
tracer-ysyx <tracer@ysyx.org>
2025-10-12 10:42:14 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.51-1-lts #1 SMP PREEMPT_DYNAMIC Mon, 06 Oct 2025 11:05:41 +0000 x86_64 GNU/Linux  10:42:14 up 11:51,  1 user,  load average: 1.43, 1.07, 0.80

--- npc/vsrc/ysyx_24080020_IR.v
+++ npc/vsrc/ysyx_24080020_IR.v
@@ -53,12 +53,12 @@ always @(posedge clk) begin
         valid_q_reg     <= 1'b0;
     end
     // else if ((inst_fin_valid & inst_fin_ready)) begin
-    else if (arready) begin
-        valid_q_reg     <= 1'b0;
-    end
     else if (if_en_valid && if_en_ready) begin
         valid_q_reg     <= 1'b1;
         addr_q          <= addr;
+    end
+    else if (arready) begin
+        valid_q_reg     <= 1'b0;
     end
 end
 // 反压：本级空就能收
