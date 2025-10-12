85860476b6a1b86f68f5fddfecc3c8a928d090e6
tracer-ysyx <tracer@ysyx.org>
2025-10-12 14:00:55 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.51-1-lts #1 SMP PREEMPT_DYNAMIC Mon, 06 Oct 2025 11:05:41 +0000 x86_64 GNU/Linux  14:00:55 up 15:09,  1 user,  load average: 1.36, 1.01, 0.93

--- npc/vsrc/ysyx_24080020_NPC.v
+++ npc/vsrc/ysyx_24080020_NPC.v
@@ -413,7 +413,7 @@ module ysyx_24080020_NPC(
         // .correct_pc_btb(correct_pc_btb),
         // .flush_pipeline(flush_pipeline),
         // exu -> ir
-        .need_flush_pipeline((exu_mem_valid & mem_exu_ready) & (branch_not_taken_exu | (branch_taken_exu & btb_hit)),
+        .need_flush_pipeline((exu_mem_valid & mem_exu_ready) & (branch_not_taken_exu | (branch_taken_exu & btb_hit))),
         .correct_pc_btb(branch_not_taken_exu ? (pc_exu+32'd4) : dnpc_exu),
         .flush_pipeline(flush_pipeline),
 
