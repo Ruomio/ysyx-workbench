432316749461149244bc5148c29059f1b5851c71
24080020-Peizhi Peng <1065940593@qq.com>
2025-10-28 10:59:59 +0800

    fix nemu for difftest(should not copy pc)

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -17,6 +17,7 @@
 #include <cpu/cpu.h>
 #include <difftest-def.h>
 #include <memory/paddr.h>
+#include <stdint.h>
 
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   if(direction == DIFFTEST_TO_REF) likely(in_pmem(addr)) ? memcpy(guest_to_host(addr), buf, n) : assert(0);
@@ -24,8 +25,16 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
+  // recover pc when skip ref
+  uint32_t tmp_pc = cpu.pc;
+
   if(direction == DIFFTEST_TO_REF) dut != NULL ?  memcpy(&cpu, (CPU_state *)dut, sizeof(cpu)) : assert(0);
   else dut != NULL ? memcpy((CPU_state *)dut, &cpu, sizeof(cpu)) : assert(0);
+
+  // recover pc when skip ref
+  if(direction == DIFFTEST_TO_REF) {
+    cpu.pc = tmp_pc;
+  }
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
