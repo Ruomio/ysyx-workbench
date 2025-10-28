75fecc7b762c73e51808f1c1df8a38ecacff2c46
24080020-Peizhi Peng <1065940593@qq.com>
2025-10-28 18:19:47 +0800

    change sram_axi delayer to 1 cycle

--- npc/vsrc/iverilog_simulate/iverilog_netlist_top.v
+++ npc/vsrc/iverilog_simulate/iverilog_netlist_top.v
@@ -215,7 +215,7 @@ module sim_top;
     wire [5:0] lfsr;    // the number of delay cycle
 
     assign wstrb_full = {{8{io_master_wstrb[3]}}, {8{io_master_wstrb[2]}}, {8{io_master_wstrb[1]}}, {8{io_master_wstrb[0]}}};
-    assign lfsr = 6'd32;
+    assign lfsr = 6'd1;
 
     // AR
     always @(posedge clock) begin
