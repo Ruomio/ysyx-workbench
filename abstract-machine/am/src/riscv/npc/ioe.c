77d662d7faf38455b67493167159b140cfe6f8ef
tracer-ysyx <tracer@ysyx.org>
2025-10-30 16:39:59 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.55-1-lts #1 SMP PREEMPT_DYNAMIC Sun, 26 Oct 2025 07:10:27 +0000 x86_64 GNU/Linux  16:39:59 up 1 day,  7:20,  1 user,  load average: 1.43, 1.43, 1.99

--- abstract-machine/am/src/riscv/npc/ioe.c
+++ abstract-machine/am/src/riscv/npc/ioe.c
@@ -1,4 +1,5 @@
 #include <am.h>
+#include <klib.h>
 #include <klib-macros.h>
 
 void __am_timer_init();
@@ -30,5 +31,5 @@ bool ioe_init() {
   return true;
 }
 
-void ioe_read (int reg, void *buf) { ((handler_t)lut[reg])(buf); }
-void ioe_write(int reg, void *buf) { ((handler_t)lut[reg])(buf); }
+void ioe_read (int reg, void *buf) { printf("%d\n", reg); ((handler_t)lut[reg])(buf); }
+void ioe_write(int reg, void *buf) { printf("%d\n", reg); ((handler_t)lut[reg])(buf); }
