2321824293fbd99b89994bb37c9b6e1e84e9d998
tracer-ysyx <tracer@ysyx.org>
2025-10-30 14:38:02 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.55-1-lts #1 SMP PREEMPT_DYNAMIC Sun, 26 Oct 2025 07:10:27 +0000 x86_64 GNU/Linux  14:38:02 up 1 day,  5:18,  1 user,  load average: 1.58, 0.91, 1.00

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -54,7 +54,7 @@ void init_uart(int baud_rate) {
   // set baud rate, offset = 0x1 and 0x2
   // div = freq / (16 * baud rate)
   // uint16_t baud_div = 3686400 / (32 * baud_rate);
-  uint16_t baud_div = 0x1000;
+  uint16_t baud_div = 0x0000;
   outb(SERIAL_ADDR, (uint8_t)baud_div);
   outb(SERIAL_ADDR+0x1, (uint8_t)(baud_div >> 8));
 
