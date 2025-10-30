1b23f4c325dea61ef6137fa06ba80138b90687bb
tracer-ysyx <tracer@ysyx.org>
2025-10-30 14:47:54 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.55-1-lts #1 SMP PREEMPT_DYNAMIC Sun, 26 Oct 2025 07:10:27 +0000 x86_64 GNU/Linux  14:47:54 up 1 day,  5:28,  1 user,  load average: 1.44, 1.40, 1.25

--- npc/scripts/build.mk
+++ npc/scripts/build.mk
@@ -177,7 +177,7 @@ ifeq ($(ARCH), riscv32e-ysyxsoc)
 	@scripts/config --set-val CONFIG_SRAM_SIZE 0x00002000
 	@scripts/config --enable CONFIG_DIFFTEST
 	@scripts/config --enable CONFIG_LIGHTSSS
-	@scripts/config --enable CONFIG_NVBOARD
+	@scripts/config --disable CONFIG_NVBOARD
 endif
 	$(Q)$(CONF) $(silent) --syncconfig $(Kconfig)
 
