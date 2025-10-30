899d2d5dc946f2f4e892ac2b4b2f68429fba1775
tracer-ysyx <tracer@ysyx.org>
2025-10-30 19:25:34 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.55-1-lts #1 SMP PREEMPT_DYNAMIC Sun, 26 Oct 2025 07:10:27 +0000 x86_64 GNU/Linux  19:25:34 up 1 day, 10:05,  2 users,  load average: 1.24, 1.08, 0.70

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -13,7 +13,7 @@ extern void free_npc();
 extern void signal_handler_abort(int signum);
 
 int main(int argc, char **argv) {
-  signal(SIGABRT, signal_handler_abort);
+  // signal(SIGABRT, signal_handler_abort);
 
   init_monitor(argc, argv);
 
