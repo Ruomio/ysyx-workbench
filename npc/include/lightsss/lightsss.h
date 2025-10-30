fd838f441066be8f8449521d48ca4d1bfe05d23f
tracer-ysyx <tracer@ysyx.org>
2025-10-30 18:16:35 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.55-1-lts #1 SMP PREEMPT_DYNAMIC Sun, 26 Oct 2025 07:10:27 +0000 x86_64 GNU/Linux  18:16:35 up 1 day,  8:56,  2 users,  load average: 1.45, 1.54, 1.92

--- npc/include/lightsss/lightsss.h
+++ npc/include/lightsss/lightsss.h
@@ -69,7 +69,6 @@ public:
   LightSSS() {
       signal(SIGINT, signal_handler); // 注册SIGINT处理器 ctrl c
       signal(SIGTERM, signal_handler); // 注册SIGTERM处理器 kill
-      signal(SIGKILL, signal_handler); // 注册SIGTERM处理器 kill
       signal(SIGABRT, signal_handler_abort); // 注册SIGTERM处理器 assert faile
       signal(SIGUSR1, signal_handler_abort); // 注册USR1处理器
   }
