906bae9cd24993d32a610c5229bc5b89761d25a0
tracer-ysyx <tracer@ysyx.org>
2025-10-31 09:43:57 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.56-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 29 Oct 2025 14:59:09 +0000 x86_64 GNU/Linux  09:43:57 up  1:27,  1 user,  load average: 1.64, 1.79, 1.71

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -76,7 +76,7 @@ void _trm_init() {
   // fsbl();
   init_uart(115200);
   show_stu_no();
-  printf("Area.start: 0x%x, Area.end: 0x%x", heap.start, heap.end);
+  printf("Area.start: 0x%x, Area.end: 0x%x\n", heap.start, heap.end);
   int ret = main(mainargs);
   halt(ret);
 }
