d74366fb0f8e5317b006f01ec145daef424265cb
tracer-ysyx <tracer@ysyx.org>
2025-10-31 09:37:59 +0800

    > sim NPC ysyx_24080020 彭培智 Linux archlinux 6.12.56-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 29 Oct 2025 14:59:09 +0000 x86_64 GNU/Linux  09:37:59 up  1:21,  1 user,  load average: 1.37, 1.61, 1.58

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -6,6 +6,7 @@
 
 
 extern char _heap_start;
+extern char _sdram_base;
 int main(const char *args);
 
 
@@ -14,8 +15,8 @@ int main(const char *args);
 // #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
 // Area heap = RANGE(&_heap_start, PMEM_END);
-#define HEAP_SIZE (16 * 1024 * 1024)
-#define HEAP_END ((uintptr_t)&_heap_start + HEAP_SIZE)
+#define HEAP_SIZE (32 * 1024 * 1024)
+#define HEAP_END ((uintptr_t)&_sdram_base + HEAP_SIZE)
 Area heap = RANGE(&_heap_start, HEAP_END);
 static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS
 
